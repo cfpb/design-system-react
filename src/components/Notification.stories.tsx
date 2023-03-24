df9ba6c88d489d97fda4d6fe2256c7620537a2c6
Meis <meissadia@gmail.com>
2023-03-24 09:03:50 -0600

    [Stories] Notification - Use templates

--- src/components/Notification.stories.tsx
+++ src/components/Notification.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { ComponentStory } from '@storybook/react';
 import { Notification } from './Notification';
 
 export default {
@@ -17,20 +17,24 @@ Source: https://cfpb.github.io/design-system/components/notifications
   }
 };
 
-const message = 'A Notification';
-
-export const information = (): React.ReactElement => (
-  <Notification type='info' message={message} />
+const Template: ComponentStory<typeof Notification> = args => (
+  <Notification {...args} />
 );
 
-export const informationWithExplaination = (): React.ReactElement => (
-  <Notification type='info' message={message}>
-    You can also add an explanation to the notification.
-  </Notification>
-);
+export const information = Template.bind({});
+information.args = { type: 'info', message: 'A Notification' };
 
-export const informationWithLinks = (): React.ReactElement => {
-  const links = [
+export const informationWithExplaination = Template.bind({});
+informationWithExplaination.args = {
+  ...information.args,
+  children: 'You can also add an explanation to the notification.'
+};
+
+export const informationWithLinks = Template.bind({});
+informationWithLinks.args = {
+  ...information.args,
+  children: 'This is the explanation of the notification.',
+  links: [
     {
       href: '/',
       label: 'This is a link below the explaination'
@@ -40,27 +44,17 @@ export const informationWithLinks = (): React.ReactElement => {
       label: 'This is an external link',
       isExternal: true
     }
-  ];
-
-  return (
-    <Notification type='info' message={message} links={links}>
-      This is the explanation of the notification.
-    </Notification>
-  );
+  ]
 };
 
-export const success = (): React.ReactElement => (
-  <Notification type='success' message='11 results' />
-);
+export const success = Template.bind({});
+success.args = { type: 'success', message: '11 results' };
 
-export const warning = (): React.ReactElement => (
-  <Notification type='warning' message='No results found.' />
-);
+export const warning = Template.bind({});
+warning.args = { type: 'warning', message: 'No results found.' };
 
-export const error = (): React.ReactElement => (
-  <Notification type='error' message='Page not found.' />
-);
+export const error = Template.bind({});
+error.args = { type: 'error', message: 'Page not found.' };
 
-export const loading = (): React.ReactElement => (
-  <Notification type='loading' message='The page is loading….' />
-);
+export const loading = Template.bind({});
+loading.args = { type: 'loading', message: 'The page is loading….' };
