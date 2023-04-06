26b3f90c62dcaa362b63248c46d0f3b04b4578f1
James L <jlivolsi@teamraft.com>
2023-04-05 22:53:35 -0400

    fix: type castings for dropdown component

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,140 +1,161 @@
-import { MouseEventHandler, useState } from 'react';
-import Select, { createFilter } from 'react-select';
+/* eslint-disable unicorn/no-null */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import type { KeyboardEventHandler, MouseEventHandler } from 'react';
+import { useState } from 'react';
 import type {
   GroupBase,
-  MultiValue,
   OnChangeValue,
-  Props
-} from 'react-select/dist/declarations/src/index';
+  OptionsOrGroups,
+  PropsValue
+} from 'react-select';
+import Select, { createFilter } from 'react-select';
 import { Icon } from './Icon';
 import { Label } from './Label';
 
-// https://react-select.com/typescript#custom-select-props
-declare module 'react-select/dist/declarations/src/Select' {
-  export interface Props<
-    Option,
-    IsMulti extends boolean,
-    Group extends GroupBase<Option>
-  > {
-    label?: string;
-    onSelect?: Function;
-  }
+interface SelectOption {
+  value: number | string;
+  label: string;
+  onSelect?: () => void;
+}
+
+/**
+ * Utility Functions
+ */
+
+function onCloser(
+  index: number,
+  onChange: (result: PropsValue<SelectOption>) => void,
+  selected?: PropsValue<SelectOption>
+): MouseEventHandler<HTMLDivElement> {
+  return () => {
+    if (!selected || !Array.isArray(selected)) return;
+    const result = selected.filter((_, index_) => index_ !== index);
+    onChange(result);
+  };
+}
+
+const filterOptions = (
+  options: PropsValue<SelectOption>,
+  selected: PropsValue<SelectOption>,
+  isMulti: boolean
+): OptionsOrGroups<SelectOption, GroupBase<SelectOption>> => {
+  if (!selected || !options)
+    return options as OptionsOrGroups<SelectOption, GroupBase<SelectOption>>;
+
+  if (isMulti)
+    return (options as SelectOption[]).filter(
+      o => !(selected as SelectOption[]).map(s => s.value).includes(o.value)
+    );
+
+  return options as OptionsOrGroups<SelectOption, GroupBase<SelectOption>>;
+};
+
+/**
+ * Supporting Components
+ */
+
+interface PillProperties {
+  value: string;
+  onClose: MouseEventHandler<HTMLDivElement>;
+  onDelete?: KeyboardEventHandler<HTMLDivElement>;
+}
+const Pill = ({ value, onClose, onDelete }: PillProperties): JSX.Element => (
+  <div
+    tabIndex={0}
+    role='button'
+    className='pill'
+    onClick={onClose}
+    onKeyUp={onDelete}
+  >
+    <Label htmlFor={value} inline>
+      {value}
+      <div>
+        <Icon name='error' />
+      </div>
+    </Label>
+  </div>
+);
+
+interface PillsProperties {
+  selected: PropsValue<SelectOption>;
+  isMulti: boolean;
+  onChange: (event: PropsValue<SelectOption>) => void;
+}
+const Pills = ({
+  selected,
+  isMulti,
+  onChange
+}: PillsProperties): JSX.Element | null => {
+  if (
+    !isMulti ||
+    !selected ||
+    (Array.isArray(selected) && selected.length === 0)
+  )
+    return null;
+  return (
+    <div className='o-multiselect_choices pills'>
+      {Array.isArray(selected) &&
+        selected.map(({ value, label }: SelectOption, index: number) => (
+          <Pill
+            key={value}
+            value={label}
+            onClose={onCloser(index, onChange, selected)}
+          />
+        ))}
+    </div>
+  );
+};
+
+interface DropdownProperties {
+  isMulti: boolean;
+  options: SelectOption[];
+  defaultValue?: SelectOption;
+  id: string;
+  label?: string;
+  onSelect: (event: OnChangeValue<SelectOption, boolean>) => void;
 }
 
 /**
  * A dropdown input component that supports multi-select
  * @returns JSX.Element
  */
-export function Dropdown<
-  Option,
-  IsMulti extends boolean = false,
-  Group extends GroupBase<Option> = GroupBase<Option>
->(props: Props<Option, IsMulti, Group>) {
-  const {
-    isMulti = false,
-    options,
-    defaultValue,
-    id = 'dropdown',
-    label = null,
-    onSelect = (e: OnChangeValue<Option, IsMulti> | null) => null
-  } = props;
-
-  const [selected, setSelected] = useState<
-    Option | MultiValue<Option> | null | undefined
-  >(defaultValue);
+export function Dropdown({
+  isMulti = false,
+  options,
+  defaultValue,
+  id = 'dropdown',
+  label = '',
+  onSelect,
+  ...rest
+}: DropdownProperties): JSX.Element {
+  const [selected, setSelected] = useState<PropsValue<SelectOption>>(
+    defaultValue ? [defaultValue] : []
+  );
 
   // Store updated list of selected items
-  const onChange = (option): void => {
-    onSelect && onSelect(option);
+  const onChange = (option: PropsValue<SelectOption>): void => {
+    onSelect(option);
     setSelected(option);
   };
 
   return (
     <div className='m-form-field m-form-field__select'>
-      {label && <Label htmlFor={id}>{label}</Label>}
+      {!!label && <Label htmlFor={id}>{label}</Label>}
       <Pills selected={selected} isMulti={isMulti} onChange={onChange} />
       <Select
-        {...props}
+        isMulti={isMulti}
         className='o-multiselect'
         value={selected}
         options={filterOptions(options, selected, isMulti)}
         onChange={onChange}
         filterOption={createFilter({ ignoreAccents: false })}
         controlShouldRenderValue={!isMulti}
+        {...rest}
       />
     </div>
   );
 }
-
-/**
- * Supporting Components
- */
-
-interface PillsProps {
-  selected: typeof Option | MultiValue<typeof Option> | null | undefined;
-  isMulti: boolean;
-  onChange: Function;
-}
-const Pills = ({ selected, isMulti, onChange }: PillsProps) => {
-  if (!isMulti || !selected || !selected.length) return null;
-  return (
-    <ul className='o-multiselect_choices pills'>
-      {selected.map((v, i: number) => {
-        return (
-          <Pill
-            key={i}
-            value={v.label}
-            close={makeCloser(selected, i, onChange)}
-          />
-        );
-      })}
-    </ul>
-  );
-};
-
-interface PillProps {
-  value: string;
-  close: MouseEventHandler<HTMLLIElement>;
-}
-const Pill = ({ value, close }: PillProps) => {
-  return (
-    <li className='pill' onClick={close}>
-      <Label htmlFor={value} inline>
-        {value}
-        <div>
-          <Icon name='error' />
-        </div>
-      </Label>
-    </li>
-  );
-};
-
-/**
- * Utility Functions
- */
-
-function makeCloser(
-  selected,
-  index: number,
-  onChange: Function
-): MouseEventHandler<HTMLLIElement> {
-  return function () {
-    if (!selected) return;
-    const result: Array<any> = [];
-    const length = selected?.length || 0;
-    for (let i = 0; i < length; i++) {
-      if (i !== index) result.push(selected[i]);
-    }
-    onChange(result);
-  };
-}
-
-const filterOptions = (options, selected, isMulti: boolean) => {
-  if (!selected || !options) return options;
-
-  if (isMulti) return options.filter(o => !selected.includes(o.value));
-
-  return options;
-};
 
 export default Dropdown;
