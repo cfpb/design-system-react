8a5d1c1debd27556682c5103df6a5386e0fa065e
Chris Contolini <contolini@users.noreply.github.com>
2023-04-10 13:30:49 -0400

    Set up build and publishing tooling
    Configures vite and typescript to export components into a structure
    that can be published to npm and imported into other projects.

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import '@cfpb/cfpb-design-system/src/cfpb-design-system.less';
+
+export { Button } from './components/Button';
+export { Checkbox } from './components/Checkbox';
+export { Icon } from './components/Icon';
+export { Label } from './components/Label';
+export { Notification } from './components/Notification';
+export { Pagination } from './components/Pagination';
+export { Radio } from './components/Radio';
+export { TextInput } from './components/TextInput';
--- vite.config.ts
+++ vite.config.ts
@@ -1,59 +1,41 @@
-/// <reference types="vitest" />
-import eslintPlugin from '@nabla/vite-plugin-eslint'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import eslintPlugin from '@nabla/vite-plugin-eslint';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { name } from './package.json';
 
-export default defineConfig(({ mode }) => ({
-	test: {
-		css: false,
-		include: ['src/**/__tests__/*'],
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: 'src/setupTests.ts',
-		clearMocks: true,
-		coverage: {
-			provider: 'istanbul',
-			enabled: true,
-			'100': true,
-			reporter: ['text', 'lcov'],
-			reportsDirectory: 'coverage'
-		}
-	},
-	plugins: [
-		tsconfigPaths(),
-		react(),
-		...(mode === 'test'
-			? []
-			: [
-					eslintPlugin(),
-					VitePWA({
-						registerType: 'autoUpdate',
-						includeAssets: [
-							'favicon.png',
-							'robots.txt',
-							'apple-touch-icon.png',
-							'icons/*.svg',
-							'fonts/*.woff2'
-						],
-						manifest: {
-							theme_color: '#BD34FE',
-							icons: [
-								{
-									src: '/android-chrome-192x192.png',
-									sizes: '192x192',
-									type: 'image/png',
-									purpose: 'any maskable'
-								},
-								{
-									src: '/android-chrome-512x512.png',
-									sizes: '512x512',
-									type: 'image/png'
-								}
-							]
-						}
-					})
-			  ])
-	]
-}))
+export default defineConfig(() => ({
+  plugins: [
+    eslintPlugin(),
+    react(),
+    tsConfigPaths(),
+    dts({
+      insertTypesEntry: true
+    })
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'index.ts'),
+      name,
+      formats: ['es', 'cjs'],
+      fileName: (format): string => `${name}.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
+    optimizeDeps: {
+      exclude: ['react']
+    },
+    esbuild: {
+      minify: true
+    }
+  }
+}));
