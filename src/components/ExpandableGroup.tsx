b474fa0bb6c1c95f1a7e25dfee222b3a137f1108
James L <jlivolsi@teamraft.com>
2023-04-10 12:29:31 -0400

    fix: typings

--- src/components/ExpandableGroup.tsx
+++ src/components/ExpandableGroup.tsx
@@ -1,18 +1,24 @@
-import React, { ReactNode, useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 
-interface ExpandableProps {
+import type { ExpandableProps } from './Expandable';
+
+interface ExpandableGroupProps {
   accordion?: boolean;
-  children: ReactNode;
+  children: ReactElement<ExpandableProps>[];
 }
 
-const ExpandableGroup: React.FC<ExpandableProps> = ({
+const ExpandableGroup: React.FC<ExpandableGroupProps> = ({
   accordion = false,
   children
 }) => {
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(undefined);
   const childrenWithProps = React.Children.map(children, (child, index) => {
     if (React.isValidElement(child) && accordion) {
-      return React.cloneElement(child, { index, activeIndex, setActiveIndex });
+      return React.cloneElement(child, {
+        index,
+        activeIndex,
+        setActiveIndex
+      } as Pick<ExpandableProps, 'index' | 'activeIndex' | 'setActiveIndex'>);
     }
     return child;
   });
