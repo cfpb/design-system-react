b93b5293c40c6193241f77a0625b9a1945d538ce
Meis <meissadia@gmail.com>
2023-04-11 12:27:41 -0600

    feat: [Dropdown] Apply CFPB DS styles for :hover and :focus

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -8,6 +8,8 @@
 import type { MouseEventHandler } from 'react';
 import { useState } from 'react';
 import type {
+  CSSObjectWithLabel,
+  ControlProps,
   GroupBase,
   OnChangeValue,
   OptionsOrGroups,
@@ -22,6 +24,25 @@ interface SelectOption {
   label: string;
   onSelect?: () => void;
 }
+
+const extendedSelectStyles = {
+  control: (
+    base: CSSObjectWithLabel,
+    state: ControlProps<SelectOption, boolean, GroupBase<SelectOption>>
+  ): CSSObjectWithLabel => ({
+    ...base,
+    borderColor: state.isFocused ? '#0072ce' : base.borderColor,
+    outline: state.isFocused ? '1px dotted #0072ce !important' : base.outline,
+    outlineOffset: state.isFocused ? '3px' : base.outlineOffset,
+    '&:hover': {
+      borderColor: '#0072ce',
+      outline: state.isFocused
+        ? '1px dotted #0072ce !important'
+        : '1px solid #0072ce !important',
+      outlineOffset: state.isFocused ? '3px' : '0'
+    }
+  })
+};
 
 /**
  * Utility Functions
@@ -165,6 +186,7 @@ export function Dropdown({
         filterOption={createFilter({ ignoreAccents: false })}
         controlShouldRenderValue={!isMulti}
         closeMenuOnSelect={!isMulti}
+        styles={extendedSelectStyles}
         {...rest}
       />
     </div>
