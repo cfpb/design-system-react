a89a0154e8da801e0324ae9cee01e09d53199742
James Vincent <121906052+the-raft-oar@users.noreply.github.com>
2023-04-11 12:33:21 -0400

    Fix type castings (#19)
    * Add Expandable and Expandable.stories files
    
    * Add plus & minus icons, add in-line styling, change paragraphText
    
    * Update src/components/Expandable.tsx
    
    * Update src/assets/icons/minus-round.svg
    
    * add Accordian expandable
    
    * Add expandable group
    
    * Update src/components/ExpandableGroup.tsx
    
    * Update src/components/ExpandableGroup.tsx
    
    * fix: misspelling
    
    * fix: typings
    
    * fix: export ExpandableGroupProps
    
    ---------
    
    Co-authored-by: Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>

--- src/components/ExpandableGroup.tsx
+++ src/components/ExpandableGroup.tsx
@@ -1,18 +1,23 @@
-import React, { ReactNode, useState } from 'react';
+import React, { ReactElement, useState } from 'react';
+import type { ExpandableProps } from './Expandable';
 
-interface ExpandableProps {
+export interface ExpandableGroupProps {
   accordion?: boolean;
-  children: ReactNode;
+  children: ReactElement<ExpandableProps>[];
 }
 
-const ExpandableGroup: React.FC<ExpandableProps> = ({
+const ExpandableGroup: React.FC<ExpandableGroupProps> = ({
   accordion = false,
   children
 }) => {
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(undefined);
   const childrenWithProps = React.Children.map(children, (child, index) => {
     if (React.isValidElement(child) && accordion) {
-      return React.cloneElement(child, { index, activeIndex, setActiveIndex });
+      return React.cloneElement(child, {
+        index,
+        activeIndex,
+        setActiveIndex
+      } as Pick<ExpandableProps, 'index' | 'activeIndex' | 'setActiveIndex'>);
     }
     return child;
   });
