e4fa48a548f1f530d3bfa40aa9ea046bbd5a0794
James L <jlivolsi@teamraft.com>
2023-05-09 19:09:26 -0400

    feat: add storybook interactions for e2e testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,21 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y', '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/react-vite',
     options: {}
   },
   features: {
+    interactionsDebugger: true,
     storyStoreV7: true
   },
-  async viteFinal(config, { configType }) {
+  async viteFinal(config, {
+    configType
+  }) {
     config.base = process.env.BASE_PATH || config.base;
 
     // Skip type declaration generation for non-dist builds
-    config.plugins = config.plugins.filter(
-      plugin => plugin.name !== 'vite:dts'
-    );
+    config.plugins = config.plugins.filter(plugin => plugin.name !== 'vite:dts');
 
     // return the customized config
     return config;
