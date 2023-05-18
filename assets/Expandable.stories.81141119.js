0d183ba9e756e7c9b28c6139fec4df572dde7b15
meissadia <meissadia@gmail.com>
2023-05-18 17:37:48 +0000

    Deploying to gh-pages from @ cfpb/design-stories@4c5b1f6c4d78d965715039f53557a6f83a1b8f1c 🚀

---
+++ assets/Expandable.stories.81141119.js
@@ -0,0 +1,17 @@
+import{E as d}from"./Expandable.42bfb6a8.js";import{a as i,F as m,j as c}from"./jsx-runtime.556016cd.js";import"./Icon.d0366cc7.js";import"./index.cfa9e344.js";import"./iframe.252b8116.js";import"../sb-preview/runtime.mjs";import"./_commonjsHelpers.712cc82f.js";const O={component:d,parameters:{docs:{description:{component:`
+### CFPB DS - Expandable component
+
+Source: https://cfpb.github.io/design-system/components/expandables
+`}}}},u=i(m,{children:["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ipsa voluptatibus soluta nobis unde quisquam temporibus magnam debitis quidem. Ducimus ratione corporis nesciunt earum vel est quaerat blanditiis dolore ipsa?\xA0",c("a",{href:"/?path=/story/components-expandable--default",children:"Lorem link"})]}),e={args:{header:"Expandable Header",children:u}},a={args:{...e.args,header:"Expandable Header",openOnLoad:!0}};var r,o,n;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    header: 'Expandable Header',
+    children: Content
+  }
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var s,t,p;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    header: 'Expandable Header',
+    openOnLoad: true
+  }
+}`,...(p=(t=a.parameters)==null?void 0:t.docs)==null?void 0:p.source}}};const L=["Default","OpenOnLoad"];export{e as Default,a as OpenOnLoad,L as __namedExportsOrder,O as default};
+//# sourceMappingURL=Expandable.stories.81141119.js.map
---
+++ assets/Label.stories.6583a416.js
@@ -0,0 +1,15 @@
+import{L as p}from"./Label.99995e29.js";import{T as m}from"./TextInput.2ad97be4.js";import{j as e,F as n,a as i}from"./jsx-runtime.556016cd.js";import"./index.cfa9e344.js";import"./Icon.d0366cc7.js";import"./iframe.252b8116.js";import"../sb-preview/runtime.mjs";import"./_commonjsHelpers.712cc82f.js";const I={component:p,argTypes:{inline:{control:"boolean"}},parameters:{docs:{description:{component:`
+### CFPB DS - Label component
+
+Source: https://cfpb.github.io/design-system/components/expandables
+`}}}},t={args:{children:e(n,{children:"Text input label"}),htmlFor:"testInput"},render:s=>i(n,{children:[e(p,{...s,htmlFor:"testInput"}),e(m,{id:"testInput",name:"testInput",type:"text",placeholder:"Example input"})]})};var r,a,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: <>Text input label</>,
+    htmlFor: 'testInput'
+  },
+  render: arguments_ => <>
+      <Label {...arguments_} htmlFor='testInput' />
+      <TextInput id='testInput' name='testInput' type='text' placeholder='Example input' />
+    </>
+}`,...(o=(a=t.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const F=["LabelHeading"];export{t as LabelHeading,F as __namedExportsOrder,I as default};
+//# sourceMappingURL=Label.stories.6583a416.js.map
