4ca9c39bca7dbf10f5fb4177ff8dd0836f9cc0a0
Meis <meissadia@gmail.com>
2023-05-18 11:36:55 -0600

    [Test] Notification (#53)
    * fix: cleanup eslint errors
    
    * test: <Notification />
    
    ---------
    
    Co-authored-by: Chris Contolini <contolini@users.noreply.github.com>

---
+++ src/components/Notification.test.tsx
@@ -0,0 +1,68 @@
+import '@testing-library/jest-dom';
+import { render, screen, within } from '@testing-library/react';
+import { Notification } from './Notification';
+
+const testType = (type: string) => async (): Promise<void> => {
+  render(<Notification type={type} />);
+  const element = screen.getByTestId('notification');
+  expect(element).toHaveClass(`m-notification__${type}`);
+
+  // Renders Icon
+  const icon = await within(element).findByRole('img');
+  expect(icon).toBeInTheDocument();
+};
+
+const notificationTypes = ['success', 'warning', 'error', 'info'];
+
+describe('<Notification />', () => {
+  for (const type of notificationTypes) {
+    it(`renders notification of type "${type}"`, testType(type));
+  }
+
+  it('displays message when provided', () => {
+    render(<Notification type='info' />);
+    const noMessage = screen.queryByTestId('message');
+    expect(noMessage).not.toBeInTheDocument();
+
+    render(<Notification type='info' message='testing' />);
+    const message = screen.queryByTestId('message');
+    expect(message).toBeInTheDocument();
+  });
+
+  it('displays explaination when provided', () => {
+    render(<Notification type='info' />);
+    const noExplaination = screen.queryByTestId('explaination');
+    expect(noExplaination).not.toBeInTheDocument();
+
+    render(<Notification type='info'>Explaination</Notification>);
+    const explaination = screen.queryByTestId('explaination');
+    expect(explaination).toBeInTheDocument();
+  });
+
+  it('displays links when provided', async () => {
+    render(<Notification type='info' />);
+    const noLinks = screen.queryAllByRole('listitem');
+    expect(noLinks.length).toBe(0);
+
+    const linkItems = [
+      { href: '/1', label: 'one' },
+      { href: '/2', label: 'two', isExternal: true }
+    ];
+
+    render(<Notification type='info' links={linkItems} />);
+    const links = screen.queryAllByRole('listitem');
+    expect(links.length).toBe(2);
+
+    // Link attributes are correctly propagated
+    const linkOne = screen.getByText(linkItems[0].label);
+    expect(linkOne).toHaveAttribute('href', '/1');
+
+    const linkTwo = screen.getByText(linkItems[1].label);
+    expect(linkTwo).toHaveAttribute('href', '/2');
+
+    // Icon is displayed: External link
+    const externalIcon = await within(linkTwo).findByRole('img');
+    expect(externalIcon).toBeInTheDocument();
+    expect(externalIcon).toHaveClass('cf-icon-svg__external-link');
+  });
+});
