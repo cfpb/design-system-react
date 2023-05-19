f07611385fd46fa0ff37c6115f8b4162a30b35de
James L <jlivolsi@teamraft.com>
2023-05-19 15:41:20 -0400

    feat: add interaction tests for expandable group

--- src/components/ExpandableGroup.stories.tsx
+++ src/components/ExpandableGroup.stories.tsx
@@ -1,6 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { userEvent, waitFor } from '@storybook/testing-library';
+import { within } from '@testing-library/react';
 import Expandable from './Expandable';
 import { ExpandableGroup } from './ExpandableGroup';
+
+import { expect } from '@storybook/jest';
 
 const meta: Meta<typeof ExpandableGroup> = {
   component: ExpandableGroup,
@@ -25,32 +29,60 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const Content = ({ accordion }: { accordion: boolean | undefined }) => {
-  const linkPath = `/?path=/story/components-expandablegroup--${accordion ? 'accordion' : 'default'}`
+  const linkPath = `/?path=/story/components-expandablegroup--${
+    accordion ? 'accordion' : 'default'
+  }`;
 
-  return <div>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ipsa
-    voluptatibus soluta nobis unde quisquam temporibus magnam debitis
-    quidem. Ducimus ratione corporis nesciunt earum vel est quaerat
-    blanditiis dolore ipsa?&nbsp;
-    <a href={linkPath}>
-      Lorem link
-    </a>
-  </div>
-}
-
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ipsa
+      voluptatibus soluta nobis unde quisquam temporibus magnam debitis quidem.
+      Ducimus ratione corporis nesciunt earum vel est quaerat blanditiis dolore
+      ipsa?&nbsp;
+      <a href={linkPath}>Lorem link</a>
+    </div>
+  );
+};
 
 export const Default: Story = {
-  render: args => <ExpandableGroup {...args}>
-    {[1, 2, 3].map(val =>
-      <Expandable header={`Expandable ${val}`} inAccordion={args.accordion}>
-        <Content accordion={args.accordion} />
-      </Expandable>
-    )}
-  </ExpandableGroup>,
+  render: args => (
+    <ExpandableGroup {...args}>
+      {[1, 2, 3].map(val => (
+        <Expandable header={`Expandable ${val}`} inAccordion={args.accordion}>
+          <Content accordion={args.accordion} />
+        </Expandable>
+      ))}
+    </ExpandableGroup>
+  ),
+  play: async ({ args, canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    const el = await canvas.findByTitle('Expandable 1');
+
+    await step('Expand tile - Aria Expanded is true', async () => {
+      await userEvent.click(el);
+      waitFor(
+        () => {
+          expect(el.ariaExpanded).toBe('true');
+        },
+        { timeout: 1000 }
+      );
+    });
+
+    // await step('Collapse tile - Aria Expanded is false', async () => {
+    //   await userEvent.click(el);
+    //   waitFor(
+    //     () => {
+    //       expect(el.ariaExpanded).toBe('false');
+    //     },
+    //     { timeout: 1000 }
+    //   );
+    // });
+  },
   args: {
     groupId: 'DefaultGroup'
   }
-}
+};
 
 export const Accordion: Story = {
   ...Default,
@@ -58,5 +90,4 @@ export const Accordion: Story = {
     accordion: true,
     groupId: 'AccordionGroup'
   }
-}
-
+};
