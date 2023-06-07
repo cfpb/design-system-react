7d2ff922c99a37a2577fb8f73b0fa173d3bc84ae
Chris Contolini <contolini@users.noreply.github.com>
2023-06-07 10:56:02 -0700

    Replace auto-docs with example free-form MDX documentation page (#73)
    Storybook's autodocs pages lack a lot of the information about our
    components (rendered states, guidelines, a11y tips). To show how
    MDX docs work, I created an overview page for the Checkbox component
    and copied over the content from https://cfpb.github.io/design-system/components/checkboxes
    
    MDX supports any flavor of markdown so we can add whatever we'd like to the
    docs pages if we decide to go this route.
    
    Closes https://github.com/cfpb/design-stories/issues/68

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -23,8 +23,5 @@ module.exports = {
 
     // return the customized config
     return config;
-  },
-  docs: {
-    autodocs: true
   }
 };
