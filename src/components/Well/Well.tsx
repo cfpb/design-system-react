da66d9d07176211c906d54153b0c4fd7b526a21f
Meis <meissadia@gmail.com>
2023-06-15 14:57:26 -0600

    feat: Well component

---
+++ src/components/Well/Well.tsx
@@ -0,0 +1,32 @@
+import type { HeadingLevel } from '../../types/headingLevel';
+import List from '../List/List';
+import ListItem from '../List/ListItem';
+import './Well.less';
+
+interface WellProperties {
+  heading: string;
+  text: JSX.Element | string;
+  links?: JSX.Element[];
+  headingLevel: HeadingLevel;
+}
+
+export default function Well({
+  heading,
+  headingLevel = 'h4',
+  links,
+  text
+}: WellProperties): JSX.Element {
+  const callsToAction = [];
+  if (links)
+    for (const link of links) {
+      callsToAction.push(<ListItem>{link}</ListItem>);
+    }
+
+  return (
+    <div className='o-well'>
+      <p className={headingLevel}>{heading}</p>
+      <p className='text'>{text}</p>
+      {callsToAction.length > 0 ? <List isLinks>{callsToAction}</List> : null}
+    </div>
+  );
+}
