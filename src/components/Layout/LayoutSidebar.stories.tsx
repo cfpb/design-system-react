caa0e4f40596933ab13ee424d5cb6eb62c7e24da
Chris Contolini <contolini@users.noreply.github.com>
2023-06-19 22:36:03 -0700

    fix: Rename Layout component entrypoint file

--- src/components/Layout/LayoutSidebar.stories.tsx
+++ src/components/Layout/LayoutSidebar.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Layout from '.';
+import Layout from './Layout';
 
 const meta: Meta<typeof Layout.Sidebar> = {
   component: Layout.Sidebar,
