b11f2558798d676da92787bb8a13446ae39f919b
contolini <contolini@users.noreply.github.com>
2023-06-21 16:45:31 +0000

    Deploying to gh-pages from @ cfpb/design-stories@1341996ab0854ab937dda0926cd8123368d2ea8c 🚀

---
+++ assets/Well.stories.b5849d4c.js
@@ -0,0 +1,25 @@
+import{a as x,L as q}from"./ListItem.c68c12b4.js";import{j as e,a as p,F as h}from"./jsx-runtime.3c5b5edf.js";import"./index.cfa9e344.js";import"./_commonjsHelpers.712cc82f.js";function r({heading:n,headingLevel:f="h4",links:s,text:g}){const a=[];if(s)for(const b of s)a.push(e(x,{children:b}));return p("div",{className:"o-well",children:[e("p",{className:f,children:n}),e("p",{className:"text",children:g}),a.length>0?e(q,{isLinks:!0,children:a}):null]})}try{r.displayName="Well",r.__docgenInfo={description:"",displayName:"Well",props:{heading:{defaultValue:null,description:"",name:"heading",required:!0,type:{name:"string"}},text:{defaultValue:null,description:"",name:"text",required:!0,type:{name:"string | Element"}},links:{defaultValue:null,description:"",name:"links",required:!1,type:{name:"Element[]"}},headingLevel:{defaultValue:{value:"h4"},description:"",name:"headingLevel",required:!1,type:{name:"enum",value:[{value:'"h1"'},{value:'"h2"'},{value:'"h3"'},{value:'"h4"'},{value:'"h5"'},{value:'"h6"'}]}}}}}catch{}const _={component:r,argTypes:{}},i={args:{heading:"Well title",text:p(h,{children:["Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae dolor distinctio tenetur quibusdam rem debitis, voluptate nesciunt culpa officiis quos perspiciatis nostrum illo libero autem beatae temporibus ratione reprehenderit? ",e("a",{href:"#",children:"Example link"}),"."]})}},t={args:{heading:"Well title",text:e(h,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae dolor distinctio tenetur quibusdam rem debitis, voluptate nesciunt culpa officiis quos perspiciatis nostrum illo libero autem beatae temporibus ratione reprehenderit?"}),links:[e("a",{href:"#",children:"Example link"},"example1")]}};var l,o,u;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    heading: 'Well title',
+    text: <>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae dolor
+        distinctio tenetur quibusdam rem debitis, voluptate nesciunt culpa
+        officiis quos perspiciatis nostrum illo libero autem beatae temporibus
+        ratione reprehenderit? <a href='#'>Example link</a>.
+      </>
+  }
+}`,...(u=(o=i.parameters)==null?void 0:o.docs)==null?void 0:u.source}}};var c,d,m;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    heading: 'Well title',
+    text: <>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae dolor
+        distinctio tenetur quibusdam rem debitis, voluptate nesciunt culpa
+        officiis quos perspiciatis nostrum illo libero autem beatae temporibus
+        ratione reprehenderit?
+      </>,
+    links: [<a href='#' key='example1'>
+        Example link
+      </a>]
+  }
+}`,...(m=(d=t.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const A=["Standard","CallToAction"];export{t as CallToAction,i as Standard,A as __namedExportsOrder,_ as default};
+//# sourceMappingURL=Well.stories.b5849d4c.js.map
