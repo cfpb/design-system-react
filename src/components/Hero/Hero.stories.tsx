8cb5831dc483f81e5e6dd39d12ee9145f55b45a5
Meis <meissadia@gmail.com>
2023-06-22 14:41:38 -0600

    feat: Hero component v2 w/ image improvements

--- src/components/Hero/Hero.stories.tsx
+++ src/components/Hero/Hero.stories.tsx
@@ -22,39 +22,59 @@ type Story = StoryObj<typeof meta>;
 
 export const WithIllustration: Story = {
   args: {
-    heading: 'Hero Heading',
-    image: 'images/credit-card-hero.png',
+    heading: '41 chars max for a one-line heading',
+    image:
+      'https://cfpb.github.io/design-system/images/uploads/hero_illustration_example_keys.png',
     subheading:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae dolor distinctio tenetur quibusdam rem debitis, voluptate nesciunt culpa officiis quos perspiciatis nostrum illo libero autem beatae temporibus ratione reprehenderit?',
-    backgroundColor: 'lightblue',
-    textColor: undefined
+      'This text has a recommended count of 165-186 characters (three lines at 1230px) following a one-line heading and 108-124 characters (two lines at 1230px) following a two-line heading.',
+    backgroundColor: '#d4e7e6'
   }
 };
 
 export const WithPhoto: Story = {
   args: {
     ...WithIllustration.args,
-    imageIsPhoto: true
-  }
-};
-export const Knockout: Story = {
-  args: {
-    ...WithIllustration.args,
-    backgroundColor: 'teal',
-    knockout: true
+    imageIsPhoto: true,
+    image: 'https://files.consumerfinance.gov/f/images/PC_hero.original.jpg',
+    backgroundColor: '#f7f8f9'
   }
 };
 
 export const Jumbo: Story = {
   args: {
     ...WithIllustration.args,
-    isJumbo: true
+    heading: '50 chars max for a two-line heading',
+    subheading:
+      'This text has a recommended count of 140-175 characters (four lines at 1230px) following a two- or three-line heading.',
+    isJumbo: true,
+    image:
+      'https://cfpb.github.io/design-system/images/uploads/jumbo_hero_image_wide.jpeg',
+    backgroundColor: 'rgb(168,213,200	)'
   }
 };
 
 export const FiftyFifty: Story = {
   args: {
     ...WithIllustration.args,
-    is5050: true
+    heading: '50 chars max for a two-line heading',
+    subheading:
+      'This text has a recommended count of 140-175 characters (four lines at 1230px) following a two- or three-line heading.',
+    is5050: true,
+    image:
+      'https://cfpb.github.io/design-system/images/uploads/jumbo-hero-image.png',
+    backgroundColor: 'white'
+  }
+};
+
+export const Knockout: Story = {
+  args: {
+    ...WithIllustration.args,
+    heading: 'Max of 41 chars for a one-line heading',
+    subheading:
+      'This text has a recommended count of 165-186 characters (three lines at 1230px) following a one-line heading and 108-124 characters (two lines at 1230px) following a two-line heading.',
+    backgroundColor: '#207676',
+    isKnockout: true,
+    image:
+      'https://cfpb.github.io/design-system/images/uploads/design_system_hero_example.png'
   }
 };
