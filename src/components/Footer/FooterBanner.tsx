ed5a99a829e8f94412ce6c104d31faa43701879c
Meis <meissadia@gmail.com>
2023-06-23 13:03:00 -0600

    feat: Supporting components for <Footer>

---
+++ src/components/Footer/FooterBanner.tsx
@@ -0,0 +1,14 @@
+export const FooterBanner = (): JSX.Element => (
+  <div className='o-footer-post'>
+    <div
+      className='a-tagline a-tagline__large'
+      aria-label='Official website of the United States government'
+    >
+      <span className='u-usa-flag' />
+      <div className='a-tagline_text'>
+        An official website of the&nbsp;
+        <span className='u-nowrap'>United States government</span>
+      </div>
+    </div>
+  </div>
+);
