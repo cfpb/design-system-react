14439e553fecc4b92b9d885557ad91eccb6a55d5
Meis <meissadia@gmail.com>
2023-06-28 14:13:37 -0600

    feat: Link - Support styling any link with the list links  dotted bottom border - cleanup classnames

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,13 +1,21 @@
+import classnames from 'classnames';
 import type { JSXElement } from '../../types/jsxElement';
+import './Link.less';
+
+interface LinkProperties {
+  type?: 'default' | 'list';
+}
 
 export default function Link({
   children,
+  type = 'default',
   ...others
-}: React.HTMLProps<HTMLAnchorElement>): JSXElement {
-  const cname = `a-link ${others.className ?? ''}`;
+}: LinkProperties & React.HTMLProps<HTMLAnchorElement>): JSXElement {
+  const cname = ['a-link', others.className];
+  if (type === 'list') cname.push('list-link');
 
   return (
-    <a {...others} className={cname}>
+    <a {...others} className={classnames(cname)}>
       {children}
     </a>
   );
