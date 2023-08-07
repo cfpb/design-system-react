e276435c3c7a12a30a12b14e5f655b336827efe1
Meis <meissadia@gmail.com>
2023-08-07 12:31:56 -0600

    chore: [Dropdown] Extract utils to shared file

--- src/components/Dropdown/utils.tsx
+++ src/components/Dropdown/utils.tsx
@@ -1,3 +1,6 @@
+import type { GroupBase, OptionsOrGroups, PropsValue } from 'react-select';
+import type { SelectOption } from './Dropdown.types';
+
 export const MockOptions = [
   { value: 'value1', label: 'Option A' },
   { value: 'value2', label: 'Option B' },
@@ -8,3 +11,33 @@ export const MockOptions = [
       'Options can also contain long words that will be wrapped like supercalifragilisticexpialidocious'
   }
 ];
+
+/**
+ * For multi-select, hides already selected options.
+ *
+ * @param options Available options
+ * @param selected Selected options
+ * @param isMulti Is a multi-select component?
+ * @param showAllOptions Force all options to be displayed for selection
+ * @returns A list of selectable options
+ */
+export const filterOptions = (
+  options: PropsValue<SelectOption>,
+  selected: PropsValue<SelectOption>,
+  isMulti: boolean,
+  showAllOptions: boolean
+): OptionsOrGroups<SelectOption, GroupBase<SelectOption>> => {
+  if (showAllOptions || !selected || !isMulti)
+    return options as OptionsOrGroups<SelectOption, GroupBase<SelectOption>>;
+
+  return (options as SelectOption[]).filter(
+    o => !(selected as SelectOption[]).map(s => s.value).includes(o.value)
+  );
+};
+
+// Make it easier for the user to delete/edit search text by highlighting all input text onFocus
+export const onSelectInputFocus = (
+  event: React.ChangeEvent<HTMLInputElement>
+): void => {
+  event.target.select();
+};
