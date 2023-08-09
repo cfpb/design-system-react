ad701e23b2194aaf2f14ca012c59587fafc75899
meissadia <meissadia@gmail.com>
2023-08-09 20:43:33 +0000

    Deploying to gh-pages from @ cfpb/design-stories@e00dd66cda35caccfdd2989df8dfc546d732c161 🚀

---
+++ assets/Layout.37727e47.js
@@ -0,0 +1,2 @@
+import{i as a,j as t,k as o,l as r}from"./Well.30a54e15.js";const n={Content:a,Main:t,Sidebar:o,Wrapper:r};export{n as L};
+//# sourceMappingURL=Layout.37727e47.js.map
---
+++ assets/TextInput.stories.ade15179.js
@@ -0,0 +1,45 @@
+import{T as S}from"./Well.30a54e15.js";import"./Dropdown.c78429fd.js";import"./ExpandableGroup.1932ee91.js";import"./Icon.95041256.js";import"./jsx-runtime.5f84dc07.js";import"./extends.cc011e2f.js";import"./floating-ui.dom.esm.1a703617.js";import"./index.40d0d0b8.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.9a795824.js";import"../sb-preview/runtime.js";const C={component:S,argTypes:{width:{control:"select"},isDisabled:{control:"boolean"}},parameters:{docs:{description:{component:`
+### CFPB DS - TextInput component
+
+Source: https://cfpb.github.io/design-system/components/text-inputs
+`}}}},e={args:{id:"Default",name:"Default",placeholder:"Placeholder text",width:"default",type:"text"}},r={args:{...e.args,id:"FullWidth",name:"FullWidth",width:"full"}},s={args:{...e.args,id:"SuccessType",name:"SuccessType",notificationType:"success",textNotification:"This is a success message"}},a={args:{...e.args,id:"WarningType",name:"WarningType",notificationType:"warning",textNotification:"This is a warning message"}},t={args:{...e.args,id:"ErrorType",name:"ErrorType",notificationType:"error",textNotification:"This is an error message"}};var n,o,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    id: 'Default',
+    name: 'Default',
+    placeholder: 'Placeholder text',
+    width: 'default',
+    type: 'text'
+  }
+}`,...(i=(o=e.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};var c,p,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    id: 'FullWidth',
+    name: 'FullWidth',
+    width: 'full'
+  }
+}`,...(m=(p=r.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var u,d,l;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    id: 'SuccessType',
+    name: 'SuccessType',
+    notificationType: 'success',
+    textNotification: 'This is a success message'
+  }
+}`,...(l=(d=s.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var g,T,f;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    id: 'WarningType',
+    name: 'WarningType',
+    notificationType: 'warning',
+    textNotification: 'This is a warning message'
+  }
+}`,...(f=(T=a.parameters)==null?void 0:T.docs)==null?void 0:f.source}}};var y,h,x;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    id: 'ErrorType',
+    name: 'ErrorType',
+    notificationType: 'error',
+    textNotification: 'This is an error message'
+  }
+}`,...(x=(h=t.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};const O=["Default","FullWidth","SuccessType","WarningType","ErrorType"];export{e as Default,t as ErrorType,r as FullWidth,s as SuccessType,a as WarningType,O as __namedExportsOrder,C as default};
+//# sourceMappingURL=TextInput.stories.ade15179.js.map
