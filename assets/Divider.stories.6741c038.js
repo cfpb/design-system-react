0a984812dc7dcb40001d07b187f6044d09918087
meissadia <meissadia@gmail.com>
2023-08-09 20:47:21 +0000

    Deploying to gh-pages from @ cfpb/design-stories@baebc067d1e4cdce2fbd4ac84e7fb93bebfa27aa 🚀

---
+++ assets/Divider.stories.6741c038.js
@@ -0,0 +1,15 @@
+import{D as p}from"./Well.efef72a4.js";import"./Dropdown.f123c078.js";import"./ExpandableGroup.6d993d84.js";import"./Icon.763cada4.js";import{a as c,F as d,j as u}from"./jsx-runtime.5f84dc07.js";import"./extends.cc011e2f.js";import"./floating-ui.dom.esm.1a703617.js";import"./index.40d0d0b8.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.ae3b7716.js";import"../sb-preview/runtime.js";const F={component:p,parameters:{docs:{description:{component:`
+### CFPB DS Divider component
+
+A component to provide a horizontal rule for separating content.
+`}}}},e={},r={args:{className:"u-mb45 u-mt45"},render:i=>c(d,{children:["Content above",u(p,{...i}),"Content below"]})};var o,t,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var s,n,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    className: 'u-mb45 u-mt45'
+  },
+  render: arguments_ => <>
+      Content above
+      <Divider {...arguments_} />
+      Content below
+    </>
+}`,...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const S=["Default","ExampleUsage"];export{e as Default,r as ExampleUsage,S as __namedExportsOrder,F as default};
+//# sourceMappingURL=Divider.stories.6741c038.js.map
---
+++ assets/Tagline.stories.3fd6677a.js
@@ -0,0 +1,18 @@
+import{a as m}from"./Well.efef72a4.js";import"./Dropdown.f123c078.js";import"./ExpandableGroup.6d993d84.js";import"./Icon.763cada4.js";import{j as a,a as d,F as g}from"./jsx-runtime.5f84dc07.js";import"./extends.cc011e2f.js";import"./floating-ui.dom.esm.1a703617.js";import"./index.40d0d0b8.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.ae3b7716.js";import"../sb-preview/runtime.js";const F={component:m,parameters:{docs:{description:{component:`
+### CFPB DS - Tagline component
+
+Source: https://cfpb.github.io/design-system/patterns/taglines
+`}}}},e={render:c=>a(m,{...c,children:d(g,{children:["An official website of the"," ",a("span",{className:"u-nowrap",children:"United States government"})]})})},r={...e,args:{isLarge:!0}};var n,t,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: properties => <Tagline {...properties}>
+      <>
+        An official website of the{' '}
+        <span className='u-nowrap'>United States government</span>
+      </>
+    </Tagline>
+}`,...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var o,i,p;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const v=["StandardTagline","LargeTagline"];export{r as LargeTagline,e as StandardTagline,v as __namedExportsOrder,F as default};
+//# sourceMappingURL=Tagline.stories.3fd6677a.js.map
