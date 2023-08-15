f8b6fcd840703ebb7ee08f1b82bbf2668b736971
contolini <contolini@users.noreply.github.com>
2023-08-15 15:17:07 +0000

    Deploying to gh-pages from @ cfpb/design-stories@580f984d871bc4cdd80673544403db92a18e1ba1 🚀

---
+++ assets/LayoutSidebar.stories.dc3cb633.js
@@ -0,0 +1,59 @@
+import"./Well.d8e544e8.js";import"./Dropdown.a9a68a21.js";import"./ExpandableGroup.96a7bfa7.js";import"./Icon.7069cf3e.js";import{j as e,a as t}from"./jsx-runtime.5f84dc07.js";import{L as n}from"./Layout.8044cb0d.js";import"./extends.cc011e2f.js";import"./floating-ui.dom.esm.1a703617.js";import"./index.40d0d0b8.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.66b883c3.js";import"../sb-preview/runtime.js";const g={component:n.Sidebar,parameters:{docs:{description:{component:`
+### CFPB DS Layout.Sidebar component
+
+Container for the sidebar content within a layout.
+
+<ul>
+<li>[flushBottom](https://cfpb.github.io/design-system/development/main-content-and-sidebars#flush-bottom-modifier)</li>
+<li>[flushTopOnSmall](https://cfpb.github.io/design-system/development/main-content-and-sidebars#flush-top-modifier-only-on-small-screens)</li>
+<li>[flushAllOnSmall](https://cfpb.github.io/design-system/development/main-content-and-sidebars#flush-all-modifier-only-on-small-screens)</li>
+</ul>
+
+Source: https://cfpb.github.io/design-system/development/main-content-and-sidebars
+
+### Usage
+
+import Layout from './Layout<br/>
+
+< Layout.Main ><br/>
+&nbsp;&nbsp;< Hero / ><br/>
+&nbsp;&nbsp;< Layout.Wrapper ><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;< Layout.Content ><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Main Content<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;< /Layout.Content ><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;< Layout.Sidebar ><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sidebar Content<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;< /Layout.Sidebar ><br/>
+&nbsp;&nbsp;< /Layout.Wrapper ><br/>
+< /Layout.Main ><br/>
+`}}}},i={args:{flushBottom:!1,flushTopOnSmall:!1,flushAllOnSmall:!1},render:a=>e(n.Main,{children:t(n.Wrapper,{children:[e(n.Sidebar,{...a,children:t("div",{children:[e("h2",{children:"Layout.Sidebar"}),t("ul",{children:[e("li",{children:"Item 1"}),e("li",{children:"Item 2"}),e("li",{children:"Item 3"})]})]})}),t(n.Content,{children:[e("h1",{children:"Layout.Content"}),e("p",{children:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat alias eum ut officiis optio similique explicabo cupiditate architecto voluptatem nostrum recusandae, eaque consectetur iure, veritatis eos, mollitia possimus error earum?"})]})]})})};var s,o,r;i.parameters={...i.parameters,docs:{...(s=i.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    flushBottom: false,
+    flushTopOnSmall: false,
+    flushAllOnSmall: false
+  },
+  render: properties => <Layout.Main>
+      <Layout.Wrapper>
+        <Layout.Sidebar {...properties}>
+          <div>
+            <h2>Layout.Sidebar</h2>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ul>
+          </div>
+        </Layout.Sidebar>
+        <Layout.Content>
+          <h1>Layout.Content</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat
+            alias eum ut officiis optio similique explicabo cupiditate
+            architecto voluptatem nostrum recusandae, eaque consectetur iure,
+            veritatis eos, mollitia possimus error earum?
+          </p>
+        </Layout.Content>
+      </Layout.Wrapper>
+    </Layout.Main>
+}`,...(r=(o=i.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const v=["Sidebar"];export{i as Sidebar,v as __namedExportsOrder,g as default};
+//# sourceMappingURL=LayoutSidebar.stories.dc3cb633.js.map
