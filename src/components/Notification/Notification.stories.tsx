044212c787a0b10e5732a9090092c688936795ca
Meis <meissadia@gmail.com>
2023-09-13 17:12:58 -0600

    feat: Align Notification component docs with Design System feat: Add field-level examples

--- src/components/Notification/Notification.stories.tsx
+++ src/components/Notification/Notification.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Notification } from '~/src/index';
 
 const meta: Meta<typeof Notification> = {
+  title: 'Components/Notifications',
   component: Notification,
   argTypes: {
     message: { control: 'text' }
@@ -10,9 +11,10 @@ const meta: Meta<typeof Notification> = {
     docs: {
       description: {
         component: `
-### CFPB DS Notification component
+Notifications alert users to the state of a form or page. In forms, notifications can appear at the top of the form or in line with form fields and can highlight successful submissions, errors that need to be corrected, or details to know before submitting the form. When used on a page, notifications can call out important information about the content (including if the content is still loading).
 
-Source: https://cfpb.github.io/design-system/components/notifications
+
+Additional guidance: [Information (base) notification](https://cfpb.github.io/design-system/components/notifications#information-base-notification)&nbsp;&nbsp;[Modifier types](https://cfpb.github.io/design-system/components/notifications#modifier-types)&nbsp;&nbsp;[Behavior](https://cfpb.github.io/design-system/components/notifications#behavior)&nbsp;&nbsp;[Accessibility](https://cfpb.github.io/design-system/components/notifications#accessibility)&nbsp;&nbsp;[Related items](https://cfpb.github.io/design-system/components/notifications#related-items)
 `
       }
     }
@@ -30,6 +32,7 @@ export const Information: Story = {
 
 export const InformationWithExplaination: Story = {
   ...Information,
+  name: 'Information with explaination',
   args: {
     ...Information.args,
     children: 'You can also add an explanation to the notification.'
@@ -38,6 +41,7 @@ export const InformationWithExplaination: Story = {
 
 export const InformationWithLinks: Story = {
   ...Information,
+  name: 'Information with links',
   args: {
     ...Information.args,
     children: 'This is the explanation of the notification.',
@@ -60,9 +64,29 @@ export const Success: Story = {
   args: { ...Information.args, type: 'success', message: '11 results' }
 };
 
+export const SuccessFieldLevel: Story = {
+  ...Information,
+  name: 'Success (field-level)',
+  args: {
+    ...Success.args,
+    isFieldLevel: true,
+    message: 'This is an inline alert with a success state.'
+  }
+};
+
 export const Warning: Story = {
   ...Information,
   args: { ...Information.args, type: 'warning', message: 'No results found.' }
+};
+
+export const WarningFieldLevel: Story = {
+  ...Information,
+  name: 'Warning (field-level)',
+  args: {
+    ...Warning.args,
+    isFieldLevel: true,
+    message: 'This is an inline alert with a warning state.'
+  }
 };
 
 export const Error: Story = {
@@ -70,8 +94,19 @@ export const Error: Story = {
   args: { ...Information.args, type: 'error', message: 'Page not found.' }
 };
 
-export const Loading: Story = {
+export const ErrorFieldLevel: Story = {
   ...Information,
+  name: 'Error (field-level)',
+  args: {
+    ...Error.args,
+    isFieldLevel: true,
+    message: 'This is an inline alert with an error state.'
+  }
+};
+
+export const InProgress: Story = {
+  ...Information,
+  name: 'In-progress',
   args: {
     ...Information.args,
     type: 'loading',
