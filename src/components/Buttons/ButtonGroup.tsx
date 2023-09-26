3f79ef08cf3de599776b264daab60b0f8d6d2cad
Meis <meissadia@gmail.com>
2023-09-26 13:01:51 -0600

    fix: [Buttons] Flatten story structure, as opposed to grouping by Type, Size, etc. fix: Remove extraneous type definition for component children fix: Ensure sentence-case for all story names fix: Open reference links in new browser tab

--- src/components/Buttons/ButtonGroup.tsx
+++ src/components/Buttons/ButtonGroup.tsx
@@ -1,17 +1,12 @@
 import type { JSXElement } from '~/src/types/jsxElement';
 
-interface ButtonGroupProperties extends React.HTMLProps<HTMLDivElement> {
-  /** Button(s) to display */
-  children?: JSXElement | JSXElement[];
-}
-
 /**
  * Display a collection of related Buttons
  */
 export const ButtonGroup = ({
   children,
   ...properties
-}: ButtonGroupProperties): JSXElement => {
+}: React.HTMLProps<HTMLDivElement>): JSXElement => {
   if (!children) return null;
   return (
     <div className='m-btn-group' {...properties}>
