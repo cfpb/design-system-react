0328bfa8b4c16166fa98dbe322ce03060f37f9ec
Meis <meissadia@gmail.com>
2023-10-12 16:22:06 -0600

    [Paragraph] Update placeholder text

--- src/components/Paragraph/Paragraphs.stories.tsx
+++ src/components/Paragraph/Paragraphs.stories.tsx
@@ -8,20 +8,19 @@ const meta: Meta<typeof Paragraph> = {
 
 export default meta;
 
-const text =
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta numquam, dolorum rerum eaque delectus aut magnam quo eos nemo facere consequatur repellat minus commodi. Autem sapiente itaque molestiae cum aliquid!';
-
 export const Body = {
   name: 'Body text',
   args: {
-    children: text
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.'
   }
 };
 
 export const Lead = {
   name: 'Lead paragraph',
   args: {
-    children: text,
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     isLead: true
   }
 };
