b9ecc64e314903351fc5b2b7085c9ebd664dbbdf
meissadia <meissadia@gmail.com>
2023-10-20 20:06:19 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@ed4b167514766befec1115b5fd21bbf1133f89a1 🚀

---
+++ assets/Label.stories.7b4fedf4.js
@@ -0,0 +1,15 @@
+/* empty css                */import{T as m}from"./Well.b398ee6c.js";import{L as a}from"./Dropdown.ff7f50d7.js";import"./Checkbox.fe2d0150.js";import"./ExpandableGroup.366a5282.js";import"./Icon.73a77196.js";import{j as e,F as n,a as i}from"./jsx-runtime.54133100.js";import"./index.cfa9e344.js";import"./isNativeReflectConstruct.97850bd2.js";import"./index.a2b6e612.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.51c0383d.js";import"../sb-preview/runtime.js";const y={title:"Components (Draft)/Labels",component:a,argTypes:{inline:{control:"boolean"}},parameters:{docs:{description:{component:`
+### CFPB DS - Label component
+
+Source: https://cfpb.github.io/design-system/components/expandables
+`}}}},t={args:{children:e(n,{children:"Text input label"}),htmlFor:"testInput"},render:s=>i(n,{children:[e(a,{...s,htmlFor:"testInput"}),e(m,{id:"testInput",name:"testInput",type:"text",placeholder:"Example input"})]})};var r,o,p;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: <>Text input label</>,
+    htmlFor: 'testInput'
+  },
+  render: arguments_ => <>
+      <Label {...arguments_} htmlFor='testInput' />
+      <TextInput id='testInput' name='testInput' type='text' placeholder='Example input' />
+    </>
+}`,...(p=(o=t.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};const _=["LabelHeading"];export{t as LabelHeading,_ as __namedExportsOrder,y as default};
+//# sourceMappingURL=Label.stories.7b4fedf4.js.map
---
+++ assets/Layout.88c14091.js
@@ -0,0 +1,2 @@
+import{j as a,k as t,l as o,m as r}from"./Well.b398ee6c.js";const n={Content:a,Main:t,Sidebar:o,Wrapper:r};export{n as L};
+//# sourceMappingURL=Layout.88c14091.js.map
