35b386d2e71ccc6832f2ac58445a857b00050447
Meis <meissadia@gmail.com>
2023-10-23 12:32:36 -0600

    fix: [Well] Add Storybook intro

--- src/components/Well/Well.tsx
+++ src/components/Well/Well.tsx
@@ -10,6 +10,11 @@ interface WellProperties {
   className?: string | undefined;
 }
 
+/**
+ * Wells are used to highlight specific information within a designated section of a page. This breaks up the flow of content on the page and helps to emphasize and set apart the content that is included. They are generally used on <a href="https://cfpb.github.io/design-system/pages/browse-pages">browse), <a href="https://cfpb.github.io/design-system/pages/learn-pages">learn), and <a href="https://cfpb.github.io/design-system/pages/document-detail-pages">document detail pages).
+ * 
+ * Source: https://cfpb.github.io/design-system/patterns/wells
+ */
 export default function Well({
   heading,
   headingLevel = 'h4',
