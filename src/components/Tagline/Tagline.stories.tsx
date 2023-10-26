f745c3c9043f9ab1e04c3150122d93b2df4e54a3
Bill Himmelsbach <whimmels@gmail.com>
2023-10-26 08:51:46 -0700

    feat(tagline): move to verified components

--- src/components/Tagline/Tagline.stories.tsx
+++ src/components/Tagline/Tagline.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Tagline } from '~/src/index';
 
 const meta: Meta<typeof Tagline> = {
-  title: 'Components (Draft)/Taglines',
+  title: 'Components (Verified)/Taglines',
   component: Tagline,
   parameters: {
     docs: {
