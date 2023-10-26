2e537ef3fbf512ba452336d8f431694f6e7986c5
billhimmelsbach <whimmels@gmail.com>
2023-10-26 02:11:38 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@e6be18e7c2ff304111db11c0f89acbe4a50e862e 🚀

---
+++ assets/Tagline.stories.93fe7790.js
@@ -0,0 +1,19 @@
+/* empty css                */import{a as m}from"./Well.b0360c5f.js";import"./Dropdown.09068a4f.js";import"./Checkbox.fe2d0150.js";import"./ExpandableGroup.39686099.js";import"./Icon.57b762d8.js";import{j as a,a as g,F as d}from"./jsx-runtime.54133100.js";import"./index.cfa9e344.js";import"./isNativeReflectConstruct.97850bd2.js";import"./index.a2b6e612.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.9f878e00.js";import"../sb-preview/runtime.js";const A={title:"Components (Draft)/Taglines",component:m,parameters:{docs:{description:{component:`
+
+Taglines are short paragraphs of text with a USA flag to their left.
+
+Source: https://cfpb.github.io/design-system/patterns/taglines
+`}}}},e={render:c=>a(m,{...c,children:g(d,{children:["An official website of the"," ",a("span",{className:"u-nowrap",children:"United States government"})]})})};e.storyName="Standard tagline";const r={...e,args:{isLarge:!0}};r.storyName="Large tagline";var t,n,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: properties => <Tagline {...properties}>
+      <>
+        An official website of the{' '}
+        <span className='u-nowrap'>United States government</span>
+      </>
+    </Tagline>
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var o,i,p;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const U=["StandardTagline","LargeTagline"];export{r as LargeTagline,e as StandardTagline,U as __namedExportsOrder,A as default};
+//# sourceMappingURL=Tagline.stories.93fe7790.js.map
---
+++ assets/Well.stories.1f7d85b4.js
@@ -0,0 +1,25 @@
+/* empty css                */import{W as p}from"./Well.b0360c5f.js";import"./Dropdown.09068a4f.js";import"./Checkbox.fe2d0150.js";import"./ExpandableGroup.39686099.js";import"./Icon.57b762d8.js";import{a as u,F as m,j as i}from"./jsx-runtime.54133100.js";import"./index.cfa9e344.js";import"./isNativeReflectConstruct.97850bd2.js";import"./index.a2b6e612.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.9f878e00.js";import"../sb-preview/runtime.js";const L={title:"Components (Draft)/Wells",component:p,argTypes:{}},e={args:{heading:"Well title",text:u(m,{children:["Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae dolor distinctio tenetur quibusdam rem debitis, voluptate nesciunt culpa officiis quos perspiciatis nostrum illo libero autem beatae temporibus ratione reprehenderit? ",i("a",{href:"#",children:"Example link"}),"."]})}},t={args:{heading:"Well title",text:i(m,{children:"Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae dolor distinctio tenetur quibusdam rem debitis, voluptate nesciunt culpa officiis quos perspiciatis nostrum illo libero autem beatae temporibus ratione reprehenderit?"}),links:[i("a",{href:"#",children:"Example link"},"example1")]}};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    heading: 'Well title',
+    text: <>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae dolor
+        distinctio tenetur quibusdam rem debitis, voluptate nesciunt culpa
+        officiis quos perspiciatis nostrum illo libero autem beatae temporibus
+        ratione reprehenderit? <a href='#'>Example link</a>.
+      </>
+  }
+}`,...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var s,n,l;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    heading: 'Well title',
+    text: <>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae dolor
+        distinctio tenetur quibusdam rem debitis, voluptate nesciunt culpa
+        officiis quos perspiciatis nostrum illo libero autem beatae temporibus
+        ratione reprehenderit?
+      </>,
+    links: [<a href='#' key='example1'>
+        Example link
+      </a>]
+  }
+}`,...(l=(n=t.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const S=["Standard","CallToAction"];export{t as CallToAction,e as Standard,S as __namedExportsOrder,L as default};
+//# sourceMappingURL=Well.stories.1f7d85b4.js.map
