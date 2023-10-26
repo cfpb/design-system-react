085f1cb02e3ea38cda625e881cda1245c49f38d8
Meis <meissadia@gmail.com>
2023-10-25 20:40:24 -0600

    fix: [Well] Correct intro paragraph's links

--- src/components/Well/Well.tsx
+++ src/components/Well/Well.tsx
@@ -11,7 +11,7 @@ interface WellProperties {
 }
 
 /**
- * Wells are used to highlight specific information within a designated section of a page. This breaks up the flow of content on the page and helps to emphasize and set apart the content that is included. They are generally used on <a href="https://cfpb.github.io/design-system/pages/browse-pages">browse), <a href="https://cfpb.github.io/design-system/pages/learn-pages">learn), and <a href="https://cfpb.github.io/design-system/pages/document-detail-pages">document detail pages).
+ * Wells are used to highlight specific information within a designated section of a page. This breaks up the flow of content on the page and helps to emphasize and set apart the content that is included. They are generally used on <a href="https://cfpb.github.io/design-system/pages/browse-pages">browse</a>, <a href="https://cfpb.github.io/design-system/pages/learn-pages">learn</a>, and <a href="https://cfpb.github.io/design-system/pages/document-detail-pages">document detail pages</a>.
  * 
  * Source: https://cfpb.github.io/design-system/patterns/wells
  */
