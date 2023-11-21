f98fe6a67351912dd42c94b029b96132adaa68df
Meis <meissadia@gmail.com>
2023-11-21 15:43:11 -0700

    feat: [Well] Stories - Move to Verified - Adjust names to match components - Set `children` type to string to avoid page crashes

--- src/components/Well/Well.stories.tsx
+++ src/components/Well/Well.stories.tsx
@@ -1,10 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Link, Well, WelledCTA } from '~/src/index';
+import { Link, WellContainer, WellContent } from '~/src/index';
 
-const meta: Meta<typeof Well> = {
-  title: 'Components (Draft)/Wells',
-  component: Well,
-  argTypes: {}
+const meta: Meta<typeof WellContainer> = {
+  title: 'Components (Verified)/Wells',
+  component: WellContainer,
+  argTypes: {
+    children: { type: 'string' }
+  }
 };
 
 export default meta;
@@ -12,27 +14,14 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Container: Story = {
-  name: 'Well',
-  args: {
-    children: [
-      <p key='p1'>
-        Grey wells should be used to feature content or specific calls to
-        action. They will help visually set apart the content within it, so use
-        sparingly to draw attention to that content.
-      </p>,
-      <p key='p2'>
-        Ideally, content within a well should not take up more than a third of
-        page content.
-      </p>
-    ]
-  }
+  name: 'Well (container)'
 };
 
-export const WellPattern: Story = {
-  name: 'Welled call-to-action',
+export const Content: Story = {
+  name: 'Well (content)',
   args: {
     heading: 'Heading',
-    headingLevel: 'h2',
+    headingLevel: 'h3',
     text: 'Lorem ipsum dolor sit amet, ei ius adhuc inani iudico, labitur instructior ex pri. Cu pri inani constituto, cum aeque noster commodo',
     links: [
       <Link type='list' href='/#' key='example1'>
@@ -40,5 +29,5 @@ export const WellPattern: Story = {
       </Link>
     ]
   },
-  render: arguments_ => <WelledCTA {...arguments_} />
+  render: arguments_ => <WellContent {...arguments_} />
 };
