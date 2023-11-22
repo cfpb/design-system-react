0fce81d3a032b97dd3397674a5474d9bf391c8e4
meissadia <meissadia@gmail.com>
2023-11-22 20:56:50 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@f717c9f71c398b74ce36b60b8de9a879ce49d30a 🚀

---
+++ assets/PageHeader.stories.c03b33f3.js
@@ -0,0 +1,11 @@
+/* empty css                */import{d as i,E as p}from"./Well.a655144f.js";import"./Dropdown.bfe8d62f.js";import"./Checkbox.ad7351d6.js";import"./ExpandableGroup.ae4ffb1f.js";import"./Icon.0a9dc2e9.js";import"./jsx-runtime.54133100.js";import"./Link.53e442ec.js";import"./Paragraph.878bd808.js";import"./index.cfa9e344.js";import"./isNativeReflectConstruct.97850bd2.js";import"./index.a2b6e612.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.4a0683de.js";import"../sb-preview/runtime.js";const U={title:"Components (Draft)/Page header",component:i,argTypes:{}},g={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},r={args:{links:p,user:g}},e={args:{links:p}};var o,s,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var a,n,m;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...(m=(n=e.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const _=["LoggedIn","LoggedOut"];export{r as LoggedIn,e as LoggedOut,_ as __namedExportsOrder,U as default};
+//# sourceMappingURL=PageHeader.stories.c03b33f3.js.map
---
+++ assets/Tagline.stories.5e377dc5.js
@@ -0,0 +1,19 @@
+/* empty css                */import{b as m}from"./Well.a655144f.js";import"./Dropdown.bfe8d62f.js";import"./Checkbox.ad7351d6.js";import"./ExpandableGroup.ae4ffb1f.js";import"./Icon.0a9dc2e9.js";import{j as a,a as d,F as g}from"./jsx-runtime.54133100.js";import"./Link.53e442ec.js";import"./Paragraph.878bd808.js";import"./index.cfa9e344.js";import"./isNativeReflectConstruct.97850bd2.js";import"./index.a2b6e612.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.4a0683de.js";import"../sb-preview/runtime.js";const U={title:"Components (Verified)/Taglines",component:m,parameters:{docs:{description:{component:`
+
+Taglines are short paragraphs of text with the USA flag to their left that are used in the header and footer across consumerfinance.gov.
+
+Source: https://cfpb.github.io/design-system/components/taglines
+`}}}},e={render:c=>a(m,{...c,children:d(g,{children:["An official website of the"," ",a("span",{className:"u-nowrap",children:"United States government"})]})})};e.storyName="Standard tagline";const r={...e,args:{isLarge:!0}};r.storyName="Large tagline";var t,n,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: properties => <Tagline {...properties}>
+      <>
+        An official website of the{' '}
+        <span className='u-nowrap'>United States government</span>
+      </>
+    </Tagline>
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var s,i,p;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const F=["StandardTagline","LargeTagline"];export{r as LargeTagline,e as StandardTagline,F as __namedExportsOrder,U as default};
+//# sourceMappingURL=Tagline.stories.5e377dc5.js.map
