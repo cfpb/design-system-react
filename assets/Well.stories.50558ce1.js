8ce53068e2f9e3e0a66e7b29ec47be9f3661c2c3
shindigira <shindigira@gmail.com>
2023-12-04 19:06:24 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@c5832aa7f0beba27e1e33f7b2b6aad40ba25cf74 🚀

---
+++ assets/Well.stories.50558ce1.js
@@ -0,0 +1,15 @@
+/* empty css                */import{W as p,L as c,a as u}from"./Well.1fbbc12e.js";import"./Dropdown.d02b064c.js";import"./Checkbox.ad7351d6.js";import"./ExpandableGroup.18a30c66.js";import"./Icon.cfa93e49.js";import{j as t}from"./jsx-runtime.54133100.js";import"./Paragraph.878bd808.js";import"./index.cfa9e344.js";import"./isNativeReflectConstruct.97850bd2.js";import"./index.a2b6e612.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.1be43d92.js";import"../sb-preview/runtime.js";const v={title:"Components (Verified)/Wells",component:p,argTypes:{children:{type:"string"}}},e={name:"Well (container)"},n={name:"Well (content)",args:{heading:"Heading",headingLevel:"h3",text:"Lorem ipsum dolor sit amet, ei ius adhuc inani iudico, labitur instructior ex pri. Cu pri inani constituto, cum aeque noster commodo",links:[t(c,{type:"list",href:"/#",children:"Call-to-action link"},"example1")]},render:l=>t(u,{...l})};var r,i,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Well (container)'
+}`,...(o=(i=e.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};var a,s,m;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: 'Well (content)',
+  args: {
+    heading: 'Heading',
+    headingLevel: 'h3',
+    text: 'Lorem ipsum dolor sit amet, ei ius adhuc inani iudico, labitur instructior ex pri. Cu pri inani constituto, cum aeque noster commodo',
+    links: [<Link type='list' href='/#' key='example1'>
+        Call-to-action link
+      </Link>]
+  },
+  render: arguments_ => <WellContent {...arguments_} />
+}`,...(m=(s=n.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};const H=["Container","Content"];export{e as Container,n as Content,H as __namedExportsOrder,v as default};
+//# sourceMappingURL=Well.stories.50558ce1.js.map
