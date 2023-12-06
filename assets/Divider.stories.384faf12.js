a01cacdf0ffdc892076fb57f7f565cfec2bbb919
meissadia <meissadia@gmail.com>
2023-12-06 16:48:18 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@858326f6aa6659cb7509a04c9d028b713bf610fb 🚀

---
+++ assets/Divider.stories.384faf12.js
@@ -0,0 +1,15 @@
+/* empty css                */import{o as p}from"./Well.f7ee2847.js";import"./Dropdown.8b018e69.js";import"./Checkbox.ad7351d6.js";import"./ExpandableGroup.57f915e7.js";import"./Icon.1292521f.js";import{a as c,F as d,j as u}from"./jsx-runtime.54133100.js";import"./Paragraph.878bd808.js";import"./index.cfa9e344.js";import"./isNativeReflectConstruct.97850bd2.js";import"./index.a2b6e612.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.fd5bc1f2.js";import"../sb-preview/runtime.js";const w={title:"Components (Draft)/Divider",component:p,parameters:{docs:{description:{component:`
+### CFPB DS Divider component
+
+A component to provide a horizontal rule for separating content.
+`}}}},e={},r={args:{className:"u-mb45 u-mt45"},render:i=>c(d,{children:["Content above",u(p,{...i}),"Content below"]})};var o,t,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var s,n,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    className: 'u-mb45 u-mt45'
+  },
+  render: arguments_ => <>
+      Content above
+      <Divider {...arguments_} />
+      Content below
+    </>
+}`,...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const N=["Default","ExampleUsage"];export{e as Default,r as ExampleUsage,N as __namedExportsOrder,w as default};
+//# sourceMappingURL=Divider.stories.384faf12.js.map
---
+++ assets/Layout.69d84b83.js
@@ -0,0 +1,2 @@
+import{j as a,k as t,l as o,m as r}from"./Well.f7ee2847.js";const n={Content:a,Main:t,Sidebar:o,Wrapper:r};export{n as L};
+//# sourceMappingURL=Layout.69d84b83.js.map
