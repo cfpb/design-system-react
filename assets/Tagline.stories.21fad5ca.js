7aa7fdc7d2160e7c5c0298140143e4d6f4f31a18
meissadia <meissadia@gmail.com>
2024-01-08 22:03:26 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@327b52754914fa517d6114263ddc5f52109aa1f1 🚀

---
+++ assets/Tagline.stories.21fad5ca.js
@@ -0,0 +1,19 @@
+/* empty css                */import{c as m}from"./Well.ff4da208.js";import"./Dropdown.a8c60362.js";import"./Checkbox.ad7351d6.js";import"./ExpandableGroup.d4c8afc1.js";import"./Icon.947fcbcc.js";import{j as a,a as d,F as g}from"./jsx-runtime.54133100.js";import"./Paragraph.878bd808.js";import"./index.cfa9e344.js";import"./isNativeReflectConstruct.97850bd2.js";import"./index.a2b6e612.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.d6e6f406.js";import"../sb-preview/runtime.js";const A={title:"Components (Verified)/Taglines",component:m,parameters:{docs:{description:{component:`
+
+Taglines are short paragraphs of text with the USA flag to their left that are used in the header and footer across consumerfinance.gov.
+
+Source: https://cfpb.github.io/design-system/components/taglines
+`}}}},e={render:c=>a(m,{...c,children:d(g,{children:["An official website of the"," ",a("span",{className:"u-nowrap",children:"United States government"})]})})};e.storyName="Standard tagline";const r={...e,args:{isLarge:!0}};r.storyName="Large tagline";var t,n,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: properties => <Tagline {...properties}>
+      <>
+        An official website of the{' '}
+        <span className='u-nowrap'>United States government</span>
+      </>
+    </Tagline>
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var s,i,p;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const U=["StandardTagline","LargeTagline"];export{r as LargeTagline,e as StandardTagline,U as __namedExportsOrder,A as default};
+//# sourceMappingURL=Tagline.stories.21fad5ca.js.map
