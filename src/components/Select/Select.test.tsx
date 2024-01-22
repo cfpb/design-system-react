d7828a2e7ecc759154344e1f8be1fa9a288168cd
Meis <meissadia@gmail.com>
2024-01-22 12:13:15 -0700

    fix: Update Multiselect tests now that we can rely on the DS to render it's Option elements

--- src/components/Select/Select.test.tsx
+++ src/components/Select/Select.test.tsx
@@ -1,12 +1,12 @@
 import { jest } from '@storybook/jest';
 import '@testing-library/jest-dom';
-import { act, render, screen } from '@testing-library/react';
+import { act, render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Select } from './Select';
 import { DemoOptions } from './testUtils';
 
-describe.skip('<SelectSingle />', () => {
-  it.skip('renders Single select with default value', () => {
+describe('<SelectSingle />', () => {
+  it('renders Single select with default value', () => {
     render(<Select id='single' options={DemoOptions} />);
     expect(screen.getByRole('combobox')).toHaveValue('option0');
     expect(screen.getByRole('option', { name: 'Option 0' }).selected).toBe(
@@ -34,8 +34,8 @@ describe.skip('<SelectSingle />', () => {
   });
 });
 
-describe.skip('<SelectMulti />', () => {
-  it.skip('Is interactable', async () => {
+describe('<SelectMulti />', () => {
+  it('Is interactable', async () => {
     const id = 'multi';
     const label = 'MultiLabel';
     const maxSelections = 2;
@@ -61,11 +61,14 @@ describe.skip('<SelectMulti />', () => {
     // Initial Select has nothing selected
     expect(onChange).toHaveBeenCalledWith([]);
 
+    // Selection limit has not been reached
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(document.querySelectorAll('.u-max-selections').length).toBe(0);
+
     // Allows selection of multiple options, up to the limit
     await act(async () => {
       await user.click(screen.getByLabelText('Option 1'));
       await user.click(screen.getByLabelText('Option 5'));
-      await user.click(screen.getByLabelText('Option 0'));
     });
 
     // Change handler is called with the expected content
@@ -74,9 +77,30 @@ describe.skip('<SelectMulti />', () => {
       { ...DemoOptions[5], selected: true }
     ]);
 
-    // Tags are rendered only for the selected options
-    expect(screen.getByTestId(`tag-multi-option1`)).toBeInTheDocument();
-    expect(screen.getByTestId(`tag-multi-option5`)).toBeInTheDocument();
-    expect(screen.queryByTestId(`tag-multi-option0`)).not.toBeInTheDocument();
+    // Tags are rendered for the selected options
+    const AllButtons = screen.getAllByRole(`button`);
+    expect(within(AllButtons[0]).getByText(`Option 1`)).toBeInTheDocument();
+    expect(within(AllButtons[1]).getByText(`Option 5`)).toBeInTheDocument();
+    expect(AllButtons.length).toBe(2);
+
+    /* TODO: Verify that no further selections are allowed, beyond the max.
+     *  We are relying on the DS implementation of Multiselect which uses CSS
+     *  to show/hide options, but the options' <li> remain in the DOM. To Vitest,
+     *  these elements, even when CSS is set to `display: none`, are still "visible".
+     *
+     *  For now, I'm just checking that the `u-max-selections` class is applied.
+     */
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(document.querySelectorAll('.u-max-selections').length).toBe(1);
+
+    // Allows deselection of options
+    await act(async () => {
+      await user.click(screen.getByLabelText('Option 1'));
+      await user.click(screen.getByLabelText('Option 5'));
+    });
+
+    const NoButtons = screen.queryAllByRole(`button`);
+    expect(NoButtons.length).toBe(0);
+    expect(onChange).toHaveBeenCalledWith([]);
   });
 });
