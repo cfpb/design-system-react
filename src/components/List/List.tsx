722ad14f43853a4c9096f8edef4fa0a3999c1456
shindigira <shindigira@gmail.com>
2024-01-26 11:44:29 -0500

    style: Updated styles on Checkbox Overview

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,5 @@
 import classnames from 'classnames';
+import './List.less';
 
 interface ListProperties {
   children: JSX.Element | JSX.Element[];
@@ -8,6 +9,7 @@ interface ListProperties {
   isOrdered?: boolean;
   isSpaced?: boolean;
   isUnstyled?: boolean;
+  forceListStyleOverride?: boolean;
 }
 
 export default function List({
@@ -17,13 +19,16 @@ export default function List({
   isLinks = false,
   isOrdered,
   isSpaced,
+  forceListStyleOverride,
   isUnstyled
 }: ListProperties): JSX.Element {
-  const cnames = [className, 'm-list'];
+  const cnames = ['m-list'];
   if (isHorizontal) cnames.push('m-list__horizontal');
   if (isLinks) cnames.push('m-list__links');
   if (isSpaced) cnames.push('m-list__spaced');
   if (isUnstyled) cnames.push('m-list__unstyled');
+  if (forceListStyleOverride) cnames.push('cfpb-list-style-override');
+  if (className) cnames.push(className);
 
   if (isOrdered) return <ol className={classnames(cnames)}>{children}</ol>;
   return <ul className={classnames(cnames)}>{children}</ul>;
