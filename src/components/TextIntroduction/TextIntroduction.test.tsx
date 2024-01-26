2adf8d961d6f1bc6c563690b6c3fdcfc289d57d3
Meis <meissadia@gmail.com>
2024-01-26 13:09:53 -0700

    [TextIntroduction][Testing] Improve focus of coverage report

--- src/components/TextIntroduction/TextIntroduction.test.tsx
+++ src/components/TextIntroduction/TextIntroduction.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import { TextIntroduction } from '~/src/index';
+import { TextIntroduction } from './TextIntroduction';
 import placeholders from './testHelpers';
 
 describe('<TextIntroduction />', () => {
