03732a4f0afa8de9889aefbc065a727a00c572fe
shindigira <shindigira@gmail.com>
2024-01-30 19:39:45 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@307f2918ff8becf9198d49387e9434e17315edfc 🚀

---
+++ assets/LayoutSidebar.stories.b372ca22.js
@@ -0,0 +1,59 @@
+/* empty css                */import"./Well.e4dc9050.js";import"./Dropdown.51036178.js";import"./Checkbox.adabeb2b.js";import"./ExpandableGroup.54048ccb.js";import"./Icon.f601679c.js";import{j as t,a as n}from"./jsx-runtime.54133100.js";import{L as e}from"./Layout.c9793155.js";import"./Paragraph.878bd808.js";import"./index.cfa9e344.js";import"./isNativeReflectConstruct.97850bd2.js";import"./index.a2b6e612.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.034b3e74.js";import"../sb-preview/runtime.js";const O={title:"Components (Draft)/Layout/Sidebar",tags:["autodocs"],component:e.Sidebar,parameters:{docs:{description:{component:`
+### CFPB DS Layout.Sidebar component
+
+Container for the sidebar content within a layout.
+
+<ul>
+<li>[flushBottom](https://cfpb.github.io/design-system/development/main-content-and-sidebars#flush-bottom-modifier)</li>
+<li>[flushTopOnSmall](https://cfpb.github.io/design-system/development/main-content-and-sidebars#flush-top-modifier-only-on-small-screens)</li>
+<li>[flushAllOnSmall](https://cfpb.github.io/design-system/development/main-content-and-sidebars#flush-all-modifier-only-on-small-screens)</li>
+</ul>
+
+Source: https://cfpb.github.io/design-system/development/main-content-and-sidebars
+
+### Usage
+
+import Layout from './Layout<br/>
+
+< Layout.Main ><br/>
+&nbsp;&nbsp;< Hero / ><br/>
+&nbsp;&nbsp;< Layout.Wrapper ><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;< Layout.Content ><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Main Content<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;< /Layout.Content ><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;< Layout.Sidebar ><br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sidebar Content<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;< /Layout.Sidebar ><br/>
+&nbsp;&nbsp;< /Layout.Wrapper ><br/>
+< /Layout.Main ><br/>
+`}}}},i={args:{flushBottom:!1,flushTopOnSmall:!1,flushAllOnSmall:!1},render:a=>t(e.Main,{children:n(e.Wrapper,{children:[t(e.Sidebar,{...a,children:n("div",{children:[t("h2",{children:"Layout.Sidebar"}),n("ul",{children:[t("li",{children:"Item 1"}),t("li",{children:"Item 2"}),t("li",{children:"Item 3"})]})]})}),n(e.Content,{children:[t("h1",{children:"Layout.Content"}),t("p",{children:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat alias eum ut officiis optio similique explicabo cupiditate architecto voluptatem nostrum recusandae, eaque consectetur iure, veritatis eos, mollitia possimus error earum?"})]})]})})};var o,s,r;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    flushBottom: false,
+    flushTopOnSmall: false,
+    flushAllOnSmall: false
+  },
+  render: properties => <Layout.Main>
+      <Layout.Wrapper>
+        <Layout.Sidebar {...properties}>
+          <div>
+            <h2>Layout.Sidebar</h2>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ul>
+          </div>
+        </Layout.Sidebar>
+        <Layout.Content>
+          <h1>Layout.Content</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat
+            alias eum ut officiis optio similique explicabo cupiditate
+            architecto voluptatem nostrum recusandae, eaque consectetur iure,
+            veritatis eos, mollitia possimus error earum?
+          </p>
+        </Layout.Content>
+      </Layout.Wrapper>
+    </Layout.Main>
+}`,...(r=(s=i.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const x=["Sidebar"];export{i as Sidebar,x as __namedExportsOrder,O as default};
+//# sourceMappingURL=LayoutSidebar.stories.b372ca22.js.map
---
+++ assets/List.stories.4b9f40d4.js
@@ -0,0 +1,45 @@
+/* empty css                */import{a as j,b as o,h as d}from"./Well.e4dc9050.js";import"./Dropdown.51036178.js";import"./Checkbox.adabeb2b.js";import"./ExpandableGroup.54048ccb.js";import"./Icon.f601679c.js";import{a as z,F as H,j as e}from"./jsx-runtime.54133100.js";import"./Paragraph.878bd808.js";import"./index.cfa9e344.js";import"./isNativeReflectConstruct.97850bd2.js";import"./index.a2b6e612.js";import"./_commonjsHelpers.712cc82f.js";import"./iframe.034b3e74.js";import"../sb-preview/runtime.js";const N={title:"Components (Draft)/Lists",tags:["autodocs"],component:j,argTypes:{}},r={args:{children:z(H,{children:[e(o,{children:"First"}),e(o,{children:"Second"}),e(o,{children:"Third"})]})}},s={args:{...r.args,isOrdered:!0}},t={args:{...r.args,isUnstyled:!0}},n={args:{...r.args,isHorizontal:!0}},i={args:{...r.args,isSpaced:!0}},a={args:{isLinks:!0,children:z(H,{children:[e(d,{type:"list",href:"#",children:"First Link"}),e(d,{type:"list",href:"#",children:"Second Link"}),e(d,{type:"list",href:"#",children:"Third Link"})]})}};var c,p,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <ListItem>First</ListItem>
+        <ListItem>Second</ListItem>
+        <ListItem>Third</ListItem>
+      </>
+  }
+}`,...(m=(p=r.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var l,L,u;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...Unordered.args,
+    isOrdered: true
+  }
+}`,...(u=(L=s.parameters)==null?void 0:L.docs)==null?void 0:u.source}}};var g,h,k;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    ...Unordered.args,
+    isUnstyled: true
+  }
+}`,...(k=(h=t.parameters)==null?void 0:h.docs)==null?void 0:k.source}}};var S,y,f;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    ...Unordered.args,
+    isHorizontal: true
+  }
+}`,...(f=(y=n.parameters)==null?void 0:y.docs)==null?void 0:f.source}}};var U,I,F;i.parameters={...i.parameters,docs:{...(U=i.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    ...Unordered.args,
+    isSpaced: true
+  }
+}`,...(F=(I=i.parameters)==null?void 0:I.docs)==null?void 0:F.source}}};var O,T,x;a.parameters={...a.parameters,docs:{...(O=a.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    isLinks: true,
+    children: <>
+        <ListLink type='list' href='#'>
+          First Link
+        </ListLink>
+        <ListLink type='list' href='#'>
+          Second Link
+        </ListLink>
+        <ListLink type='list' href='#'>
+          Third Link
+        </ListLink>
+      </>
+  }
+}`,...(x=(T=a.parameters)==null?void 0:T.docs)==null?void 0:x.source}}};const P=["Unordered","Ordered","Unstyled","Horizontal","Spaced","Links"];export{n as Horizontal,a as Links,s as Ordered,i as Spaced,r as Unordered,t as Unstyled,P as __namedExportsOrder,N as default};
+//# sourceMappingURL=List.stories.4b9f40d4.js.map
