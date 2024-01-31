db2caade71ff981715e15c8fc25702f216c80706
Meis <meissadia@gmail.com>
2024-01-30 17:38:28 -0700

    [Select] Separate stories for Multiple into its own file

---
+++ src/components/Select/SelectMulti.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Select } from '~/src/index';
+import { DemoOptions } from './testUtils';
+
+const meta: Meta<typeof Select> = {
+  title: 'Components (Draft)/Selects/Multiple',
+  component: Select,
+  tags: ['autodocs'],
+  argTypes: {
+    disabled: { control: 'boolean' },
+    isMulti: { control: 'boolean' }
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Enabled: Story = {
+  args: {
+    id: 'multiSelect',
+    label: 'Label',
+    isMulti: true,
+    options: DemoOptions,
+    disabled: true
+  }
+};
