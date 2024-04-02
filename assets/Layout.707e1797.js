5f31c5f5c13366812774fdc69e4d5635ebba6e15
billhimmelsbach <whimmels@gmail.com>
2024-04-02 18:19:16 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@50af172422abec29ac4a09b1aa30765dfd091a82 🚀

---
+++ assets/Layout.707e1797.js
@@ -0,0 +1,1 @@
+import{e as a,f as t,g as o,h as r}from"./TextIntroduction.37482027.js";const n={Content:a,Main:t,Sidebar:o,Wrapper:r};export{n as L};
---
+++ assets/RadioButton.stories.811a5178.js
@@ -0,0 +1,33 @@
+/* empty css                */import{R}from"./TextIntroduction.37482027.js";import"./Dropdown.771b5c5c.js";import"./Checkbox.1213d1d0.js";import"./ExpandableGroup.f6d6ab5a.js";import"./index.7eca609e.js";import"./Icon.dd7126da.js";import"./Well.e7055d12.js";import"./Paragraph.9304145b.js";import"./TextArea.7a5ccf81.js";import"./index.ca1bfdbe.js";import"./_commonjsHelpers.712cc82f.js";import"./extends.946277fc.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm.67af5abc.js";import"./index.f108a8f5.js";import"./iframe.73b1d99c.js";import"../sb-preview/runtime.js";const O={title:"Components (Verified)/Radio buttons",tags:["autodocs"],component:R,argTypes:{disabled:{control:"boolean"},isLarge:{control:"boolean"}},parameters:{docs:{description:{component:`Use radio buttons when the user can select exactly one option from a group. Avoid long lists of radio buttons (usually no more than 6-8 options). When there are more than two options, stack radio buttons vertically. Use [checkboxes](https://cfpb.github.io/design-system/components/checkboxes) when the user can select more than one option from a group.
+
+Source: https://cfpb.github.io/design-system/components/radio-buttons`}}}},b="This is optional helper text",a={name:"Standard radio button",args:{id:"testRadio",label:"Standard radio button",name:"Radio select"}},r={name:"Standard radio button with helper text",args:{...a.args,id:"StandardRadioWithHelper",helperText:b}},e={name:"Large target area radio button",args:{...a.args,id:"LargeRadio",isLarge:!0,label:"Large target area radio button"}},t={name:"Large target area radio button with helper text",args:{...e.args,id:"LargeRadioWithHelper",isLarge:!0,helperText:b}};var o,n,i;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  name: 'Standard radio button',
+  args: {
+    id: 'testRadio',
+    label: 'Standard radio button',
+    name: 'Radio select'
+  }
+}`,...(i=(n=a.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var s,d,p;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: 'Standard radio button with helper text',
+  args: {
+    ...StandardRadio.args,
+    id: 'StandardRadioWithHelper',
+    helperText
+  }
+}`,...(p=(d=r.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var m,c,g;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  name: 'Large target area radio button',
+  args: {
+    ...StandardRadio.args,
+    id: 'LargeRadio',
+    isLarge: true,
+    label: 'Large target area radio button'
+  }
+}`,...(g=(c=e.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};var l,u,h;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  name: 'Large target area radio button with helper text',
+  args: {
+    ...LargeRadio.args,
+    id: 'LargeRadioWithHelper',
+    isLarge: true,
+    helperText
+  }
+}`,...(h=(u=t.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};const V=["StandardRadio","StandardRadioWithHelper","LargeRadio","LargeRadioWithHelper"];export{e as LargeRadio,t as LargeRadioWithHelper,a as StandardRadio,r as StandardRadioWithHelper,V as __namedExportsOrder,O as default};
