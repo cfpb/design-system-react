164669d52d14dff0e2aa320186b45920ecb1b983
shindigira <shindigira@gmail.com>
2024-05-31 09:16:12 -0700

    removed aria-label

--- src/components/Alert/AlertLink.tsx
+++ src/components/Alert/AlertLink.tsx
@@ -12,11 +12,7 @@ export const AlertLink = ({
   isExternal
 }: AlertLinkProperties): JSX.Element => (
   <li className='m-list_item'>
-    <a
-      className='m-list_link'
-      href={href}
-      aria-label={`${label || ''} external link}`}
-    >
+    <a className='m-list_link' href={href}>
       {label} {isExternal ? <Icon name='external-link' /> : null}
     </a>
   </li>
