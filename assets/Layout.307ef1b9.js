354dc0e874f85179649987fc2336350086903ece
shindigira <shindigira@gmail.com>
2024-05-31 21:19:43 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@5498b4238491ad3476042a4f4c763e6252bed926 🚀

---
+++ assets/Layout.307ef1b9.js
@@ -0,0 +1,1 @@
+import{f as a,g as t,h as o,i as r}from"./TextIntroduction.2d6744a1.js";const n={Content:a,Main:t,Sidebar:o,Wrapper:r};export{n as L};
---
+++ assets/PageHeader.stories.2428cd08.js
@@ -0,0 +1,10 @@
+/* empty css                */import{P as i,E as p}from"./TextIntroduction.2d6744a1.js";import"./Dropdown.6bd69fd7.js";import"./Checkbox.1213d1d0.js";import"./ExpandableGroup.c1217789.js";import"./Icon.afebc5d3.js";import"./Well.e7055d12.js";import"./Paragraph.9304145b.js";import"./TextArea.7a5ccf81.js";import"./index.7eca609e.js";import"./index.ca1bfdbe.js";import"./_commonjsHelpers.712cc82f.js";import"./extends.946277fc.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm.67af5abc.js";import"./index.f108a8f5.js";import"./iframe.292196a9.js";import"../sb-preview/runtime.js";const y={title:"Components (Draft)/Page header",tags:["autodocs"],component:i,argTypes:{}},g={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},r={args:{links:p,user:g}},o={args:{links:p}};var e,s,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var a,n,m;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...(m=(n=o.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const C=["LoggedIn","LoggedOut"];export{r as LoggedIn,o as LoggedOut,C as __namedExportsOrder,y as default};
