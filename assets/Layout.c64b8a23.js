50763f80902eb32edd22c7ff4af3235e466649de
ojbravo <ojbravo@gmail.com>
2024-06-24 15:12:40 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@aaa4267c4cbaf7edb6a5c60878548ec489f63510 🚀

--- assets/Layout.c64b8a23.js
+++
@@ -1,1 +0,0 @@
-import{f as a,g as t,h as o,i as r}from"./TextIntroduction.1f66657c.js";const n={Content:a,Main:t,Sidebar:o,Wrapper:r};export{n as L};
---
+++ assets/Tagline.stories.c9a1894e.js
@@ -0,0 +1,18 @@
+import{j as t}from"./index.7eca609e.js";/* empty css                */import{m}from"./TextIntroduction.f2af4f4d.js";import"./Dropdown.4fe1bd26.js";import"./Checkbox.1213d1d0.js";import"./ExpandableGroup.87f134ef.js";import"./Icon.d6b273da.js";import"./Well.e7055d12.js";import"./Paragraph.9304145b.js";import"./TextArea.7a5ccf81.js";import"./index.ca1bfdbe.js";import"./_commonjsHelpers.712cc82f.js";import"./extends.946277fc.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm.67af5abc.js";import"./index.f108a8f5.js";import"./iframe.fde415cf.js";import"../sb-preview/runtime.js";const U={title:"Components (Verified)/Taglines",tags:["autodocs"],component:m,parameters:{docs:{description:{component:`
+
+Taglines are short paragraphs of text with the USA flag to their left that are used in the header and footer across consumerfinance.gov.
+
+Source: https://cfpb.github.io/design-system/components/taglines
+`}}}},e={render:c=>t.exports.jsx(m,{...c,children:t.exports.jsxs(t.exports.Fragment,{children:["An official website of the"," ",t.exports.jsx("span",{className:"u-nowrap",children:"United States government"})]})})};e.storyName="Standard tagline";const r={...e,args:{isLarge:!0}};r.storyName="Large tagline";var a,o,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: properties => <Tagline {...properties}>
+      <>
+        An official website of the{' '}
+        <span className='u-nowrap'>United States government</span>
+      </>
+    </Tagline>
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var n,i,p;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const _=["StandardTagline","LargeTagline"];export{r as LargeTagline,e as StandardTagline,_ as __namedExportsOrder,U as default};
