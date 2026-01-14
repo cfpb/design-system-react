d896dec39d602a42e7449f6cb7c28fdda1add9d6
Natalia Fitzgerald <77348967+natalia-fitzgerald@users.noreply.github.com>
2026-01-14 14:41:12 -0500

    Update Expandable.stories.tsx

--- src/components/Expandable/Expandable.stories.tsx
+++ src/components/Expandable/Expandable.stories.tsx
@@ -12,8 +12,8 @@ const meta: Meta<typeof Expandable> = {
     docs: {
       description: {
         component: `
-### CFPB DS - Expandable component
-
+### Expandables
+Expandables are components that have additional content that can be opened (expanded) and closed (collapsed). They can appear on their own or in groups. They may be helpful for FAQ sections, schedules, and for conserving vertical space by collapsing secondary information on mobile devices.
 Source: https://cfpb.github.io/design-system/components/expandables
 `
       }
