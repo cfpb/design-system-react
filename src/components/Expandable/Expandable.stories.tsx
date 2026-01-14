8bd0816d6573651167efaa07f646ef3271557734
Natalia Fitzgerald <77348967+natalia-fitzgerald@users.noreply.github.com>
2026-01-14 14:29:36 -0500

    Update Expandable.stories.tsx

--- src/components/Expandable/Expandable.stories.tsx
+++ src/components/Expandable/Expandable.stories.tsx
@@ -5,7 +5,7 @@ import { Expandable } from '~/src/index';
 import { sleep } from '../../utils/sleep';
 
 const meta: Meta<typeof Expandable> = {
-  title: 'Components (Draft)/Expandable/Single',
+  title: 'Components (Draft)/Expandables',
   component: Expandable,
   tags: ['autodocs'],
   parameters: {
