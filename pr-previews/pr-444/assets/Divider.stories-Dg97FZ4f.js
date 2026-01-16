2ac2da0f3de6e635239643dd287ae21900ecabc8
flacoman91 <flacoman91@users.noreply.github.com>
2026-01-16 17:59:12 +0000

    Deploy preview for PR 444 🛫

---
+++ pr-previews/pr-444/assets/Divider.stories-Dg97FZ4f.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-Wp9Qcrv3.js";/* empty css                */import{D as i}from"./TextIntroduction-DSW2Zlwc.js";import"./Dropdown-CdQkMC_z.js";import"./Checkbox-uScmMply.js";import"./ExpandableGroup-2Yk9s-Ct.js";import"./Icon-BoyTgjCy.js";import"./Well-gJoKksGL.js";import"./Paragraph-CHJO3p8T.js";import"./TextArea-byJ3EejR.js";import"./index-KmaxkdDD.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DUolvyrz.js";import"./index-livxNL0y.js";import"./iframe-B7HMSWCU.js";const w={title:"Components (Draft)/Divider",component:i,tags:["autodocs"],parameters:{docs:{description:{component:`
+### CFPB DS Divider component
+
+A component to provide a horizontal rule for separating content.
+`}}}},e={},r={args:{className:"u-mb45 u-mt45"},render:c=>o.jsxs(o.Fragment,{children:["Content above",o.jsx(i,{...c}),"Content below"]})};var t,a,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var n,m,p;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    className: 'u-mb45 u-mt45'
+  },
+  render: arguments_ => <>
+      Content above
+      <Divider {...arguments_} />
+      Content below
+    </>
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const F=["Default","ExampleUsage"];export{e as Default,r as ExampleUsage,F as __namedExportsOrder,w as default};
--- pr-previews/pr-444/assets/index-Y2oSI8_S.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CKEOE3cD.js";import"./DocsRenderer-CFRXHY34-DOYhLnLz.js";const{definePreview:i}=__STORYBOOK_MODULE_PREVIEW_API__;
