474c8a72caae9f7e0500ed64675b50377949b9c6
flacoman91 <flacoman91@users.noreply.github.com>
2026-01-20 21:16:27 +0000

    Deploy preview for PR 438 🛫

---
+++ pr-previews/pr-438/assets/Footer.stories-DNGzXAM7.js
@@ -0,0 +1,11 @@
+import{j as n}from"./jsx-runtime-Wp9Qcrv3.js";/* empty css                */import{F as C,d as u}from"./TextIntroduction-C2195LFb.js";import{I as p}from"./Dropdown-C3xHnweU.js";import"./Checkbox-uScmMply.js";import"./Well-gJoKksGL.js";import"./Paragraph-CHJO3p8T.js";import"./TextArea-ZDMBHX8g.js";import"./index-KmaxkdDD.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DUolvyrz.js";import"./index-livxNL0y.js";import"./iframe-DhrzolHZ.js";const G={title:"Components (Draft)/Footers",tags:["autodocs"],component:C,argTypes:{}},o=(i,r)=>n.jsxs("a",{href:window.location.href,className:`m-list__link a-link ${r?"a-link--icon":""}`,children:[n.jsx("span",{className:"a-link__text",children:i}),r?n.jsx(p,{name:"external-link",alt:"External link"}):null]},i),k=i=>n.jsx("a",{href:window.location.href,children:n.jsx(p,{name:i,withBg:!0})},i),e={args:{navLinks:[o("Nav 1"),o("Nav 2"),o("Nav 3")],socialLinks:[k("facebook"),k("youtube")],linksCol1:[o("Col1 Link 1"),o("Col1 Link 2"),o("Col1 Link 3")],linksCol2:[o("Col2 Link 1"),o("Col2 Link 2"),o("Col2 Link 3")],linksCol3:[o("Col3 Link 1",!0),o("Col3 Link 2",!0),o("Col3 Link 3",!0)]}},a={render:()=>n.jsx(u,{})};var s,t,l;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    navLinks: [makeLink('Nav 1'), makeLink('Nav 2'), makeLink('Nav 3')],
+    socialLinks: [makeSocialLink('facebook'), makeSocialLink('youtube')],
+    linksCol1: [makeLink('Col1 Link 1'), makeLink('Col1 Link 2'), makeLink('Col1 Link 3')],
+    linksCol2: [makeLink('Col2 Link 1'), makeLink('Col2 Link 2'), makeLink('Col2 Link 3')],
+    linksCol3: [makeLink('Col3 Link 1', true), makeLink('Col3 Link 2', true), makeLink('Col3 Link 3', true)]
+  }
+}`,...(l=(t=e.parameters)==null?void 0:t.docs)==null?void 0:l.source}}};var m,L,c;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => <FooterCfGov />
+}`,...(c=(L=a.parameters)==null?void 0:L.docs)==null?void 0:c.source}}};const y=["Example","CFGov"];export{a as CFGov,e as Example,y as __namedExportsOrder,G as default};
---
+++ pr-previews/pr-438/assets/index-CIGtiHJB.js
@@ -0,0 +1,1 @@
+import"./preview-Bw-BFJS_.js";import"./DocsRenderer-CFRXHY34-Bs6VRg6Z.js";const{definePreview:i}=__STORYBOOK_MODULE_PREVIEW_API__;
