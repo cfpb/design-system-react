62e40b274f81723b62d974a373d085049803077a
flacoman91 <flacoman91@users.noreply.github.com>
2026-01-23 22:58:36 +0000

    Remove preview for PR 454 🛬

--- pr-previews/pr-454/assets/Breadcrumb.stories-sSaNrEpo.js
+++
@@ -1,9 +0,0 @@
-import{j as s}from"./jsx-runtime-Wp9Qcrv3.js";/* empty css                */import{c as m}from"./TextIntroduction-BVba2Kfp.js";import"./Dropdown-COw_U7iR.js";import"./Checkbox-uScmMply.js";import"./Well-DMVuktKN.js";import"./Paragraph-CHJO3p8T.js";import"./TextArea-Dpxs-BUW.js";import"./index-KmaxkdDD.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DUolvyrz.js";import"./index-XmDEAOo_.js";import"./iframe-Vlv9My4X.js";const B={title:"Components (Draft)/Breadcrumbs",tags:["autodocs"],component:m,argTypes:{}},r={render:a=>s.jsx(m,{...a}),args:{crumbs:[{href:"/example",label:"Page link"}]}};r.storyName="Crumb";var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  render: properties => <Breadcrumb {...properties} />,
-  args: {
-    crumbs: [{
-      href: '/example',
-      label: 'Page link'
-    }]
-  }
-}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const h=["Crumb"];export{r as Crumb,h as __namedExportsOrder,B as default};
--- pr-previews/pr-454/assets/index-Ddwwymsb.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CUyl52Qu.js";import"./DocsRenderer-CFRXHY34-C4OdgOLg.js";const{definePreview:i}=__STORYBOOK_MODULE_PREVIEW_API__;
