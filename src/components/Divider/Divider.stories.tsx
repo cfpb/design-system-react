e614f449b08849f29f1010f61d32528a6dff31db
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-01-23 14:28:24 -0800

    remove divider example 45px

--- src/components/Divider/Divider.stories.tsx
+++ src/components/Divider/Divider.stories.tsx
@@ -2,14 +2,14 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Divider } from '~/src/index';
 
 const meta: Meta<typeof Divider> = {
-  title: 'Components (Draft)/Divider',
+  title: 'Components (Draft)/Dividers',
   component: Divider,
   tags: ['autodocs'],
   parameters: {
     docs: {
       description: {
         component: `
-A component to provide a horizontal rule for separating content.
+Dividers create visual separation between content sections by rendering a horizontal line.
 
 <a href="https://cfpb.github.io/design-system/development/blocks#content-dividers">
 https://cfpb.github.io/design-system/development/blocks#content-dividers
@@ -26,7 +26,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
 
-export const ExampleUsage30px: Story = {
+export const ExampleUsage: Story = {
   args: {
     className: 'u-mb30 u-mt30'
   },
@@ -35,19 +35,6 @@ export const ExampleUsage30px: Story = {
       This content is above the divider by 30px
       <Divider {...arguments_} />
       This content is below the divider by 30px
-    </>
-  )
-};
-
-export const ExampleUsage45px: Story = {
-  args: {
-    className: 'u-mb45 u-mt45'
-  },
-  render: arguments_ => (
-    <>
-      This content is above the divider by 45px
-      <Divider {...arguments_} />
-      This content is below the divider by 45px
     </>
   )
 };
