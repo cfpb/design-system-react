331c17f662716a4f2adee274413904b316c9b694
flacoman91 <flacoman91@users.noreply.github.com>
2026-01-23 15:00:46 +0000

    Deploy preview for PR 454 🛫

---
+++ pr-previews/pr-454/assets/Breadcrumb.stories-Dzwpn0zN.js
@@ -0,0 +1,32 @@
+import{j as c}from"./jsx-runtime-Wp9Qcrv3.js";/* empty css                */import{c as t}from"./TextIntroduction-D2z2XICC.js";import"./Dropdown-CcXW3sd0.js";import"./Checkbox-uScmMply.js";import"./Well-mGyiCRe5.js";import"./Paragraph-CHJO3p8T.js";import"./TextArea-ZDMBHX8g.js";import"./index-KmaxkdDD.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DUolvyrz.js";import"./index-XmDEAOo_.js";import"./iframe-CM1H8Tzm.js";const N={title:"Components (Draft)/Breadcrumb",tags:["autodocs"],component:t,argTypes:{}},e={render:r=>c.jsx(t,{...r}),args:{crumbs:[{href:"/data-research/prepaid-accounts/",label:"Prepaid Account Agreements"}]}};e.storyName="Single crumb";const a={render:r=>c.jsx(t,{...r}),args:{crumbs:[{href:"/data-research/",label:"Data & research"},{href:"/data-research/prepaid-accounts/",label:"Prepaid Account Agreements"}]}};a.storyName="Multiple crumbs";const s={render:r=>c.jsx(t,{...r}),args:{crumbs:[{href:"/data-research/",label:"Data & research"},{href:"/data-research/prepaid-accounts/",label:"Prepaid Account Agreements",isCurrent:!0}]}};s.storyName="Current page (aria-current)";var n,o,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      href: '/data-research/prepaid-accounts/',
+      label: 'Prepaid Account Agreements'
+    }]
+  }
+}`,...(p=(o=e.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};var m,u,i;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      href: '/data-research/',
+      label: 'Data & research'
+    }, {
+      href: '/data-research/prepaid-accounts/',
+      label: 'Prepaid Account Agreements'
+    }]
+  }
+}`,...(i=(u=a.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var d,l,b;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      href: '/data-research/',
+      label: 'Data & research'
+    }, {
+      href: '/data-research/prepaid-accounts/',
+      label: 'Prepaid Account Agreements',
+      isCurrent: true
+    }]
+  }
+}`,...(b=(l=s.parameters)==null?void 0:l.docs)==null?void 0:b.source}}};const E=["SingleCrumb","MultipleCrumbs","CurrentPage"];export{s as CurrentPage,a as MultipleCrumbs,e as SingleCrumb,E as __namedExportsOrder,N as default};
---
+++ pr-previews/pr-454/assets/index-Jt-1-K0Y.js
@@ -0,0 +1,1 @@
+import"./preview-BuXaP6Im.js";import"./DocsRenderer-CFRXHY34-aKnA3gfi.js";const{definePreview:i}=__STORYBOOK_MODULE_PREVIEW_API__;
