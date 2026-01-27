e8b0e8911c56f09b363d69304bbf01501abc9ee2
flacoman91 <flacoman91@users.noreply.github.com>
2026-01-27 21:00:54 +0000

    Deploy preview for PR 455 🛫

---
+++ pr-previews/pr-455/assets/Tagline.stories-BXXcCB6e.js
@@ -0,0 +1,18 @@
+import{j as a}from"./jsx-runtime-Wp9Qcrv3.js";/* empty css                */import{p as m}from"./TextIntroduction-BVB8MwWQ.js";import"./Dropdown-D8ZBRam0.js";import"./Checkbox-uScmMply.js";import"./Well-mGyiCRe5.js";import"./Paragraph-CHJO3p8T.js";import"./TextArea-byJ3EejR.js";import"./index-KmaxkdDD.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DUolvyrz.js";import"./index-XmDEAOo_.js";import"./iframe-B_TC-5CD.js";const N={title:"Components (Verified)/Taglines",tags:["autodocs"],component:m,parameters:{docs:{description:{component:`
+
+Taglines are short paragraphs of text with the USA flag to their left that are used in the header and footer across consumerfinance.gov.
+
+Source: https://cfpb.github.io/design-system/components/taglines
+`}}}},e={render:c=>a.jsx(m,{...c,children:a.jsxs(a.Fragment,{children:["An official website of the"," ",a.jsx("span",{className:"u-nowrap",children:"United States government"})]})})};e.storyName="Standard tagline";const r={...e,args:{isLarge:!0}};r.storyName="Large tagline";var t,n,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: properties => <Tagline {...properties}>
+      <>
+        An official website of the{' '}
+        <span className='u-nowrap'>United States government</span>
+      </>
+    </Tagline>
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var o,i,p;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const v=["StandardTagline","LargeTagline"];export{r as LargeTagline,e as StandardTagline,v as __namedExportsOrder,N as default};
---
+++ pr-previews/pr-455/assets/index-BXGyZYLY.js
@@ -0,0 +1,1 @@
+import"./preview-Dq2jFtOv.js";import"./DocsRenderer-CFRXHY34-C--X9puf.js";const{definePreview:i}=__STORYBOOK_MODULE_PREVIEW_API__;
