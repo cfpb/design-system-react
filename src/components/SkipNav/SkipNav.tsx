3daee223999747629a841df344b2d52f8585eaa2
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-01-27 10:45:30 -0800

    adding jsdoc

--- src/components/SkipNav/SkipNav.tsx
+++ src/components/SkipNav/SkipNav.tsx
@@ -1,6 +1,13 @@
 import type { JSXElement } from '../../types/jsxElement';
 import './SkipNav.scss';
 
+/**
+ * A skipnav (skip navigation) link is an accessibility feature, usually hidden
+ * until focused, placed at the top of a webpage to allow keyboard and screen
+ * reader users to bypass repetitive navigation menus and jump directly to the
+ * main content. It acts as an anchor link, enhancing user experience and
+ * efficiency.
+ */
 export default function SkipNav({
   href = '#main',
   text = 'Skip to main content'
