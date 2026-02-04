47bc96aaffacc57f44e918deda12c03f2a82fc90
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-02-03 17:59:24 -0800

    updating docs for Link component to match ds

--- src/components/Link/Link.stories.tsx
+++ src/components/Link/Link.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { BrowserRouter } from 'react-router-dom';
 import {
   DestructiveLink,
+  Heading,
   Icon,
   Link,
   LinkText,
@@ -27,7 +28,17 @@ const DefaultArguments = {
 };
 
 export const Inline: Story = {
-  name: 'Inline links',
+  name: 'Inline',
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Inline links appear within a line of text or other inline elements. ' +
+          'Inline links are underlined and retain a consistent appearance across ' +
+          'all screen widths.'
+      }
+    }
+  },
   render: () => (
     <p>
       Here&apos;s the <Link href='/#'>default link style</Link>. For reference,
@@ -52,6 +63,91 @@ export const Inline: Story = {
   )
 };
 
+export const Standalone: Story = {
+  name: 'Standalone',
+  args: {
+    ...DefaultArguments.args,
+    isJump: true
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Standalone links sit on their own line. At larger screen widths, ' +
+          'standalone links are medium weight and underlined. At smaller ' +
+          'screen widths, standalone links convert to full block links with ' +
+          'top and bottom borders that have a finger-friendly touch area.'
+      }
+    }
+  },
+  render: arguments_ => (
+    <Link {...arguments_}>
+      <LinkText>Standalone link</LinkText>
+    </Link>
+  )
+};
+
+export const WithIcon: Story = {
+  name: 'With icon',
+  args: {
+    ...DefaultArguments.args,
+    hasIcon: true
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Each icon should be used exclusively and consistently for one ' +
+          'action. An icon should appear after the text it represents. The ' +
+          'only exception is a link that takes a user to a previous step or ' +
+          'page, where the icon should appear before the link’s text. Icons ' +
+          'should never be underlined.'
+      }
+    }
+  },
+  render: arguments_ => (
+    <>
+      <Heading type='4'>Inline</Heading>
+      <p>
+        The document icon should emphasize a link that contains a{' '}
+        <Link {...arguments_}>
+          <LinkText>file or document</LinkText> <Icon name='download' />
+        </Link>
+        . The external link icon is used to emphasize a link to a{' '}
+        <Link {...arguments_}>
+          <LinkText>a non-CFPB webpage</LinkText> <Icon name='external-link' />
+        </Link>
+        .
+      </p>
+      <Heading type='4'>Standalone</Heading>
+      <p>
+        <Link hasIcon isJump href='https://www.example.com'>
+          <Icon name='left' />
+          <LinkText>Go back</LinkText>
+        </Link>
+      </p>
+      <p>
+        <Link hasIcon isJump href='https://www.example.com'>
+          <LinkText>Continue</LinkText>
+          <Icon name='right' />
+        </Link>
+      </p>
+      <p>
+        <Link hasIcon isJump href='https://www.example.com'>
+          <LinkText>External link</LinkText>
+          <Icon name='external-link' />
+        </Link>
+      </p>
+      <p>
+        <Link hasIcon isJump href='https://www.example.com'>
+          <LinkText>Document or file</LinkText>
+          <Icon name='document' />
+        </Link>
+      </p>
+    </>
+  )
+};
+
 export const ListLinks: Story = {
   name: 'List links',
   args: {
@@ -60,10 +156,13 @@ export const ListLinks: Story = {
   render: arguments_ => (
     <List isLinks>
       <ListLink {...arguments_}>
-        <LinkText>List link 1</LinkText>
+        <LinkText>List item 1</LinkText>
       </ListLink>
       <ListLink {...arguments_}>
-        <LinkText>List link 2</LinkText>
+        <LinkText>List item 2</LinkText>
+      </ListLink>
+      <ListLink {...arguments_}>
+        <LinkText>List item 3</LinkText>
       </ListLink>
     </List>
   )
@@ -75,73 +174,19 @@ export const Destructive: Story = {
     ...DefaultArguments.args,
     children: 'Destructive link'
   },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'A destructive action is an action that will lead to data loss, ' +
+          'for example, deleting data from a form or removing a file that ' +
+          'was previously uploaded. Although a destructive action can be ' +
+          'styled as a button, we recommend using the destructive link ' +
+          'style (shown below) to ensure consistency across cf.gov products.'
+      }
+    }
+  },
   render: arguments_ => <DestructiveLink {...arguments_} />
-};
-
-export const LinkWithIcon: Story = {
-  name: 'Link with icon',
-  args: {
-    ...DefaultArguments.args,
-    hasIcon: true
-  },
-  render: arguments_ => (
-    <>
-      The document icon should emphasize a link that contains a{' '}
-      <Link {...arguments_}>
-        <LinkText>file or document</LinkText> <Icon name='download' />
-      </Link>
-      . Use the external link icon to emphasize{' '}
-      <Link {...arguments_}>
-        <LinkText>a non-CFPB webpage</LinkText> <Icon name='external-link' />
-      </Link>{' '}
-      for further details.
-    </>
-  )
-};
-
-export const LinkWithIconNoWrapping: Story = {
-  name: 'Non-wrapping link with icon',
-  args: {
-    ...LinkWithIcon.args,
-    noWrap: true
-  },
-  render: arguments_ => (
-    <>
-      The document icon should emphasize a link that contains a{' '}
-      <Link {...arguments_}>
-        <LinkText>file or document</LinkText> <Icon name='download' />
-      </Link>
-      .
-    </>
-  )
-};
-
-export const JumpLink: Story = {
-  name: 'Jump link',
-  args: {
-    ...DefaultArguments.args,
-    isJump: true
-  },
-  render: arguments_ => (
-    <Link {...arguments_}>
-      <LinkText>Jump link</LinkText>
-      <Icon name='right' />
-    </Link>
-  )
-};
-
-export const JumpLinkIconLeft: Story = {
-  name: 'Jump link with icon on left',
-  args: {
-    ...DefaultArguments.args,
-    isJump: true
-  },
-  render: arguments_ => (
-    <Link {...arguments_}>
-      <Icon name='left' />
-      <LinkText>Jump link with icon on left</LinkText>
-    </Link>
-  )
 };
 
 export const LinkWithReactRouterLink: Story = {
@@ -150,7 +195,8 @@ export const LinkWithReactRouterLink: Story = {
     docs: {
       description: {
         story:
-          'See [React Router Link docs](https://reactrouter.com/en/main/components/link) for usage information'
+          'See [React Router Link docs](https://reactrouter.com/en/main/components/link) ' +
+          'for usage information'
       }
     }
   },
