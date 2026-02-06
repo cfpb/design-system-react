ce7653569a881f07d291fb8f11678410eb291492
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-06 05:12:54 +0000

    Deploy preview for PR 452 🛫

---
+++ pr-previews/pr-452/assets/Tagline.stories-CaKrQUEY.js
@@ -0,0 +1,18 @@
+import{j as a}from"./jsx-runtime-BLchON5X.js";/* empty css                */import{Q as t}from"./Well-BsFFX5va.js";import"./Checkbox-C8PZvyyp.js";import"./Paragraph-CSOJQ8Ca.js";import"./TextArea-lN4SEnvj.js";import"./index-kA4PVysc.js";import"./index-6tNKdD3S.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-D_VgsJHj.js";import"./iframe-DWDNXMOZ.js";const h={title:"Components (Verified)/Taglines",tags:["autodocs"],component:t,parameters:{docs:{description:{component:`
+
+Taglines are short paragraphs of text with the USA flag to their left that are used in the header and footer across consumerfinance.gov.
+
+Source: https://cfpb.github.io/design-system/components/taglines
+`}}}},e={render:n=>a.jsx(t,{...n,children:a.jsxs(a.Fragment,{children:["An official website of the"," ",a.jsx("span",{className:"u-nowrap",children:"United States government"})]})})};e.storyName="Standard tagline";const r={...e,args:{isLarge:!0}};r.storyName="Large tagline";e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: properties => <Tagline {...properties}>
+      <>
+        An official website of the{' '}
+        <span className='u-nowrap'>United States government</span>
+      </>
+    </Tagline>
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...r.parameters?.docs?.source}}};const S=["StandardTagline","LargeTagline"];export{r as LargeTagline,e as StandardTagline,S as __namedExportsOrder,h as default};
---
+++ pr-previews/pr-452/assets/index-D6MTzMBE.js
@@ -0,0 +1,1 @@
+import"./preview-kfDJSVNM.js";import"./DocsRenderer-CFRXHY34-CbfwSyUo.js";const{definePreview:i}=__STORYBOOK_MODULE_PREVIEW_API__;
