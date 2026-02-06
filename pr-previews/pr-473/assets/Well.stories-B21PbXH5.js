6528b42fe1a5f0a9649c8d9f01dc1b32b42abd50
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-06 22:08:35 +0000

    Deploy preview for PR 473 🛫

---
+++ pr-previews/pr-473/assets/Well.stories-B21PbXH5.js
@@ -0,0 +1,12 @@
+import{j as n}from"./jsx-runtime-BLchON5X.js";import{W as r,V as i,h as a}from"./Well-CnCh6sZN.js";import"./index-kA4PVysc.js";import"./index-6tNKdD3S.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-D_VgsJHj.js";import"./TextArea-Ddz0oDhE.js";/* empty css                */import"./iframe-JUPStrlu.js";import"./Checkbox-C8PZvyyp.js";import"./Paragraph-CSOJQ8Ca.js";const W={title:"Components (Verified)/Wells",tags:["autodocs"],component:r,argTypes:{children:{type:"string"}}},e={name:"Well (container)"},t={name:"Well (content)",args:{heading:"Heading",headingLevel:"h3",text:"Lorem ipsum dolor sit amet, ei ius adhuc inani iudico, labitur instructior ex pri. Cu pri inani constituto, cum aeque noster commodo",links:[n.jsx(a,{href:"/#",label:"Call-to-action link"},"example1")]},render:o=>n.jsx(i,{...o})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: 'Well (container)'
+}`,...e.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  name: 'Well (content)',
+  args: {
+    heading: 'Heading',
+    headingLevel: 'h3',
+    text: 'Lorem ipsum dolor sit amet, ei ius adhuc inani iudico, labitur instructior ex pri. Cu pri inani constituto, cum aeque noster commodo',
+    links: [<ListLink href='/#' key='example1' label='Call-to-action link' />]
+  },
+  render: arguments_ => <WellContent {...arguments_} />
+}`,...t.parameters?.docs?.source}}};const L=["Container","Content"];export{e as Container,t as Content,L as __namedExportsOrder,W as default};
---
+++ pr-previews/pr-473/assets/index-CWcH0SIH.js
@@ -0,0 +1,1 @@
+import"./preview-SfsODl5Y.js";import"./DocsRenderer-CFRXHY34-CZoVRgeY.js";const{definePreview:i}=__STORYBOOK_MODULE_PREVIEW_API__;
