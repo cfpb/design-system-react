d3289b6ef457c799152d67b657e9fef903de2754
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-06 23:39:32 +0000

    Deploy preview for PR 482 🛫

---
+++ pr-previews/pr-482/assets/PageHeader.stories-E9CKcojv.js
@@ -0,0 +1,10 @@
+import"./iframe-C_5WM9JR.js";import{E as o,P as s}from"./Well-CGVsAuv8.js";import"./Checkbox-DYXARMN6.js";import"./Paragraph-AhOD15To.js";import"./TextArea-CI5-FUk8.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-CeNSEPfi.js";const l={title:"Components (Draft)/Page header",tags:["autodocs"],component:s,argTypes:{}},a={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},e={args:{links:o,user:a}},r={args:{links:o}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const u=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,u as __namedExportsOrder,l as default};
---
+++ pr-previews/pr-482/assets/formatter-EIJCOSYU-DrD158Fo.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-CzV0_BlT.js";import{b as t}from"./iframe-C_5WM9JR.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DNJ3AYWe.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
