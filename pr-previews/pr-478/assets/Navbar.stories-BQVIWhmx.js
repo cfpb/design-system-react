fbc2d89914bf7359116276eb92e4c46414a85c72
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-06 23:03:16 +0000

    Remove preview for PR 478 🛬

--- pr-previews/pr-478/assets/Navbar.stories-BQVIWhmx.js
+++
@@ -1,9 +0,0 @@
-/* empty css                */import{N as e,E as a}from"./Well-XJQj6jH-.js";import"./Checkbox-bkZbx_rd.js";import"./Paragraph-DvazyV92.js";import"./TextArea-DilN63vT.js";import"./jsx-runtime-u17CrQMm.js";import"./index-6tNKdD3S.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BAD8l0rO.js";import"./iframe-D1T_t9ZN.js";const L={title:"Components (Draft)/Navbar",tags:["autodocs"],component:e,argTypes:{}},r={args:{links:a}},s={args:{}},o={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...o.parameters?.docs?.source}}};const N=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,o as NoUser,N as __namedExportsOrder,L as default};
--- pr-previews/pr-478/assets/index-BbxN3AYP.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CJTvsos1.js";import"./DocsRenderer-CFRXHY34-CY8rXhRg.js";const{definePreview:i}=__STORYBOOK_MODULE_PREVIEW_API__;
