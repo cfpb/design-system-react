4029cc9d7e51354900fe78d5cf845e9d05d479c7
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-07 00:08:30 +0000

    Deploy preview for PR 481 🛫

---
+++ pr-previews/pr-481/assets/PageHeader.stories-DyteuUfV.js
@@ -0,0 +1,10 @@
+import"./iframe-COr090Aj.js";import{E as o,P as s}from"./Well-BTR_rvVR.js";import"./Checkbox-BzlyeYrr.js";import"./Paragraph-lzUqpfh3.js";import"./TextArea-DA-XWu0K.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-akr4av0X.js";const l={title:"Components (Draft)/Page header",tags:["autodocs"],component:s,argTypes:{}},a={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},e={args:{links:o,user:a}},r={args:{links:o}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const u=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,u as __namedExportsOrder,l as default};
