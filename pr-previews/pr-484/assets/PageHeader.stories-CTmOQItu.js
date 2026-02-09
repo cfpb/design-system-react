a871933f77e204af32012adedee1748f15caab18
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-09 15:00:14 +0000

    Deploy preview for PR 484 🛫

---
+++ pr-previews/pr-484/assets/PageHeader.stories-CTmOQItu.js
@@ -0,0 +1,10 @@
+import"./iframe-DR5cCYxv.js";import{E as o,P as s}from"./Well-DcHbEZrb.js";import"./Checkbox-CzlagvtO.js";import"./Paragraph-BnY8lner.js";import"./TextArea-BTMJpzYl.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-Bwy_2akt.js";const l={title:"Components (Draft)/Page header",tags:["autodocs"],component:s,argTypes:{}},a={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},e={args:{links:o,user:a}},r={args:{links:o}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const u=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,u as __namedExportsOrder,l as default};
