e4c6da3dbc38fd6a29477c5d91fb81b5ae7f8d52
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-10 13:50:53 +0000

    Remove preview for PR 483 🛬

--- pr-previews/pr-483/assets/Navbar.stories-C_5HlYn4.js
+++
@@ -1,9 +0,0 @@
-import"./iframe-Bbqw4Xdb.js";import{E as a,N as o}from"./Well-DgXLSOkc.js";import"./Checkbox-CGiiA-sq.js";import"./Paragraph-4O_4_CDV.js";import"./TextArea-DpLMYo3U.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-DosWk5ot.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
--- pr-previews/pr-483/assets/ResponsiveMenu.stories-DHPEdhFX.js
+++
@@ -1,9 +0,0 @@
-import"./iframe-Bbqw4Xdb.js";import{z as o,C as a}from"./Well-DgXLSOkc.js";import"./Checkbox-CGiiA-sq.js";import"./Paragraph-4O_4_CDV.js";import"./TextArea-DpLMYo3U.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-DosWk5ot.js";const u={title:"Components (Draft)/ResponsiveMenu",tags:["autodocs"],component:a,argTypes:{}},r={args:{links:o}},e={args:{}},s={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,e as LoggedOut,s as NoUser,l as __namedExportsOrder,u as default};
