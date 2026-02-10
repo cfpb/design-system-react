2f61f101ca2d9f1146b6ae5a4f1f6e8b1c74b964
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-10 17:56:00 +0000

    Remove preview for PR 485 🛬

--- pr-previews/pr-485/assets/navbar.stories-BYETvRCP.js
+++
@@ -1,9 +0,0 @@
-import"./iframe-3jGGAWKC.js";import{E as a,N as o}from"./well-Cv_PinXS.js";import"./checkbox-DxgqjU_B.js";import"./paragraph-ntGEbj4Q.js";import"./text-area-B2JZ5ajy.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-D5I_OEUa.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
