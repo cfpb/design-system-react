959dc1ffbc1adad887dabe5ae1ee4ede4375dbe9
Natalia Fitzgerald <77348967+natalia-fitzgerald@users.noreply.github.com>
2026-02-10 16:54:03 -0500

    Update List.stories.tsx

--- src/components/List/List.stories.tsx
+++ src/components/List/List.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { LinkText, List, ListItem, ListLink } from '~/src/index';
 
 const meta: Meta<typeof List> = {
