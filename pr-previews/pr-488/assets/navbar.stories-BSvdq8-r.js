a21d3f03d8f3c383b52fdb680adcf765c77b0dda
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-10 20:36:04 +0000

    Remove preview for PR 488 🛬

--- pr-previews/pr-488/assets/navbar.stories-BSvdq8-r.js
+++
@@ -1,9 +0,0 @@
-import"./iframe-CnDuvxnQ.js";import{E as a,N as o}from"./well-D1arDNMP.js";import"./checkbox-IstnveaU.js";import"./paragraph-DOkvH-Gg.js";import"./text-area-CnzjOCIN.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-bnzT29YK.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
