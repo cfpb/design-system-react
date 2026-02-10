daff119e842902f7c4925fece71af5fe68c743b6
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-10 00:54:50 +0000

    Deploy preview for PR 484 🛫

---
+++ pr-previews/pr-484/assets/Navbar.stories-DRqbH5JS.js
@@ -0,0 +1,9 @@
+import"./iframe-C_6lVzEB.js";import{E as a,N as o}from"./Well-Cbh-8ADm.js";import"./Checkbox-CvGyiEif.js";import"./Paragraph-ZvdCECWQ.js";import"./TextArea-BNgS7B6i.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-CcnEGSut.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
