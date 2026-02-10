9179cf6e92c493d3f29949aed428bd2abeb7778d
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-10 16:43:07 +0000

    Deploy preview for PR 485 🛫

---
+++ pr-previews/pr-485/assets/checkbox-overview-CnTliHwo.js
@@ -0,0 +1,9 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as c}from"./index-CGM-wLQQ.js";import{M as a}from"./blocks-BT0s5bdT.js";import"./iframe-3jGGAWKC.js";import{H as o,c as h,d as p,e as n}from"./well-Cv_PinXS.js";import"./checkbox-DxgqjU_B.js";import{P as r}from"./paragraph-ntGEbj4Q.js";import"./text-area-B2JZ5ajy.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DHqFad-p.js";import"./index-D5I_OEUa.js";function i(t){const s={a:"a",...c(),...t.components};return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(a,{title:"Components (Verified)/Checkboxes/Overview"}),`
+`,`
+`,e.jsx(o,{type:"1",children:"Checkboxes"}),`
+`,e.jsxs(r,{children:["Use checkboxes when the user can select more than one option from a list. Make clear with helper text that this is the case. Use ",e.jsx(s.a,{href:"https://cfpb.github.io/design-system/components/radio-buttons",rel:"nofollow",children:"radio buttons"})," when the user can choose only one option from a list."]}),`
+`,e.jsxs(r,{children:["Source: ",e.jsx(h,{href:"https://cfpb.github.io/design-system/components/checkboxes",children:"https://cfpb.github.io/design-system/components/checkboxes"})]}),`
+`,e.jsx("br",{}),`
+`,e.jsxs("div",{className:"sb-unstyled",children:[e.jsx(o,{type:"4",children:"Types"}),e.jsxs(p,{isLink:!0,children:[e.jsx(n,{target:"_top",href:"./?path=/docs/components-verified-checkboxes-checkbox--overview",label:"Checkbox"}),e.jsx(n,{target:"_top",href:"./?path=/docs/components-verified-checkboxes-large-target-area--overview",label:"Large target area"})]})]})]})}function y(t={}){const{wrapper:s}={...c(),...t.components};return s?e.jsx(s,{...t,children:e.jsx(i,{...t})}):i(t)}export{y as default};
---
+++ pr-previews/pr-485/assets/formatter-EIJCOSYU-Dp4E_oPN.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-BT0s5bdT.js";import{b as t}from"./iframe-3jGGAWKC.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DHqFad-p.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
