496092da507ca6d93ee9fe72ee98a182b77acb96
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-10 20:24:00 +0000

    Remove preview for PR 486 🛬

--- pr-previews/pr-486/assets/responsive-menu.stories-Cxx6pAkj.js
+++
@@ -1,9 +0,0 @@
-import"./iframe-BlQyTykr.js";import{z as o,C as a}from"./well-KydzX0oJ.js";import"./checkbox-CLbB4kX5.js";import"./paragraph-CdTAbASJ.js";import"./text-area--5xze90Q.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-D3a4Shrj.js";const u={title:"Components (Draft)/ResponsiveMenu",tags:["autodocs"],component:a,argTypes:{}},r={args:{links:o}},e={args:{}},s={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,e as LoggedOut,s as NoUser,l as __namedExportsOrder,u as default};
