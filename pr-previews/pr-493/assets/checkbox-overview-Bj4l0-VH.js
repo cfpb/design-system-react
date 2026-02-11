9dcf03f99a2d20ad0ba0dd061045fa755411eea4
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-11 00:13:53 +0000

    Deploy preview for PR 493 🛫

--- pr-previews/pr-493/assets/checkbox-overview-Bj4l0-VH.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as c}from"./index-UnkciYQM.js";import{M as a}from"./blocks-BTwr1jW8.js";import"./iframe-DQhvA41i.js";import{H as o,c as h,d as p,e as n}from"./well-BQF_3T9l.js";import"./checkbox-C_Vy1lyO.js";import{P as r}from"./paragraph-B-Xl_xRa.js";import"./text-area-CsV7B_F8.js";import"./preload-helper-PPVm8Dsz.js";import"./index-lNeIqLdp.js";import"./index-CXhNCxT2.js";function i(t){const s={a:"a",...c(),...t.components};return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(a,{title:"Components (Verified)/Checkboxes/Overview"}),`
-`,`
-`,e.jsx(o,{type:"1",children:"Checkboxes"}),`
-`,e.jsxs(r,{children:["Use checkboxes when the user can select more than one option from a list. Make clear with helper text that this is the case. Use ",e.jsx(s.a,{href:"https://cfpb.github.io/design-system/components/radio-buttons",rel:"nofollow",children:"radio buttons"})," when the user can choose only one option from a list."]}),`
-`,e.jsxs(r,{children:["Source: ",e.jsx(h,{href:"https://cfpb.github.io/design-system/components/checkboxes",children:"https://cfpb.github.io/design-system/components/checkboxes"})]}),`
-`,e.jsx("br",{}),`
-`,e.jsxs("div",{className:"sb-unstyled",children:[e.jsx(o,{type:"4",children:"Types"}),e.jsxs(p,{isLink:!0,children:[e.jsx(n,{target:"_top",href:"./?path=/docs/components-verified-checkboxes-checkbox--overview",label:"Checkbox"}),e.jsx(n,{target:"_top",href:"./?path=/docs/components-verified-checkboxes-large-target-area--overview",label:"Large target area"})]})]})]})}function y(t={}){const{wrapper:s}={...c(),...t.components};return s?e.jsx(s,{...t,children:e.jsx(i,{...t})}):i(t)}export{y as default};
--- pr-previews/pr-493/assets/navbar.stories-BBWUu4Be.js
+++
@@ -1,9 +0,0 @@
-import"./iframe-DQhvA41i.js";import{E as a,N as o}from"./well-BQF_3T9l.js";import"./checkbox-C_Vy1lyO.js";import"./paragraph-B-Xl_xRa.js";import"./text-area-CsV7B_F8.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-CXhNCxT2.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
