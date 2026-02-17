6992bc5dc731319746cd651dba2f149b7da44a2e
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-17 22:38:41 +0000

    Deploy preview for PR 495 🛫

---
+++ pr-previews/pr-495/assets/page-header.stories-joIWATay.js
@@ -0,0 +1,10 @@
+import"./iframe-OPJJixt-.js";import{E as o,P as s}from"./well-Ewn4rYO8.js";import"./checkbox-CH1Eu_JR.js";import"./paragraph-DHrVEz_C.js";import"./text-area-BZNapIpi.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-r_OtKnhl.js";const l={title:"Components (Draft)/Page header",tags:["autodocs"],component:s,argTypes:{}},a={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},e={args:{links:o,user:a}},r={args:{links:o}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const u=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,u as __namedExportsOrder,l as default};
---
+++ pr-previews/pr-495/assets/tagline.stories-DhH6iblJ.js
@@ -0,0 +1,18 @@
+import{j as a}from"./jsx-runtime-u17CrQMm.js";import"./iframe-OPJJixt-.js";import{h as t}from"./well-Ewn4rYO8.js";import"./checkbox-CH1Eu_JR.js";import"./paragraph-DHrVEz_C.js";import"./text-area-BZNapIpi.js";import"./preload-helper-PPVm8Dsz.js";import"./index-r_OtKnhl.js";const l={title:"Components (Verified)/Taglines",tags:["autodocs"],component:t,parameters:{docs:{description:{component:`
+
+Taglines are short paragraphs of text with the USA flag to their left that are used in the header and footer across consumerfinance.gov.
+
+Source: https://cfpb.github.io/design-system/components/taglines
+`}}}},e={render:n=>a.jsx(t,{...n,children:a.jsxs(a.Fragment,{children:["An official website of the"," ",a.jsx("span",{className:"u-nowrap",children:"United States government"})]})})};e.storyName="Standard tagline";const r={...e,args:{isLarge:!0}};r.storyName="Large tagline";e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: properties => <Tagline {...properties}>
+      <>
+        An official website of the{' '}
+        <span className='u-nowrap'>United States government</span>
+      </>
+    </Tagline>
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...r.parameters?.docs?.source}}};const u=["StandardTagline","LargeTagline"];export{r as LargeTagline,e as StandardTagline,u as __namedExportsOrder,l as default};
