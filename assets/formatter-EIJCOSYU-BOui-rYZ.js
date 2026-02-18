fded603c75ad5520f99807781ec1692d5fcf3dc8
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-18 19:01:07 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@e5fa67f59b4350ffede6eb6ea84fa991c23a693e 🚀

---
+++ assets/formatter-EIJCOSYU-BOui-rYZ.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-C4XQjzpP.js";import{b as t}from"./iframe-BkiKjuFC.js";import"./preload-helper-lKO2eUTi.js";import"./index-BbtEu2jT.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
