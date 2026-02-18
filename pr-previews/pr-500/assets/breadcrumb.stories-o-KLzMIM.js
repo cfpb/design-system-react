1e7a3519763eeae21f9d517cb805bb54bc5d3258
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-18 22:05:34 +0000

    Deploy preview for PR 500 🛫

---
+++ pr-previews/pr-500/assets/breadcrumb.stories-o-KLzMIM.js
@@ -0,0 +1,21 @@
+import{j as n}from"./jsx-runtime-u17CrQMm.js";import"./iframe-CZtzQfmA.js";import{l as s}from"./well-2d6V_mQu.js";import"./checkbox-0fmNZE4P.js";import"./paragraph-6o23eEkU.js";import"./text-area-0CNIanS0.js";import"./preload-helper-PPVm8Dsz.js";import"./index-5LqoGjt6.js";const d={title:"Components (Draft)/Breadcrumbs",tags:["autodocs"],component:s,argTypes:{}},e={name:"Breadcrumb",render:a=>n.jsx(s,{...a}),args:{crumbs:[{href:"/example",label:"Page link"},{href:"/example2",label:"Page link"}]}},r={render:a=>n.jsx(s,{...a}),args:{crumbs:[{href:"/example",label:"Page link"}]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: 'Breadcrumb',
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      href: '/example',
+      label: 'Page link'
+    }, {
+      href: '/example2',
+      label: 'Page link'
+    }]
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      href: '/example',
+      label: 'Page link'
+    }]
+  }
+}`,...r.parameters?.docs?.source}}};const b=["Default","Crumb"];export{r as Crumb,e as Default,b as __namedExportsOrder,d as default};
---
+++ pr-previews/pr-500/assets/navbar.stories-CFcuCv8_.js
@@ -0,0 +1,9 @@
+import"./iframe-CZtzQfmA.js";import{E as a,N as o}from"./well-2d6V_mQu.js";import"./checkbox-0fmNZE4P.js";import"./paragraph-6o23eEkU.js";import"./text-area-0CNIanS0.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-5LqoGjt6.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
