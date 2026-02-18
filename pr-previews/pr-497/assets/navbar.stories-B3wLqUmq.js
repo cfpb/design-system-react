d983d420dabed7b1fa39c2878a0693d8252507fb
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-18 18:56:19 +0000

    Deploy preview for PR 497 🛫

---
+++ pr-previews/pr-497/assets/navbar.stories-B3wLqUmq.js
@@ -0,0 +1,9 @@
+import"./iframe-DTs8InHX.js";import{E as a,N as o}from"./well-ISkZuL_7.js";import"./checkbox-DJpVQ79g.js";import"./paragraph-DueNnGIB.js";import"./text-area-BUNNmfHy.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-LjdQFPNG.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
---
+++ pr-previews/pr-497/assets/paragraphs.stories-DfaNRR7O.js
@@ -0,0 +1,12 @@
+import{P as o}from"./paragraph-DueNnGIB.js";import"./jsx-runtime-u17CrQMm.js";import"./index-LjdQFPNG.js";import"./iframe-DTs8InHX.js";import"./preload-helper-PPVm8Dsz.js";const m={title:"Components (Verified)/Paragraphs",tags:["autodocs"],component:o},e={name:"Body text",args:{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore."}},i={name:"Lead paragraph",args:{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",isLead:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: 'Body text',
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.'
+  }
+}`,...e.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  name: 'Lead paragraph',
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    isLead: true
+  }
+}`,...i.parameters?.docs?.source}}};const d=["Body","Lead"];export{e as Body,i as Lead,d as __namedExportsOrder,m as default};
