7e4dcbd98e03937df77b831c5527c3b271f7a5a5
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-18 21:19:44 +0000

    Deploy preview for PR 500 🛫

---
+++ pr-previews/pr-500/assets/navbar.stories-BaEyMais.js
@@ -0,0 +1,9 @@
+import"./iframe-ClpbJREE.js";import{E as a,N as o}from"./well-DAqAVGGj.js";import"./checkbox-Bl1uHAQi.js";import"./paragraph-2Oo5DeWf.js";import"./text-area-C1-ZTMur.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-CO96v2fo.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
