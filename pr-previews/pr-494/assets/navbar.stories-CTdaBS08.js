6fcf2018906689da4c2f450bfc4e755f877fffe9
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-18 17:10:23 +0000

    Remove preview for PR 494 🛬

--- pr-previews/pr-494/assets/navbar.stories-CTdaBS08.js
+++
@@ -1,9 +0,0 @@
-import"./iframe-DK6GwZnm.js";import{E as a,N as o}from"./well-BkqrN2CJ.js";import"./checkbox-DPyG8Ghr.js";import"./paragraph-Bq8faYWh.js";import"./text-area-COJ-tAPp.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BzUj7vgA.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
