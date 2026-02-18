88745c22c0fd23a4a2e4902818b4d4dedb7f8b82
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-18 18:55:05 +0000

    Deploy preview for PR 498 🛫

---
+++ pr-previews/pr-498/assets/list.stories-BzFsW6Y4.js
@@ -0,0 +1,43 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import"./iframe-C7CLD7XE.js";import{d as o,y as i,c as d}from"./well-BY4lLWP9.js";import"./checkbox-DTrXWhF1.js";import"./paragraph-DvBI9xe3.js";import"./text-area-BpN4y1Ed.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BZODDVJY.js";const f={title:"Components (Draft)/Lists",component:o,argTypes:{},tags:["autodocs"],parameters:{docs:{description:{component:`
+Lists are an effective way to visually highlight important information so that it can be more easily scanned and read. Before writing a list, it’s important to identify the best style needed for the information being presented.
+
+Source: https://cfpb.github.io/design-system/components/lists
+`}}}},r={name:"Unordered",args:{children:e.jsxs(e.Fragment,{children:[e.jsx(i,{children:"List item 1"}),e.jsx(i,{children:"List item 2"}),e.jsx(i,{children:"List item 3"})]})}},s={name:"Ordered",args:{...r.args,isOrdered:!0}},t={name:"Unstyled",args:{...r.args,isUnstyled:!0}},n={name:"Horizontal",args:{...r.args,isHorizontal:!0}},a={name:"Link",args:{isLinks:!0,children:e.jsxs(e.Fragment,{children:["The"," ",e.jsx(d,{label:"list links",href:"./?path=/docs/components-verified-links--listlink"})," ","component presents each list item as a standalone link, which converts to a touch-friendly link with a large tap area at smaller screen widths."]})}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  name: 'Unordered',
+  args: {
+    children: <>
+        <ListItem>List item 1</ListItem>
+        <ListItem>List item 2</ListItem>
+        <ListItem>List item 3</ListItem>
+      </>
+  }
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  name: 'Ordered',
+  args: {
+    ...Unordered.args,
+    isOrdered: true
+  }
+}`,...s.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  name: 'Unstyled',
+  args: {
+    ...Unordered.args,
+    isUnstyled: true
+  }
+}`,...t.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  name: 'Horizontal',
+  args: {
+    ...Unordered.args,
+    isHorizontal: true
+  }
+}`,...n.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  name: 'Link',
+  args: {
+    isLinks: true,
+    children: <>
+        The{' '}
+        <Link label='list links' href='./?path=/docs/components-verified-links--listlink' />{' '}
+       component presents each list item as a standalone link, which converts
+        to a touch-friendly link with a large tap area at smaller screen widths.
+         </>
+  }
+}`,...a.parameters?.docs?.source}}};const k=["Unordered","Ordered","Unstyled","Horizontal","Links"];export{n as Horizontal,a as Links,s as Ordered,r as Unordered,t as Unstyled,k as __namedExportsOrder,f as default};
