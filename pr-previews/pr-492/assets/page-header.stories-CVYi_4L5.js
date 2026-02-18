50978aea0045da2616375ca7bb66d26fb3c9782b
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-18 22:36:26 +0000

    Deploy preview for PR 492 🛫

---
+++ pr-previews/pr-492/assets/page-header.stories-CVYi_4L5.js
@@ -0,0 +1,10 @@
+import"./iframe-3X7PRrqC.js";import{E as o,P as s}from"./well-LiwVMie0.js";import"./checkbox-cIG052V7.js";import"./paragraph-cTLXnCif.js";import"./text-area-D_x4j5zM.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-Dx0x_lvz.js";const l={title:"Components (Draft)/Page header",tags:["autodocs"],component:s,argTypes:{}},a={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},e={args:{links:o,user:a}},r={args:{links:o}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const u=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,u as __namedExportsOrder,l as default};
---
+++ pr-previews/pr-492/assets/select-multi.stories-CxiwIrfh.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-u17CrQMm.js";import"./iframe-3X7PRrqC.js";import{S as r}from"./well-LiwVMie0.js";import"./checkbox-cIG052V7.js";import"./paragraph-cTLXnCif.js";import"./text-area-D_x4j5zM.js";import{M as s}from"./test-utils-CMDZRPRi.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Dx0x_lvz.js";const b={title:"Components (Draft)/Selects/Multiselect",component:r,tags:["autodocs"],argTypes:{disabled:{control:"boolean"},isMulti:{control:"boolean"}}},e={args:{id:"multiSelect",label:"Label",isMulti:!0,options:s,disabled:!0},render:o=>t.jsx("div",{style:{minHeight:"200px"},children:t.jsx(r,{...o})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: 'multiSelect',
+    label: 'Label',
+    isMulti: true,
+    options: MultipleSelectOptions,
+    disabled: true
+  },
+  render: arguments_ => <div style={{
+    minHeight: '200px'
+  }}>
+      <Select {...arguments_} />
+    </div>
+}`,...e.parameters?.docs?.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,b as default};
