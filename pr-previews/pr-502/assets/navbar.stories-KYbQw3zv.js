8c8561bb038648c41b982b4f6005ef57267f47f2
natalia-fitzgerald <natalia-fitzgerald@users.noreply.github.com>
2026-02-19 18:32:13 +0000

    Deploy preview for PR 502 🛫

--- pr-previews/pr-502/assets/navbar.stories-KYbQw3zv.js
+++
@@ -1,9 +0,0 @@
-import"./iframe-hq81_UT3.js";import{E as a,N as o}from"./well-CANPvQjx.js";import"./checkbox-DJJheoR4.js";import"./paragraph-CP94D1mD.js";import"./text-area-SPCXy0Bs.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-DbfCVGpY.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
