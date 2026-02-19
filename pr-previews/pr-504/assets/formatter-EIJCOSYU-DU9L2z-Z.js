b48d58605db70d5434faa398ebf150f27ba9c198
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-19 23:49:39 +0000

    Remove preview for PR 504 🛬

--- pr-previews/pr-504/assets/formatter-EIJCOSYU-DU9L2z-Z.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./blocks-Clih3J3j.js";import{b as t}from"./iframe-B1CFJC-J.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BeU512H9.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
--- pr-previews/pr-504/assets/navbar.stories-B9kl_VVL.js
+++
@@ -1,9 +0,0 @@
-import"./iframe-B1CFJC-J.js";import{E as a,N as o}from"./well-BIkLQRVi.js";import"./checkbox-BO6NvAnJ.js";import"./paragraph-D_5DTtxf.js";import"./text-area-Cp76Ud5l.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-zAo2mUYG.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
