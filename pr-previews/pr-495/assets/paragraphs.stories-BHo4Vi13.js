aead93d8fc52a78a729c46803ba9ea7ff33e1966
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-19 23:42:33 +0000

    Deploy preview for PR 495 🛫

---
+++ pr-previews/pr-495/assets/paragraphs.stories-BHo4Vi13.js
@@ -0,0 +1,12 @@
+import{P as o}from"./paragraph-D1KDhEWA.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BFE_lHUK.js";import"./iframe-C1uI79Qj.js";import"./preload-helper-PPVm8Dsz.js";const m={title:"Components (Verified)/Paragraphs",tags:["autodocs"],component:o},e={name:"Body text",args:{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore."}},i={name:"Lead paragraph",args:{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",isLead:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: 'Body text',
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.'
+  }
+}`,...e.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  name: 'Lead paragraph',
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    isLead: true
+  }
+}`,...i.parameters?.docs?.source}}};const d=["Body","Lead"];export{e as Body,i as Lead,d as __namedExportsOrder,m as default};
