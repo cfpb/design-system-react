796d46de0685bbe896490a46358f1bf979787834
natalia-fitzgerald <natalia-fitzgerald@users.noreply.github.com>
2026-02-19 17:14:35 +0000

    Deploy preview for PR 502 🛫

---
+++ pr-previews/pr-502/assets/breadcrumb.stories-9wNt5D7T.js
@@ -0,0 +1,21 @@
+import{j as n}from"./jsx-runtime-u17CrQMm.js";import"./iframe-Ck701EdX.js";import{l as s}from"./well-CDZUQlMa.js";import"./checkbox-9RgNZA1P.js";import"./paragraph-9qhXpJF7.js";import"./text-area-BZbtmJ8Q.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C-780ZyD.js";const d={title:"Components (Draft)/Breadcrumbs",tags:["autodocs"],component:s,argTypes:{}},e={name:"Breadcrumb",render:a=>n.jsx(s,{...a}),args:{crumbs:[{href:"/example",label:"Page link"},{href:"/example2",label:"Page link"}]}},r={render:a=>n.jsx(s,{...a}),args:{crumbs:[{href:"/example",label:"Page link"}]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: 'Breadcrumb',
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      href: '/example',
+      label: 'Page link'
+    }, {
+      href: '/example2',
+      label: 'Page link'
+    }]
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      href: '/example',
+      label: 'Page link'
+    }]
+  }
+}`,...r.parameters?.docs?.source}}};const b=["Default","Crumb"];export{r as Crumb,e as Default,b as __namedExportsOrder,d as default};
