6ca914204aabf08b6ef0b29939ccf7d4570ecbcc
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-19 22:50:10 +0000

    Deploy preview for PR 500 🛫

---
+++ pr-previews/pr-500/assets/page-header.stories--Lnq0mN9.js
@@ -0,0 +1,10 @@
+import"./iframe-NB9Nj_UP.js";import{E as o,P as s}from"./well-DLEDANcQ.js";import"./checkbox-DJ1oTS0p.js";import"./paragraph-Be9wcQPK.js";import"./text-area-B6hguKnG.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-C3dJCbT2.js";const l={title:"Components (Draft)/Page header",tags:["autodocs"],component:s,argTypes:{}},a={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},e={args:{links:o,user:a}},r={args:{links:o}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const u=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,u as __namedExportsOrder,l as default};
---
+++ pr-previews/pr-500/assets/well.stories-DGDZzQrM.js
@@ -0,0 +1,12 @@
+import{j as t}from"./jsx-runtime-u17CrQMm.js";import{W as r,i,e as a}from"./well-DLEDANcQ.js";import"./index-C3dJCbT2.js";import"./iframe-NB9Nj_UP.js";import"./preload-helper-PPVm8Dsz.js";import"./checkbox-DJ1oTS0p.js";import"./text-area-B6hguKnG.js";import"./paragraph-Be9wcQPK.js";const x={title:"Components (Verified)/Wells",tags:["autodocs"],component:r,argTypes:{children:{type:"string"}}},e={name:"Well (container)"},n={name:"Well (content)",args:{heading:"Heading",headingLevel:"h3",text:"Lorem ipsum dolor sit amet, ei ius adhuc inani iudico, labitur instructior ex pri. Cu pri inani constituto, cum aeque noster commodo",links:[t.jsx(a,{href:"/#",label:"Call-to-action link"},"example1")]},render:o=>t.jsx(i,{...o})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: 'Well (container)'
+}`,...e.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  name: 'Well (content)',
+  args: {
+    heading: 'Heading',
+    headingLevel: 'h3',
+    text: 'Lorem ipsum dolor sit amet, ei ius adhuc inani iudico, labitur instructior ex pri. Cu pri inani constituto, cum aeque noster commodo',
+    links: [<ListLink href='/#' key='example1' label='Call-to-action link' />]
+  },
+  render: arguments_ => <WellContent {...arguments_} />
+}`,...n.parameters?.docs?.source}}};const C=["Container","Content"];export{e as Container,n as Content,C as __namedExportsOrder,x as default};
