e102aa91bd4522bfc4b0f39d522958c12ece566d
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-19 23:48:41 +0000

    Deploy preview for PR 504 🛫

---
+++ pr-previews/pr-504/assets/select-multi.stories-B05eQJIU.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-u17CrQMm.js";import"./iframe-B1CFJC-J.js";import{S as r}from"./well-BIkLQRVi.js";import"./checkbox-BO6NvAnJ.js";import"./paragraph-D_5DTtxf.js";import"./text-area-Cp76Ud5l.js";import{M as s}from"./test-utils-CMDZRPRi.js";import"./preload-helper-PPVm8Dsz.js";import"./index-zAo2mUYG.js";const b={title:"Components (Draft)/Selects/Multiselect",component:r,tags:["autodocs"],argTypes:{disabled:{control:"boolean"},isMulti:{control:"boolean"}}},e={args:{id:"multiSelect",label:"Label",isMulti:!0,options:s,disabled:!0},render:o=>t.jsx("div",{style:{minHeight:"200px"},children:t.jsx(r,{...o})})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: 'multiSelect',
+    label: 'Label',
+    isMulti: true,
+    options: MultipleSelectOptions,
+    disabled: true
+  },
+  render: arguments_ => <div style={{
+    minHeight: '200px'
+  }}>
+      <Select {...arguments_} />
+    </div>
+}`,...e.parameters?.docs?.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,b as default};
