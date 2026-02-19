2cb192f1d22554ef05ff25d5eddaa007e870cf0e
anselmbradford <anselmbradford@users.noreply.github.com>
2026-02-19 23:37:37 +0000

    Deploy preview for PR 504 🛫

---
+++ pr-previews/pr-504/assets/divider.stories-vT6wmiup.js
@@ -0,0 +1,16 @@
+import{j as s}from"./jsx-runtime-u17CrQMm.js";import"./iframe-CPVtjHFZ.js";import{D as r}from"./well-DPZ4F5h1.js";import"./checkbox-CTKQVZBm.js";import"./paragraph-BFjiDn2P.js";import"./text-area-CO-URohW.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C_clzQh3.js";const u={title:"Components (Draft)/Dividers",component:r,tags:["autodocs"],parameters:{docs:{description:{component:`
+Dividers create visual separation between content sections by rendering a horizontal line.
+
+<a href="https://cfpb.github.io/design-system/development/blocks#content-dividers">
+https://cfpb.github.io/design-system/development/blocks#content-dividers
+</a>
+`}}}},e={},t={args:{className:"u-mb30 u-mt30"},render:o=>s.jsxs(s.Fragment,{children:["This content is above the divider by 30px",s.jsx(r,{...o}),"This content is below the divider by 30px"]})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    className: 'u-mb30 u-mt30'
+  },
+  render: arguments_ => <>
+      This content is above the divider by 30px
+      <Divider {...arguments_} />
+      This content is below the divider by 30px
+    </>
+}`,...t.parameters?.docs?.source}}};const b=["Default","ExampleUsage"];export{e as Default,t as ExampleUsage,b as __namedExportsOrder,u as default};
---
+++ pr-previews/pr-504/assets/page-header.stories-B_B5i6DE.js
@@ -0,0 +1,10 @@
+import"./iframe-CPVtjHFZ.js";import{E as o,P as s}from"./well-DPZ4F5h1.js";import"./checkbox-CTKQVZBm.js";import"./paragraph-BFjiDn2P.js";import"./text-area-CO-URohW.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-C_clzQh3.js";const l={title:"Components (Draft)/Page header",tags:["autodocs"],component:s,argTypes:{}},a={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},e={args:{links:o,user:a}},r={args:{links:o}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const u=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,u as __namedExportsOrder,l as default};
