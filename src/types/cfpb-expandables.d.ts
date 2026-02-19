a049481e9c200fdb9710f5b781e678b7924a4838
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-02-19 17:45:24 -0600

    Fixes the postcss loader & linting fixes (#503)
    Bump version number
    fixing tslint error
    update dependencies
    Update process-icons.ts reference so yarn build doesnt throw errors

--- src/types/cfpb-expandables.d.ts
+++ src/types/cfpb-expandables.d.ts
@@ -1,20 +1,20 @@
  
 declare module '@cfpb/cfpb-design-system/src/components/cfpb-expandables' {
-  export const Expandable = {
-    init(element?: Element | NodeList | null): void;,
+  export const Expandable: {
+    init(element?: Element | NodeList | null): void;
   };
 }
 
 declare module '@cfpb/cfpb-design-system/src/components/cfpb-expandables' {
-  export const ExpandableGroup = {
-    init(element?: Element | NodeList | null): void;,
+  export const ExpandableGroup: {
+    init(element?: Element | NodeList | null): void;
   };
 }
 
-export const Summary = {
-  init(element?: Element | NodeList | null): void;,
+export const Summary: {
+  init(element?: Element | NodeList | null): void;
 };
 
-export const SummaryMinimal = {
-  init(element?: Element | NodeList | null): void;,
+export const SummaryMinimal: {
+  init(element?: Element | NodeList | null): void;
 };
