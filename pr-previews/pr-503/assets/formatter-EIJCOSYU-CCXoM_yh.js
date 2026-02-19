f272bd937b95980612994609260299c43d06f8ab
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-19 23:26:53 +0000

    Deploy preview for PR 503 🛫

---
+++ pr-previews/pr-503/assets/formatter-EIJCOSYU-CCXoM_yh.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-C2J93__s.js";import{b as t}from"./iframe-Bj827cUU.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DtiTV9za.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
---
+++ pr-previews/pr-503/assets/page-header.stories-CAVD6_a-.js
@@ -0,0 +1,10 @@
+import"./iframe-Bj827cUU.js";import{E as o,P as s}from"./well-B2mD3lQX.js";import"./checkbox-ChCxRgD8.js";import"./paragraph-DnXZKZNU.js";import"./text-area-C01-nFIz.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-Bn6QogVW.js";const l={title:"Components (Draft)/Page header",tags:["autodocs"],component:s,argTypes:{}},a={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},e={args:{links:o,user:a}},r={args:{links:o}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const u=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,u as __namedExportsOrder,l as default};
