177f353b0d34cf6812baaca0b56cd624145f78f1
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-20 23:59:21 +0000

    Deploy preview for PR 506 🛫

---
+++ pr-previews/pr-506/assets/formatter-EIJCOSYU-DzUUKVh2.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-B3Ayi8cN.js";import{b as t}from"./iframe-BxRy30bs.js";import"./preload-helper-PPVm8Dsz.js";import"./index-EjkwZKSe.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
---
+++ pr-previews/pr-506/assets/tagline.stories-CDOet2Pt.js
@@ -0,0 +1,18 @@
+import{j as a}from"./jsx-runtime-u17CrQMm.js";import"./iframe-BxRy30bs.js";import{h as t}from"./well-CKSCo3f7.js";import"./checkbox-eNzfJxXr.js";import"./paragraph-En40OEeR.js";import"./text-area-TO6qiOk8.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C_ObpIo6.js";const l={title:"Components (Verified)/Taglines",tags:["autodocs"],component:t,parameters:{docs:{description:{component:`
+
+Taglines are short paragraphs of text with the USA flag to their left that are used in the header and footer across consumerfinance.gov.
+
+Source: https://cfpb.github.io/design-system/components/taglines
+`}}}},e={render:n=>a.jsx(t,{...n,children:a.jsxs(a.Fragment,{children:["An official website of the"," ",a.jsx("span",{className:"u-nowrap",children:"United States government"})]})})};e.storyName="Standard tagline";const r={...e,args:{isLarge:!0}};r.storyName="Large tagline";e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: properties => <Tagline {...properties}>
+      <>
+        An official website of the{' '}
+        <span className='u-nowrap'>United States government</span>
+      </>
+    </Tagline>
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...r.parameters?.docs?.source}}};const u=["StandardTagline","LargeTagline"];export{r as LargeTagline,e as StandardTagline,u as __namedExportsOrder,l as default};
