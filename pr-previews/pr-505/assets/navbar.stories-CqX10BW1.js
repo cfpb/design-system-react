fab1af38a1c0c3d8ab955a4cb0a425eee4a34f40
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-20 17:45:32 +0000

    Deploy preview for PR 505 🛫

---
+++ pr-previews/pr-505/assets/navbar.stories-CqX10BW1.js
@@ -0,0 +1,9 @@
+import"./iframe-DCVMh0To.js";import{E as a,N as o}from"./well-JnIQHlA_.js";import"./checkbox-eYwJNKIr.js";import"./paragraph-OcwSUU0v.js";import"./text-area-RjO03kJ2.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-D877C_Tt.js";const u={title:"Components (Draft)/Navbar",tags:["autodocs"],component:o,argTypes:{}},r={args:{links:a}},s={args:{}},e={args:{}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...e.parameters?.docs?.source}}};const l=["LoggedIn","LoggedOut","NoUser"];export{r as LoggedIn,s as LoggedOut,e as NoUser,l as __namedExportsOrder,u as default};
---
+++ pr-previews/pr-505/assets/page-header.stories-D-cEEivM.js
@@ -0,0 +1,10 @@
+import"./iframe-DCVMh0To.js";import{E as o,P as s}from"./well-JnIQHlA_.js";import"./checkbox-eYwJNKIr.js";import"./paragraph-OcwSUU0v.js";import"./text-area-RjO03kJ2.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-D877C_Tt.js";const l={title:"Components (Draft)/Page header",tags:["autodocs"],component:s,argTypes:{}},a={name:"John Sample",logoutHref:"/logout",loginHref:"/login"},e={args:{links:o,user:a}},r={args:{links:o}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks,
+    user: LoggedInUser
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const u=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,u as __namedExportsOrder,l as default};
