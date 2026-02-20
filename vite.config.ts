642417efacbca3c382502e0f6e2da43e00b7be0d
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-02-20 09:44:34 -0800

    update path of css

--- vite.config.ts
+++ vite.config.ts
@@ -76,11 +76,9 @@ export default defineConfig(({ mode }) => {
         output: {
           // This prevents the "flat" file explosion for icons/assets in the root
           assetFileNames: (assetInfo): string => {
-            const ext = path.extname(assetInfo.name ?? '');
-            if (ext === '.css') {
-              return 'assets/style.css';
-            }
-            return 'assets/[name].[ext]';
+            return assetInfo.name === 'design-system-react.css'
+              ? 'assets/index.css'
+              : 'assets/[name].[ext]';
           },
           chunkFileNames: 'chunks/[name]-[hash].js',
           globals: {
