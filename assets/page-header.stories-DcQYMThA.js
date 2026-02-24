406c96f24b832f4af494dc987a744a708ee41dee
flacoman91 <1038306+flacoman91@users.noreply.github.com>
2026-02-24 18:09:43 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@2b68a82ff5e520e1498b56702ce2239e94b007c4 🚀

---
+++ assets/page-header.stories-DcQYMThA.js
@@ -0,0 +1,5 @@
+import"./iframe-B00tz3WJ.js";import{E as t,P as e}from"./text-introduction-Bs44dgyo.js";import"./checkbox-rFxilFx2.js";import"./icon-B01K-La_.js";import"./well-_VZlO1sa.js";import"./paragraph--ktAq8qD.js";import"./summary-BWvyvEHq.js";import"./text-area-CsSFm9aJ.js";import"./preload-helper-lKO2eUTi.js";import"./jsx-runtime-u17CrQMm.js";import"./index-PLxGpoGT.js";const u={title:"Components (Draft)/Page header",tags:["autodocs"],component:e,argTypes:{}},r={args:{links:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
--- assets/page-header.stories-DtBKHl_r.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-Cq7KoB05.js";import{E as t,P as e}from"./text-introduction-y-b3litA.js";import"./checkbox-C62uXJMJ.js";import"./icon-Be2BkFDk.js";import"./well-Cl9F7R_f.js";import"./paragraph-BjGEKrGU.js";import"./summary-AUjnSqMU.js";import"./text-area-D1__2DFL.js";import"./preload-helper-lKO2eUTi.js";import"./jsx-runtime-u17CrQMm.js";import"./index-DgkhZh2E.js";const u={title:"Components (Draft)/Page header",tags:["autodocs"],component:e,argTypes:{}},r={args:{links:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
