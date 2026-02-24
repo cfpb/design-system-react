c5c9d844344dd275050a5a2d2e293e477e671b43
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-24 17:16:16 +0000

    Remove preview for PR 508 🛬

--- pr-previews/pr-508/assets/page-header.stories-D5gZjgN_.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-DIafsF33.js";import{E as t,P as e}from"./text-introduction-DXJAEf6K.js";import"./checkbox-DQmGlGnF.js";import"./icon-BO2Wt2uS.js";import"./well-auIv5KiR.js";import"./paragraph-BXqm-g3S.js";import"./summary-BnQu8ige.js";import"./text-area--ilMOJtN.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-C1mR1LCW.js";const u={title:"Components (Draft)/Page header",tags:["autodocs"],component:e,argTypes:{}},r={args:{links:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
