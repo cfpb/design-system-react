450483f2262418033ebfc727d946172d25d3e0f4
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-24 18:04:35 +0000

    Remove preview for PR 507 🛬

--- pr-previews/pr-507/assets/page-header.stories-KPbirk4A.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-DEAYNN6s.js";import{E as t,P as e}from"./text-introduction-CcjU8y9i.js";import"./checkbox-CbLMhfLq.js";import"./icon-vlHjCO3u.js";import"./well-B74HKbXZ.js";import"./paragraph-Dt9MwLLE.js";import"./summary-_VogDJIn.js";import"./text-area-Ccvsl9G6.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BFfrGPen.js";const u={title:"Components (Draft)/Page header",tags:["autodocs"],component:e,argTypes:{}},r={args:{links:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
