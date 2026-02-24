74aa7149d72b931dd28ab206167416f9ebbcef65
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-24 14:27:01 +0000

    Remove preview for PR 506 🛬

--- pr-previews/pr-506/assets/page-header.stories-B6yhJyXD.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-Tn0tRcN0.js";import{E as e,P as a}from"./well-BRxTawqM.js";import"./checkbox-LC0SxjVI.js";import"./paragraph-CGfRRUl-.js";import"./text-area-Bjy3kKXq.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BcZqUmiA.js";const d={title:"Components (Draft)/Page header",tags:["autodocs"],component:a,argTypes:{}},r={args:{links:e}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};const l=["Default"];export{r as Default,l as __namedExportsOrder,d as default};
--- pr-previews/pr-506/assets/responsive-menu.stories-CmLjopXR.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-Tn0tRcN0.js";import{E as s,z as o}from"./well-BRxTawqM.js";import"./checkbox-LC0SxjVI.js";import"./paragraph-CGfRRUl-.js";import"./text-area-Bjy3kKXq.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BcZqUmiA.js";const l={title:"Components (Draft)/ResponsiveMenu",tags:["autodocs"],component:o,argTypes:{}},e={args:{links:s}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...e.parameters?.docs?.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,l as default};
