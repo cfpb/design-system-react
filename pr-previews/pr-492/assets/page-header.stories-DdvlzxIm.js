87a84e7708081e09fc66d0471901181838b26df5
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-25 03:10:15 +0000

    Remove preview for PR 492 🛬

--- pr-previews/pr-492/assets/page-header.stories-DdvlzxIm.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-CCmvrMeh.js";import{E as t,P as e}from"./text-introduction-BiFX6iWy.js";import"./checkbox-DX26oPA1.js";import"./icon-0BkwbXYN.js";import"./well-gRPKEI9L.js";import"./paragraph-Dr8j6Qvs.js";import"./summary-BWHEmHZd.js";import"./text-area-C6Ao0tMB.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-DRuXAzEv.js";const u={title:"Components (Draft)/Page header",tags:["autodocs"],component:e,argTypes:{}},r={args:{links:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
