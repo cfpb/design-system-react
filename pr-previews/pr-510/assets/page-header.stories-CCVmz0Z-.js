d3dbd528e39bdea4d794c3aec808f3ac227705fb
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-25 15:17:22 +0000

    Deploy preview for PR 510 🛫

---
+++ pr-previews/pr-510/assets/page-header.stories-CCVmz0Z-.js
@@ -0,0 +1,5 @@
+import"./iframe-JBD6t5J2.js";import{E as t,P as e}from"./text-introduction-BF9JS3GD.js";import"./checkbox-CiqHQ1D5.js";import"./icon-BlaL9fKk.js";import"./well-ow3p_U1b.js";import"./paragraph-Cpre4Num.js";import"./summary-BesHoPml.js";import"./text-area-DcOecqFS.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-tFBD0Khs.js";const u={title:"Components (Draft)/Page header",tags:["autodocs"],component:e,argTypes:{}},r={args:{links:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
