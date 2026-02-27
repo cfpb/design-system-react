9a5c50cda7484ce2609c9e1977d3e9dd846c4a6c
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-27 22:26:51 +0000

    Deploy preview for PR 511 🛫

---
+++ pr-previews/pr-511/assets/breadcrumb.stories-wW_skJiR.js
@@ -0,0 +1,21 @@
+import{j as m}from"./jsx-runtime-u17CrQMm.js";import"./iframe-BY1oBY3t.js";import{g as s}from"./text-introduction-BHzF-0MB.js";import"./checkbox-B3mP9NtO.js";import"./icon-bWFZ4OtM.js";import"./well-xgVlEy68.js";import"./paragraph-cE7qwktM.js";import"./summary-DYjWQE8I.js";import"./text-area-DAtCgRbu.js";import"./preload-helper-PPVm8Dsz.js";import"./index-y7A8EYVF.js";const f={title:"Components (Draft)/Breadcrumbs",tags:["autodocs"],component:s,argTypes:{}},e={name:"Breadcrumb",render:a=>m.jsx(s,{...a}),args:{crumbs:[{href:"/example",label:"Page link"},{href:"/example2",label:"Page link"}]}},r={render:a=>m.jsx(s,{...a}),args:{crumbs:[{href:"/example",label:"Page link"}]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: 'Breadcrumb',
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      href: '/example',
+      label: 'Page link'
+    }, {
+      href: '/example2',
+      label: 'Page link'
+    }]
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      href: '/example',
+      label: 'Page link'
+    }]
+  }
+}`,...r.parameters?.docs?.source}}};const x=["Default","Crumb"];export{r as Crumb,e as Default,x as __namedExportsOrder,f as default};
---
+++ pr-previews/pr-511/assets/responsive-menu.stories-CTckqP-n.js
@@ -0,0 +1,5 @@
+import"./iframe-BY1oBY3t.js";import{E as r,l as t}from"./text-introduction-BHzF-0MB.js";import"./checkbox-B3mP9NtO.js";import"./icon-bWFZ4OtM.js";import"./well-xgVlEy68.js";import"./paragraph-cE7qwktM.js";import"./summary-DYjWQE8I.js";import"./text-area-DAtCgRbu.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-y7A8EYVF.js";const f={title:"Components (Draft)/ResponsiveMenu",tags:["autodocs"],component:t,argTypes:{}},o={args:{links:r}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...o.parameters?.docs?.source}}};const g=["Default"];export{o as Default,g as __namedExportsOrder,f as default};
