b2eed672929d123843949996cce10954a8190ead
flacoman91 <flacoman91@users.noreply.github.com>
2026-02-27 18:56:42 +0000

    Remove preview for PR 512 🛬

--- pr-previews/pr-512/assets/checkbox-overview-DyTtbdiQ.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as c}from"./index-Dmkj0x8V.js";import{M as a}from"./blocks-yWgO5mOO.js";import"./iframe-C4A1XBMl.js";import{H as s}from"./text-introduction-BpamivuY.js";import"./checkbox-BpoRxq1C.js";import"./icon-BpOMC-dE.js";import{L as h,a as m,b as n}from"./well-Cdp7WKRd.js";import{P as r}from"./paragraph-CyuzwMkn.js";import"./summary-Bn4GCz1N.js";import"./text-area-BDiZm-px.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CS8RpqjB.js";import"./index-BrSk0ctL.js";function i(t){const o={a:"a",...c(),...t.components};return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(a,{title:"Components (Verified)/Checkboxes/Overview"}),`
-`,`
-`,e.jsx(s,{type:"1",children:"Checkboxes"}),`
-`,e.jsxs(r,{children:["Use checkboxes when the user can select more than one option from a list. Make clear with helper text that this is the case. Use ",e.jsx(o.a,{href:"https://cfpb.github.io/design-system/components/radio-buttons",rel:"nofollow",children:"radio buttons"})," when the user can choose only one option from a list."]}),`
-`,e.jsxs(r,{children:["Source: ",e.jsx(h,{href:"https://cfpb.github.io/design-system/components/checkboxes",children:"https://cfpb.github.io/design-system/components/checkboxes"})]}),`
-`,e.jsx("br",{}),`
-`,e.jsxs("div",{className:"sb-unstyled",children:[e.jsx(s,{type:"4",children:"Types"}),e.jsxs(m,{isLink:!0,children:[e.jsx(n,{target:"_top",href:"./?path=/docs/components-verified-checkboxes-checkbox--overview",label:"Checkbox"}),e.jsx(n,{target:"_top",href:"./?path=/docs/components-verified-checkboxes-large-target-area--overview",label:"Large target area"})]})]})]})}function C(t={}){const{wrapper:o}={...c(),...t.components};return o?e.jsx(o,{...t,children:e.jsx(i,{...t})}):i(t)}export{C as default};
--- pr-previews/pr-512/assets/page-header.stories-23HYHJOa.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-C4A1XBMl.js";import{E as t,P as e}from"./text-introduction-BpamivuY.js";import"./checkbox-BpoRxq1C.js";import"./icon-BpOMC-dE.js";import"./well-Cdp7WKRd.js";import"./paragraph-CyuzwMkn.js";import"./summary-Bn4GCz1N.js";import"./text-area-BDiZm-px.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BrSk0ctL.js";const u={title:"Components (Draft)/Page header",tags:["autodocs"],component:e,argTypes:{}},r={args:{links:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
