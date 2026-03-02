466adc5cd5ffaced40ceb3ed3e11bb38aeee0a61
flacoman91 <flacoman91@users.noreply.github.com>
2026-03-02 16:34:11 +0000

    Remove preview for PR 513 🛬

--- pr-previews/pr-513/assets/checkbox-overview-yTfPIw_N.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as c}from"./index-D2b-SGH0.js";import{M as a}from"./blocks-n4zHMW_V.js";import"./iframe-BtukPd64.js";import{H as s}from"./text-introduction-CCnJUZKi.js";import"./checkbox-BqBohTPD.js";import"./icon-Dq6WNGgY.js";import{L as h,a as m,b as n}from"./well-DiPRt7bu.js";import{P as r}from"./paragraph-DrgyqfTD.js";import"./summary-Clb8aOSy.js";import"./text-area-CeA4RO1h.js";import"./preload-helper-PPVm8Dsz.js";import"./index-e8Zqgxm9.js";import"./index-BAACUNL4.js";function i(t){const o={a:"a",...c(),...t.components};return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(a,{title:"Components (Verified)/Checkboxes/Overview"}),`
-`,`
-`,e.jsx(s,{type:"1",children:"Checkboxes"}),`
-`,e.jsxs(r,{children:["Use checkboxes when the user can select more than one option from a list. Make clear with helper text that this is the case. Use ",e.jsx(o.a,{href:"https://cfpb.github.io/design-system/components/radio-buttons",rel:"nofollow",children:"radio buttons"})," when the user can choose only one option from a list."]}),`
-`,e.jsxs(r,{children:["Source: ",e.jsx(h,{href:"https://cfpb.github.io/design-system/components/checkboxes",children:"https://cfpb.github.io/design-system/components/checkboxes"})]}),`
-`,e.jsx("br",{}),`
-`,e.jsxs("div",{className:"sb-unstyled",children:[e.jsx(s,{type:"4",children:"Types"}),e.jsxs(m,{isLink:!0,children:[e.jsx(n,{target:"_top",href:"./?path=/docs/components-verified-checkboxes-checkbox--overview",label:"Checkbox"}),e.jsx(n,{target:"_top",href:"./?path=/docs/components-verified-checkboxes-large-target-area--overview",label:"Large target area"})]})]})]})}function C(t={}){const{wrapper:o}={...c(),...t.components};return o?e.jsx(o,{...t,children:e.jsx(i,{...t})}):i(t)}export{C as default};
--- pr-previews/pr-513/assets/responsive-menu.stories-Hxw4H_T9.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-BtukPd64.js";import{E as r,l as t}from"./text-introduction-CCnJUZKi.js";import"./checkbox-BqBohTPD.js";import"./icon-Dq6WNGgY.js";import"./well-DiPRt7bu.js";import"./paragraph-DrgyqfTD.js";import"./summary-Clb8aOSy.js";import"./text-area-CeA4RO1h.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BAACUNL4.js";const f={title:"Components (Draft)/ResponsiveMenu",tags:["autodocs"],component:t,argTypes:{}},o={args:{links:r}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...o.parameters?.docs?.source}}};const g=["Default"];export{o as Default,g as __namedExportsOrder,f as default};
