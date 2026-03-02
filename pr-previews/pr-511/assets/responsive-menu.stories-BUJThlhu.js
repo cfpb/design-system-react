a5d9640e8ed3360cf7c036aecb076ad722cf5540
flacoman91 <flacoman91@users.noreply.github.com>
2026-03-02 05:28:35 +0000

    Deploy preview for PR 511 🛫

--- pr-previews/pr-511/assets/responsive-menu.stories-BUJThlhu.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-CAMFe4lG.js";import{E as r,l as t}from"./text-introduction-BrNkUkv6.js";import"./checkbox-1Ew6lzJL.js";import"./icon-MVYCYqfP.js";import"./well-Dn_qpbC7.js";import"./paragraph-CKEI3c9V.js";import"./summary-Bo8SjRGg.js";import"./text-area-CgoevPw5.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-gt-yrl4F.js";const f={title:"Components (Draft)/ResponsiveMenu",tags:["autodocs"],component:t,argTypes:{}},o={args:{links:r}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...o.parameters?.docs?.source}}};const g=["Default"];export{o as Default,g as __namedExportsOrder,f as default};
