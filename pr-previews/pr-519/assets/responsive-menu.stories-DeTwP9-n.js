3403135fc1aac40344159f6b28ef15bb758e822e
flacoman91 <flacoman91@users.noreply.github.com>
2026-03-04 18:42:11 +0000

    Remove preview for PR 519 🛬

--- pr-previews/pr-519/assets/responsive-menu.stories-DeTwP9-n.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-CkY2PHSf.js";import{E as r,m as t}from"./text-introduction--gUn_p-F.js";import"./checkbox-CYNQCjAi.js";import"./icon-BbeX25G-.js";import"./well-mzPD08mO.js";import"./paragraph-B6Hu2E0h.js";import"./summary--KIO4W1v.js";import"./text-area-BAok9hJi.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-B2lVupfH.js";const f={title:"Components (Draft)/ResponsiveMenu",tags:["autodocs"],component:t,argTypes:{}},o={args:{links:r}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...o.parameters?.docs?.source}}};const g=["Default"];export{o as Default,g as __namedExportsOrder,f as default};
