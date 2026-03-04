f7349ef5ad29485e0cd63cc9cd3619cd98bde146
flacoman91 <flacoman91@users.noreply.github.com>
2026-03-04 15:30:22 +0000

    Remove preview for PR 517 🛬

--- pr-previews/pr-517/assets/header.stories-BLw-_tof.js
+++
@@ -1,7 +0,0 @@
-import{j as o}from"./jsx-runtime-u17CrQMm.js";import"./iframe-Clnz-FxE.js";import{E as a,j as e}from"./text-introduction-Dgk8oWsg.js";import"./checkbox-Bjk-BwKY.js";import"./icon-BkGAkNvd.js";import"./well-c615wZ8q.js";import"./paragraph-BE39JBO8.js";import"./summary-DvtrMFPn.js";import"./text-area-B7Nob92k.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D7sC6rer.js";const g={title:"Components (Draft)/Header",tags:["autodocs"],component:e,argTypes:{}},r={name:"Header",render:t=>o.jsx(e,{...t}),args:{links:a}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  name: 'Header',
-  render: properties => <Header {...properties} />,
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,g as default};
--- pr-previews/pr-517/assets/responsive-menu.stories-BLGJnJok.js
+++
@@ -1,5 +0,0 @@
-import"./iframe-Clnz-FxE.js";import{E as r,m as t}from"./text-introduction-Dgk8oWsg.js";import"./checkbox-Bjk-BwKY.js";import"./icon-BkGAkNvd.js";import"./well-c615wZ8q.js";import"./paragraph-BE39JBO8.js";import"./summary-DvtrMFPn.js";import"./text-area-B7Nob92k.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-D7sC6rer.js";const f={title:"Components (Draft)/ResponsiveMenu",tags:["autodocs"],component:t,argTypes:{}},o={args:{links:r}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  }
-}`,...o.parameters?.docs?.source}}};const g=["Default"];export{o as Default,g as __namedExportsOrder,f as default};
