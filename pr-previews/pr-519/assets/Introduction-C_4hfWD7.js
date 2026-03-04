5ed87e1c97e3c163fd96d3a4dce6894c97510d23
flacoman91 <flacoman91@users.noreply.github.com>
2026-03-04 17:47:31 +0000

    Deploy preview for PR 519 🛫

---
+++ pr-previews/pr-519/assets/Introduction-C_4hfWD7.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as o}from"./index-DJatcUDd.js";import{M as r}from"./blocks-TBbBI1t2.js";import"./iframe-CkY2PHSf.js";import{H as s}from"./text-introduction--gUn_p-F.js";import"./checkbox-CYNQCjAi.js";import"./icon-BbeX25G-.js";import"./well-mzPD08mO.js";import"./paragraph-B6Hu2E0h.js";import"./summary--KIO4W1v.js";import"./text-area-BAok9hJi.js";import"./preload-helper-PPVm8Dsz.js";import"./index-SRbBjoc8.js";import"./index-B2lVupfH.js";function i(t){const n={a:"a",code:"code",p:"p",pre:"pre",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Guides/Introduction"}),`
+`,e.jsx(s,{type:"1",children:"CFPB Design System React"}),`
+`,e.jsxs(n.p,{children:["This library contains React implementations of the ",e.jsx(n.a,{href:"https://cfpb.github.io/design-system/",rel:"nofollow",children:"CFPB Design System's"})," components and patterns."]}),`
+`,e.jsx(s,{type:"2",children:"Installation"}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"@cfpb/design-system-react"})," library is released as an ",e.jsx(n.a,{href:"https://www.npmjs.com/package/@cfpb/design-system-react",rel:"nofollow",children:"NPM package"}),"."]}),`
+`,e.jsx(n.p,{children:"To install the package:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{children:`yarn add @cfpb/design-system-react
+`})}),`
+`,e.jsx(s,{type:"2",children:"Components - Verified vs Draft"}),`
+`,e.jsxs(n.p,{children:["This library is still in it's early stages of developement, with lots of changes expected in the coming months.  You will notice two subsections in the navigation sidebar: ",e.jsx(n.code,{children:"Components (Verified)"})," and ",e.jsx(n.code,{children:"Components (Draft)"}),"."]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.code,{children:"Verified"})," components have been thoroughly reviewed for consistency with their ",e.jsx(n.a,{href:"https://cfpb.github.io/design-system/",rel:"nofollow",children:"Design System"})," counterparts and can be considered unlikely to change in the near future."]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.code,{children:"Draft"})," components should be viewed as works-in-progress and may still undergo breaking changes in the near future in order to align their implementations with the ",e.jsx(n.a,{href:"https://cfpb.github.io/design-system/",rel:"nofollow",children:"Design System"}),"."]})]})}function w(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{w as default};
---
+++ pr-previews/pr-519/assets/header.stories-D9wHH5Ig.js
@@ -0,0 +1,7 @@
+import{j as o}from"./jsx-runtime-u17CrQMm.js";import"./iframe-CkY2PHSf.js";import{E as a,j as e}from"./text-introduction--gUn_p-F.js";import"./checkbox-CYNQCjAi.js";import"./icon-BbeX25G-.js";import"./well-mzPD08mO.js";import"./paragraph-B6Hu2E0h.js";import"./summary--KIO4W1v.js";import"./text-area-BAok9hJi.js";import"./preload-helper-PPVm8Dsz.js";import"./index-B2lVupfH.js";const g={title:"Components (Draft)/Header",tags:["autodocs"],component:e,argTypes:{}},r={name:"Header",render:t=>o.jsx(e,{...t}),args:{links:a}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  name: 'Header',
+  render: properties => <Header {...properties} />,
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,g as default};
