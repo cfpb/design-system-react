38216b033046566a22d436584eac5f15781001ab
flacoman91 <flacoman91@users.noreply.github.com>
2026-03-12 18:37:59 +0000

    Deploy preview for PR 525 🛫

---
+++ pr-previews/pr-525/assets/header.stories-C-afdwrk.js
@@ -0,0 +1,7 @@
+import{j as o}from"./jsx-runtime-u17CrQMm.js";import"./iframe-Nkbelm8i.js";import{E as a,j as e}from"./text-introduction-BipgsZ0O.js";import"./checkbox-BPoPUeOb.js";import"./icon-Cv6non1T.js";import"./well-C8z2qWxM.js";import"./paragraph-2itYlt5r.js";import"./summary--c4ByYZS.js";import"./text-area-DGJAW3-Q.js";import"./preload-helper-PPVm8Dsz.js";import"./index-B3Wtlj4p.js";const g={title:"Components (Draft)/Header",tags:["autodocs"],component:e,argTypes:{}},r={name:"Header",render:t=>o.jsx(e,{...t}),args:{links:a}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  name: 'Header',
+  render: properties => <Header {...properties} />,
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,g as default};
---
+++ pr-previews/pr-525/assets/responsive-menu.stories-BdH-_xPx.js
@@ -0,0 +1,5 @@
+import"./iframe-Nkbelm8i.js";import{E as r,m as t}from"./text-introduction-BipgsZ0O.js";import"./checkbox-BPoPUeOb.js";import"./icon-Cv6non1T.js";import"./well-C8z2qWxM.js";import"./paragraph-2itYlt5r.js";import"./summary--c4ByYZS.js";import"./text-area-DGJAW3-Q.js";import"./preload-helper-PPVm8Dsz.js";import"./jsx-runtime-u17CrQMm.js";import"./index-B3Wtlj4p.js";const f={title:"Components (Draft)/ResponsiveMenu",tags:["autodocs"],component:t,argTypes:{}},o={args:{links:r}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    links: ExampleLinks
+  }
+}`,...o.parameters?.docs?.source}}};const g=["Default"];export{o as Default,g as __namedExportsOrder,f as default};
