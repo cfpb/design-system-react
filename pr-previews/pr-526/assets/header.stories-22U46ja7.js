faf074f827b365bc58b1ffde09c207300eb0f41b
flacoman91 <flacoman91@users.noreply.github.com>
2026-03-12 22:46:23 +0000

    Deploy preview for PR 526 🛫

--- pr-previews/pr-526/assets/header.stories-22U46ja7.js
+++
@@ -1,7 +0,0 @@
-import{j as o}from"./jsx-runtime-u17CrQMm.js";import"./iframe-EEFFS8AL.js";import{E as a,j as e}from"./text-introduction-Bvi_s9fr.js";import"./checkbox-BmP342mS.js";import"./icon-CxKrxfdT.js";import"./well-DKV2n5Te.js";import"./paragraph-D72nnmhy.js";import"./summary-DsJt_poS.js";import"./text-area-CwLxjjKF.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BCzVO9mw.js";const g={title:"Components (Draft)/Header",tags:["autodocs"],component:e,argTypes:{}},r={name:"Header",render:t=>o.jsx(e,{...t}),args:{links:a}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  name: 'Header',
-  render: properties => <Header {...properties} />,
-  args: {
-    links: ExampleLinks
-  }
-}`,...r.parameters?.docs?.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,g as default};
---
+++ pr-previews/pr-526/assets/header.stories-3YS0ZiHP.js
@@ -0,0 +1,7 @@
+import{j as o}from"./jsx-runtime-u17CrQMm.js";import"./iframe-DBJHXYnw.js";import{E as a,j as e}from"./text-introduction-ETz6RgU5.js";import"./checkbox-Vt0UhBjB.js";import"./icon-CLR-RSId.js";import"./well-Kqt5MA5L.js";import"./paragraph-BamxGIOe.js";import"./summary-CTiKcHLQ.js";import"./text-area-BYQUOHNj.js";import"./preload-helper-PPVm8Dsz.js";import"./index-AAM21q5R.js";const g={title:"Components (Draft)/Header",tags:["autodocs"],component:e,argTypes:{}},r={name:"Header",render:t=>o.jsx(e,{...t}),args:{links:a}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  name: 'Header',
+  render: properties => <Header {...properties} />,
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,g as default};
