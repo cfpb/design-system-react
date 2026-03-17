001d89b0619f11de68e225729a7aeadc0ab79406
flacoman91 <flacoman91@users.noreply.github.com>
2026-03-17 17:03:44 +0000

    Deploy preview for PR 528 🛫

---
+++ pr-previews/pr-528/assets/header.stories-DLtAi6j4.js
@@ -0,0 +1,7 @@
+import{j as o}from"./jsx-runtime-u17CrQMm.js";import"./iframe-4_qSzndm.js";import{E as a,j as e}from"./text-introduction-DXFKnura.js";import"./checkbox-DtVGmlob.js";import"./icon-BAFyrtX8.js";import"./well-BiXkwFb_.js";import"./paragraph-B6CQaslf.js";import"./summary-Cju1BQwv.js";import"./text-area-D9FI8clx.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C54KSNba.js";const g={title:"Components (Draft)/Header",tags:["autodocs"],component:e,argTypes:{}},r={name:"Header",render:t=>o.jsx(e,{...t}),args:{links:a}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  name: 'Header',
+  render: properties => <Header {...properties} />,
+  args: {
+    links: ExampleLinks
+  }
+}`,...r.parameters?.docs?.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,g as default};
---
+++ pr-previews/pr-528/assets/select-multi.stories-BzG_ypc9.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import"./iframe-4_qSzndm.js";import{S as r}from"./text-introduction-DXFKnura.js";import"./checkbox-DtVGmlob.js";import"./icon-BAFyrtX8.js";import"./well-BiXkwFb_.js";import"./paragraph-B6CQaslf.js";import"./summary-Cju1BQwv.js";import"./text-area-D9FI8clx.js";import{M as i}from"./test-utils-CMDZRPRi.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C54KSNba.js";const x={title:"Components (Draft)/Selects/Multiselect",component:r,tags:["autodocs"],argTypes:{disabled:{control:"boolean"},isMulti:{control:"boolean"}}},t={args:{id:"multiSelect",label:"Label",isMulti:!0,options:i,disabled:!0},render:o=>e.jsx("div",{style:{minHeight:"200px"},children:e.jsx(r,{...o})})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: 'multiSelect',
+    label: 'Label',
+    isMulti: true,
+    options: MultipleSelectOptions,
+    disabled: true
+  },
+  render: arguments_ => <div style={{
+    minHeight: '200px'
+  }}>
+      <Select {...arguments_} />
+    </div>
+}`,...t.parameters?.docs?.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,x as default};
