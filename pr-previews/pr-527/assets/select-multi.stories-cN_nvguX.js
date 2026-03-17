3997df3cdd0ee255fff2322dc21f7435986dc95b
virginiacc <virginiacc@users.noreply.github.com>
2026-03-17 17:00:08 +0000

    Deploy preview for PR 527 🛫

---
+++ pr-previews/pr-527/assets/select-multi.stories-cN_nvguX.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import"./iframe-pTDUaypd.js";import{S as r}from"./text-introduction-BPK_ugar.js";import"./checkbox-BdjP3bOS.js";import"./icon-BSYlxz2d.js";import"./well-CSluGqTH.js";import"./paragraph-C_umQZR4.js";import"./summary-C8HTR-3j.js";import"./text-area-EiUmM_p1.js";import{M as i}from"./test-utils-CMDZRPRi.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DoO4HDZn.js";const x={title:"Components (Draft)/Selects/Multiselect",component:r,tags:["autodocs"],argTypes:{disabled:{control:"boolean"},isMulti:{control:"boolean"}}},t={args:{id:"multiSelect",label:"Label",isMulti:!0,options:i,disabled:!0},render:o=>e.jsx("div",{style:{minHeight:"200px"},children:e.jsx(r,{...o})})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: 'multiSelect',
+    label: 'Label',
+    isMulti: true,
+    options: MultipleSelectOptions,
+    disabled: true
+  },
+  render: arguments_ => <div style={{
+    minHeight: '200px'
+  }}>
+      <Select {...arguments_} />
+    </div>
+}`,...t.parameters?.docs?.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,x as default};
