2c4396773654f3e0837eccb52cd456bc84b146d7
flacoman91 <flacoman91@users.noreply.github.com>
2026-03-17 14:05:45 +0000

    Remove preview for PR 526 🛬

--- pr-previews/pr-526/assets/checkbox-overview-Di02GG-J.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as c}from"./index-BVzcEUnU.js";import{M as a}from"./blocks-0NMG5Aez.js";import"./iframe-sV4Vkmrp.js";import{H as s}from"./text-introduction-DVbKYglD.js";import"./checkbox-Bt1-b-F0.js";import"./icon-xbJYnv5X.js";import{L as h,a as m,b as n}from"./well-BHvbkeJF.js";import{P as r}from"./paragraph-C-4wXuPi.js";import"./summary-Hbvm5Lku.js";import"./text-area-ZXQaeFId.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D93r3AK1.js";import"./index-C4ORGo_U.js";function i(t){const o={a:"a",...c(),...t.components};return e.jsxs(e.Fragment,{children:[`
-`,`
-`,e.jsx(a,{title:"Components (Verified)/Checkboxes/Overview"}),`
-`,`
-`,e.jsx(s,{type:"1",children:"Checkboxes"}),`
-`,e.jsxs(r,{children:["Use checkboxes when the user can select more than one option from a list. Make clear with helper text that this is the case. Use ",e.jsx(o.a,{href:"https://cfpb.github.io/design-system/components/radio-buttons",rel:"nofollow",children:"radio buttons"})," when the user can choose only one option from a list."]}),`
-`,e.jsxs(r,{children:["Source: ",e.jsx(h,{href:"https://cfpb.github.io/design-system/components/checkboxes",children:"https://cfpb.github.io/design-system/components/checkboxes"})]}),`
-`,e.jsx("br",{}),`
-`,e.jsxs("div",{className:"sb-unstyled",children:[e.jsx(s,{type:"4",children:"Types"}),e.jsxs(m,{isLink:!0,children:[e.jsx(n,{target:"_top",href:"./?path=/docs/components-verified-checkboxes-checkbox--overview",label:"Checkbox"}),e.jsx(n,{target:"_top",href:"./?path=/docs/components-verified-checkboxes-large-target-area--overview",label:"Large target area"})]})]})]})}function C(t={}){const{wrapper:o}={...c(),...t.components};return o?e.jsx(o,{...t,children:e.jsx(i,{...t})}):i(t)}export{C as default};
