b74d8b79ec011442f2fbe0f5e48d2cde691e379e
flacoman91 <flacoman91@users.noreply.github.com>
2026-03-17 17:13:04 +0000

    Remove preview for PR 528 🛬

--- pr-previews/pr-528/assets/responsive-menu.stories-CWUVt5no.js
+++
@@ -1,8 +0,0 @@
-import{j as r}from"./jsx-runtime-u17CrQMm.js";import"./iframe-4_qSzndm.js";import{E as e,m as t}from"./text-introduction-DXFKnura.js";import"./checkbox-DtVGmlob.js";import"./icon-BAFyrtX8.js";import"./well-BiXkwFb_.js";import"./paragraph-B6CQaslf.js";import"./summary-Cju1BQwv.js";import"./text-area-D9FI8clx.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C54KSNba.js";const g={title:"Components (Draft)/ResponsiveMenu",tags:["autodocs"],component:t,argTypes:{}},o={args:{links:e},decorators:[s=>r.jsx("div",{className:"o-header-scope",children:r.jsx(s,{})})]};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  },
-  decorators: [Story => <div className='o-header-scope'>
-        <Story />
-      </div>]
-}`,...o.parameters?.docs?.source}}};const v=["Default"];export{o as Default,v as __namedExportsOrder,g as default};
