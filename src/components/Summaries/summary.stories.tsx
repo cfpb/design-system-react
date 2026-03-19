33530de9c2e3e656f5bb48d48abc3cf5bd9f995a
Ans <anselmbradford@users.noreply.github.com>
2026-03-19 18:08:28 -0400

    Replace `##` with `/#`

--- src/components/Summaries/summary.stories.tsx
+++ src/components/Summaries/summary.stories.tsx
@@ -28,19 +28,19 @@ const longTextContent = (
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. <a href='##'>Duis</a> aute irure dolor in reprehenderit
+      commodo consequat. <a href='/#'>Duis</a> aute irure dolor in reprehenderit
       in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     </p>
     <p>
       <ul>
         <li>
-          <a href='##'>Link</a>
+          <a href='/#'>Link</a>
         </li>
         <li>
-          <a href='##'>Link</a>
+          <a href='/#'>Link</a>
         </li>
         <li>
-          <a href='##'>Link</a>
+          <a href='/#'>Link</a>
         </li>
       </ul>
     </p>
@@ -64,7 +64,7 @@ export const MobileOnly: Story = {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. <a href='##'>Duis</a> aute irure dolor in
+        commodo consequat. <a href='/#'>Duis</a> aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
         qui officia deserunt mollit anim id est laborum.
@@ -81,7 +81,7 @@ export const Minimal: Story = {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. <a href='##'>Duis</a> aute irure dolor in
+        commodo consequat. <a href='/#'>Duis</a> aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
         qui officia deserunt mollit anim id est laborum.
