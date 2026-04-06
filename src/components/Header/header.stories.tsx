e404fcba7100c9e2e9adb0a76a416224bf3baee0
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-04-06 14:28:22 -0700

    save work
    remove gap
    
    grid fixes
    
    dont render markup when no links or phone number
    
    convert log out to button.
    
    change to setence case
    
    updated responsive menu styoe
    
    update button
    
    revert changes to margin logo

--- src/components/Header/header.stories.tsx
+++ src/components/Header/header.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Header } from '~/src/index';
-import { ExampleLinks } from '../ResponsiveMenu/responsive-menu';
+import { ExampleLinks } from './responsive-menu';
 
 const meta: Meta<typeof Header> = {
   title: 'Components (Draft)/Header',
