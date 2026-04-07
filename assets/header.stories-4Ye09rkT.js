087c97fea8635c94676c4d8563fbd8b20ff83940
flacoman91 <1038306+flacoman91@users.noreply.github.com>
2026-04-07 17:04:31 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@813333d746ec05221e59fe30287e2d57138ae329 🚀

--- assets/header.stories-4Ye09rkT.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-6sF1Ejqi.js";import{_ as n,h as r,t as i,y as a}from"./src-Bg8HMGLY.js";var o,s,c,l;e((()=>{i(),a(),o=t(),s={title:`Components (Draft)/Header`,tags:[`autodocs`],component:r,argTypes:{}},c={name:`Header`,render:e=>(0,o.jsx)(r,{...e}),args:{links:n}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  name: 'Header',
-  render: properties => <Header {...properties} />,
-  args: {
-    links: ExampleLinks
-  }
-}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
