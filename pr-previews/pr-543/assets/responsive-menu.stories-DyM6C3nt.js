1413619bdbc66253cb89126505d2d8e7ba155c70
anselmbradford <anselmbradford@users.noreply.github.com>
2026-04-07 16:21:07 +0000

    Remove preview for PR 543 🛬

--- pr-previews/pr-543/assets/responsive-menu.stories-DyM6C3nt.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-6sF1Ejqi.js";import{_ as n,g as r,t as i,v as a,y as o}from"./src-E5dG2XgJ.js";var s,c,l,u;e((()=>{i(),r(),o(),s=t(),c={title:`Components (Draft)/ResponsiveMenu`,tags:[`autodocs`],component:a,argTypes:{}},l={args:{links:n},decorators:[e=>(0,s.jsx)(`div`,{className:`o-header-scope`,children:(0,s.jsx)(e,{})})]},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
-  args: {
-    links: ExampleLinks
-  },
-  decorators: [Story => <div className='o-header-scope'>
-        <Story />
-      </div>]
-}`,...l.parameters?.docs?.source}}},u=[`Default`]}))();export{l as Default,u as __namedExportsOrder,c as default};
