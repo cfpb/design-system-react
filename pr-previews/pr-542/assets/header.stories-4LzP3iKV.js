993c6ea50518de38d5f780be1af0b9b59a8b5f67
flacoman91 <flacoman91@users.noreply.github.com>
2026-04-07 00:58:12 +0000

    Deploy preview for PR 542 🛫

--- pr-previews/pr-542/assets/header.stories-4LzP3iKV.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-CWHGkrjl.js";import{f as n,m as r,p as i,t as a}from"./src-B8JMSZt_.js";var o,s,c,l;e((()=>{a(),r(),o=t(),s={title:`Components (Draft)/Header`,tags:[`autodocs`],component:n,argTypes:{}},c={name:`Header`,render:e=>(0,o.jsx)(n,{...e}),args:{links:i}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  name: 'Header',
-  render: properties => <Header {...properties} />,
-  args: {
-    links: ExampleLinks
-  }
-}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
---
+++ pr-previews/pr-542/assets/tagline.stories-j3nHUwpd.js
@@ -0,0 +1,18 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-CWHGkrjl.js";import{P as n,t as r}from"./src-uZ82PyBo.js";var i,a,o,s,c;e((()=>{r(),i=t(),a={title:`Components (Verified)/Taglines`,tags:[`autodocs`],component:n,parameters:{docs:{description:{component:`
+
+Taglines are short paragraphs of text with the USA flag to their left that are used in the header and footer across consumerfinance.gov.
+
+Source: https://cfpb.github.io/design-system/components/taglines
+`}}}},o={render:e=>(0,i.jsx)(n,{...e,children:(0,i.jsxs)(i.Fragment,{children:[`An official website of the`,` `,(0,i.jsx)(`span`,{className:`u-nowrap`,children:`United States government`})]})})},o.storyName=`Standard tagline`,s={...o,args:{isLarge:!0}},s.storyName=`Large tagline`,o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: properties => <Tagline {...properties}>
+      <>
+        An official website of the{' '}
+        <span className='u-nowrap'>United States government</span>
+      </>
+    </Tagline>
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...s.parameters?.docs?.source}}},c=[`StandardTagline`,`LargeTagline`]}))();export{s as LargeTagline,o as StandardTagline,c as __namedExportsOrder,a as default};
