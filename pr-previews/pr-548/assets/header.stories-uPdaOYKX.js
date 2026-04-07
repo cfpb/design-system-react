c54a4b25b0744703a9b60586dd3e11c2967140b2
flacoman91 <flacoman91@users.noreply.github.com>
2026-04-07 22:20:34 +0000

    Deploy preview for PR 548 🛫

---
+++ pr-previews/pr-548/assets/header.stories-uPdaOYKX.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-6sF1Ejqi.js";import{_ as n,g as r,h as i,t as a}from"./src-l3h5b5pb.js";var o,s,c,l;e((()=>{a(),n(),o=t(),s={title:`Components (Draft)/Header`,tags:[`autodocs`],component:i,argTypes:{}},c={name:`Header`,render:e=>(0,o.jsx)(i,{...e}),args:{links:r}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  name: 'Header',
+  render: properties => <Header {...properties} />,
+  args: {
+    links: ExampleLinks
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
