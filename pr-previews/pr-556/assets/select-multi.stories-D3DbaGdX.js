b2acbb374f97e250564853ee99397c57d4cfb462
flacoman91 <flacoman91@users.noreply.github.com>
2026-04-21 20:38:04 +0000

    Deploy preview for PR 556 🛫

---
+++ pr-previews/pr-556/assets/select-multi.stories-D3DbaGdX.js
@@ -0,0 +1,14 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-BX9360Lk.js";import{o as n,t as r}from"./src-CIdlh-Fv.js";import{r as i,t as a}from"./test-utils-BFiWX1N2.js";var o,s,c,l;e((()=>{r(),i(),o=t(),s={title:`Components (Draft)/Selects/Multiselect`,component:n,tags:[`autodocs`],argTypes:{disabled:{control:`boolean`},isMulti:{control:`boolean`}}},c={args:{id:`multiSelect`,label:`Label`,isMulti:!0,options:a,disabled:!0},render:e=>(0,o.jsx)(`div`,{style:{minHeight:`200px`},children:(0,o.jsx)(n,{...e})})},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: 'multiSelect',
+    label: 'Label',
+    isMulti: true,
+    options: MultipleSelectOptions,
+    disabled: true
+  },
+  render: arguments_ => <div style={{
+    minHeight: '200px'
+  }}>
+      <Select {...arguments_} />
+    </div>
+}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
---
+++ pr-previews/pr-556/assets/well.stories-FDf2mi20.js
@@ -0,0 +1,12 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-BX9360Lk.js";import{n,r,t as i}from"./well-DBbcGByS.js";import{n as a,r as o}from"./link-hw0v0Vh7.js";var s,c,l,u,d;e((()=>{o(),r(),s=t(),c={title:`Components (Verified)/Wells`,tags:[`autodocs`],component:i,argTypes:{children:{type:`string`}}},l={name:`Well (container)`},u={name:`Well (content)`,args:{heading:`Heading`,headingLevel:`h3`,text:`Lorem ipsum dolor sit amet, ei ius adhuc inani iudico, labitur instructior ex pri. Cu pri inani constituto, cum aeque noster commodo`,links:[(0,s.jsx)(a,{href:`/#`,label:`Call-to-action link`},`example1`)]},render:e=>(0,s.jsx)(n,{...e})},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  name: 'Well (container)'
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  name: 'Well (content)',
+  args: {
+    heading: 'Heading',
+    headingLevel: 'h3',
+    text: 'Lorem ipsum dolor sit amet, ei ius adhuc inani iudico, labitur instructior ex pri. Cu pri inani constituto, cum aeque noster commodo',
+    links: [<ListLink href='/#' key='example1' label='Call-to-action link' />]
+  },
+  render: arguments_ => <WellContent {...arguments_} />
+}`,...u.parameters?.docs?.source}}},d=[`Container`,`Content`]}))();export{l as Container,u as Content,d as __namedExportsOrder,c as default};
