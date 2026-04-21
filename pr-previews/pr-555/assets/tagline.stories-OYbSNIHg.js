b60fb5acabe137ebadd442e9307e984baed05345
flacoman91 <flacoman91@users.noreply.github.com>
2026-04-21 14:21:21 +0000

    Deploy preview for PR 555 🛫

---
+++ pr-previews/pr-555/assets/tagline.stories-OYbSNIHg.js
@@ -0,0 +1,13 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-6sF1Ejqi.js";import{n,t as r}from"./src-C6HNm_rm.js";var i,a,o,s,c,l;e((()=>{r(),i=t(),n.init(),a=e=>(0,i.jsx)(`cfpb-tagline`,{...e}),o={title:`Components (Verified)/Taglines`,tags:[`autodocs`,`web-component`],component:a,parameters:{docs:{description:{component:`
+
+Taglines are short paragraphs of text with the USA flag to their left that are used in the header and footer across consumerfinance.gov.
+
+Source: https://cfpb.github.io/design-system/components/taglines
+`}}}},s={render:e=>(0,i.jsx)(`cfpb-tagline`,{...e})},s.name=`Standard tagline`,c={...s,args:{isLarge:!0}},c.name=`Large tagline`,s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  render: properties => <cfpb-tagline {...properties}></cfpb-tagline>
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  ...StandardTagline,
+  args: {
+    isLarge: true
+  }
+}`,...c.parameters?.docs?.source}}},l=[`StandardTagline`,`LargeTagline`]}))();export{c as LargeTagline,s as StandardTagline,l as __namedExportsOrder,o as default};
