e620a1106018632f665e7c7536bbd11e3bef8f95
flacoman91 <flacoman91@users.noreply.github.com>
2026-04-28 14:38:08 +0000

    Deploy preview for PR 560 🛫

---
+++ pr-previews/pr-560/assets/select-multi.stories-C6iz1t29.js
@@ -0,0 +1,14 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-CXZ2plg1.js";import{o as n,t as r}from"./src-B-YMIjbH.js";import{r as i,t as a}from"./test-utils--hOtF9Q8.js";var o,s,c,l;e((()=>{r(),i(),o=t(),s={title:`Components (Draft)/Selects/Multiselect`,component:n,tags:[`autodocs`],argTypes:{disabled:{control:`boolean`},isMulti:{control:`boolean`}}},c={args:{id:`multiSelect`,label:`Label`,isMulti:!0,options:a,disabled:!0},render:e=>(0,o.jsx)(`div`,{style:{minHeight:`200px`},children:(0,o.jsx)(n,{...e})})},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    id: 'multiSelect',
+    label: 'Label',
+    isMulti: true,
+    options: MultipleSelectOptions,
+    disabled: true
+  },
+  render: arguments_ => <div style={{
+    minHeight: '200px'
+  }}>
+      <Select {...arguments_} />
+    </div>
+}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
