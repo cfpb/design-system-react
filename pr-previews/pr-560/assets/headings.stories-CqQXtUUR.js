b0706e0548461887ab98d2bc82120567106f387c
flacoman91 <flacoman91@users.noreply.github.com>
2026-05-07 05:52:29 +0000

    Deploy preview for PR 560 🛫

---
+++ pr-previews/pr-560/assets/headings.stories-CqQXtUUR.js
@@ -0,0 +1,58 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-CXZ2plg1.js";import{t as n}from"./icon-ByNq0ULZ.js";import{m as r,t as i}from"./src-Vre_xXpB.js";var a,o,s,c,l,u,d,f,p,m,h,g;e((()=>{i(),a=t(),o={title:`Components (Verified)/Headings`,tags:[`autodocs`],component:r,argTypes:{type:{control:{type:`select`}}},parameters:{docs:{description:{component:`A successful type hierarchy establishes the order of importance of elements on a page. Consistent scaling, weights, and capitalization are used to create distinction between headings and provide users with familiar focus points when scanning text.
+
+Source: <a href='https://cfpb.github.io/design-system/foundation/headings' target='_blank'> https://cfpb.github.io/design-system/foundation/headings</a>`}}}},s={name:`H1`,args:{children:`Heading 1`}},c={name:`H2`,args:{type:`2`,children:`Heading 2`}},l={name:`H3`,args:{type:`3`,children:`Heading 3`}},u={name:`H4`,args:{type:`4`,children:`Heading 4`}},d={name:`H5`,args:{type:`5`,children:`Heading 5`}},f={args:{type:`display`,children:`Display`}},p={args:{type:`eyebrow`,children:`Eyebrow`},render:e=>(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(r,{...e}),(0,a.jsx)(`div`,{className:`h1`,children:`Heading 1`})]})},m={args:{type:`slug`,children:`Slug`}},h={args:{type:`2`},render:e=>(0,a.jsxs)(r,{...e,children:[(0,a.jsx)(n,{name:`help`}),` Information`]})},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  name: 'H1',
+  args: {
+    children: 'Heading 1'
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  name: 'H2',
+  args: {
+    type: '2',
+    children: 'Heading 2'
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  name: 'H3',
+  args: {
+    type: '3',
+    children: 'Heading 3'
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  name: 'H4',
+  args: {
+    type: '4',
+    children: 'Heading 4'
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  name: 'H5',
+  args: {
+    type: '5',
+    children: 'Heading 5'
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: 'display',
+    children: 'Display'
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: 'eyebrow',
+    children: 'Eyebrow'
+  },
+  render: arguments_ => <>
+      <Heading {...arguments_} />
+      <div className='h1'>Heading 1</div>
+    </>
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: 'slug',
+    children: 'Slug'
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: '2'
+  },
+  render: arguments_ => <Heading {...arguments_}>
+      <Icon name='help' /> Information
+    </Heading>
+}`,...h.parameters?.docs?.source}}},g=[`H1`,`H2`,`H3`,`H4`,`H5`,`Display`,`Eyebrow`,`Slug`,`WithIcon`]}))();export{f as Display,p as Eyebrow,s as H1,c as H2,l as H3,u as H4,d as H5,m as Slug,h as WithIcon,g as __namedExportsOrder,o as default};
