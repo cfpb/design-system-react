b509ab14bbd9c206780bd11a8f1734b087cb0441
flacoman91 <flacoman91@users.noreply.github.com>
2026-05-08 02:13:10 +0000

    Deploy preview for PR 560 🛫

---
+++ pr-previews/pr-560/assets/footer.stories-C-pYlg84.js
@@ -0,0 +1,18 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-BX9360Lk.js";import{i as n}from"./well-CvYWgIuy.js";import{b as r,t as i,x as a,y as o}from"./src-Cm6bq3G3.js";var s,c,l,u,d;e((()=>{i(),s=t(),c={title:`Components (Draft)/Footers`,tags:[`autodocs`],component:a,argTypes:{}},l={name:`Website (cf.gov)`,render:()=>(0,s.jsx)(o,{})},u={name:`Application`,render:()=>(0,s.jsx)(r,{navLinks:[(0,s.jsx)(n,{label:`Link 1`,href:`#`},`link_1`),(0,s.jsx)(n,{label:`Longer link 2`,href:`#`},`link_2`)],footerContent:(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)(`h3`,{children:`Heading`}),(0,s.jsx)(`p`,{children:`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`}),(0,s.jsx)(n,{href:`#`,children:`Link`})]})}),parameters:{docs:{description:{story:`A minimal two-column footer for use in applications. The left column can contain information about the app, and the right column is for relevant links.`}}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  name: 'Website (cf.gov)',
+  render: () => <FooterCfGov />
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  name: 'Application',
+  render: () => <ApplicationFooter navLinks={[<Link label='Link 1' key='link_1' href='#' />, <Link label='Longer link 2' key='link_2' href='#' />]} footerContent={<>
+          <h3>Heading</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <Link href='#'>Link</Link>
+        </>} />,
+  parameters: {
+    docs: {
+      description: {
+        story: 'A minimal two-column footer for use in applications. The left column can contain information about the app, and the right column is for relevant links.'
+      }
+    }
+  }
+}`,...u.parameters?.docs?.source}}},d=[`CFGov`,`App`]}))();export{u as App,l as CFGov,d as __namedExportsOrder,c as default};
