a278c5ec186b195c2fef55d47d028d8048e04eca
virginiacc <vczosek@gmail.com>
2026-05-11 23:33:28 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@848d3879c6e6871c826300747715d8d12b2e3c72 🚀

---
+++ assets/mdx-react-shim-DrzgEgJb.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-Dpx49iXm.js";var n=e((()=>{t()}));export{n as t};
--- assets/mdx-react-shim-axfz6XNw.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./react-BKz4oS8y.js";var n=e((()=>{t()}));export{n as t};
