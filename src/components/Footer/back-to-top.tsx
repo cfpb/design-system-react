407fd2b628dcf01879572e76df8e2d91de08c3cb
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-05-12 06:19:40 -0700

    linting, adding font check tools

--- src/components/Footer/back-to-top.tsx
+++ src/components/Footer/back-to-top.tsx
@@ -1,13 +1,15 @@
 import { JSX } from 'react';
-/* eslint-disable jsx-a11y/anchor-is-valid */
+
 import Link from '../Link/link';
 export const BackToTop = (): JSX.Element => (
-  <Link label="Back to top" 
-        isButton
-        className='a-btn--secondary a-btn--full-on-xs o-footer__top-button u-show-on-mobile u-mb45'
-        data-gtm_ignore='true'
-        data-js-hook='behavior_return-to-top'
-        data-testid='back-to-top'
-        href='#'
-        iconRight='arrow-up'/>
+  <Link
+    label='Back to top'
+    isButton
+    className='a-btn--secondary a-btn--full-on-xs o-footer__top-button u-show-on-mobile u-mb45'
+    data-gtm_ignore='true'
+    data-js-hook='behavior_return-to-top'
+    data-testid='back-to-top'
+    href='#'
+    iconRight='arrow-up'
+  />
 );
