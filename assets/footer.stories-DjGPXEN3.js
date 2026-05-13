8d1e4d40b332daeeb9dc8fb4a5766b2da8da0a6b
flacoman91 <1038306+flacoman91@users.noreply.github.com>
2026-05-13 19:11:04 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@654804662fb7d13c1185bc13348a9208e1a30d3f 🚀

---
+++ assets/footer.stories-DjGPXEN3.js
@@ -0,0 +1,18 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t}from"./jsx-runtime-BX9360Lk.js";import{b as n,m as r,t as i,x as a,y as o}from"./src-BBTXsXC7.js";import{t as s}from"./link-D3nS_vq9.js";import{t as c}from"./paragraph-DefIWyAo.js";var l,u,d,f,p;e((()=>{i(),l=t(),u={title:`Components (Draft)/Footers`,tags:[`autodocs`],component:a,argTypes:{}},d={name:`Website (cf.gov)`,render:()=>(0,l.jsx)(o,{})},f={name:`Application`,render:()=>(0,l.jsx)(n,{navLinks:[(0,l.jsx)(s,{label:`Link 1`,href:`#`},`link_1`),(0,l.jsx)(s,{label:`Longer link 2`,href:`#`},`link_2`)],footerContent:(0,l.jsxs)(l.Fragment,{children:[(0,l.jsx)(r,{type:`3`,children:`Heading`}),(0,l.jsx)(c,{children:`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`}),(0,l.jsx)(s,{href:`#`,children:`Link`})]})}),parameters:{docs:{description:{story:`A minimal two-column footer for use in applications. The left column can contain information about the app, and the right column is for relevant links.`}}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  name: 'Website (cf.gov)',
+  render: () => <FooterCfGov />
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  name: 'Application',
+  render: () => <ApplicationFooter navLinks={[<Link label='Link 1' key='link_1' href='#' />, <Link label='Longer link 2' key='link_2' href='#' />]} footerContent={<>
+          <Heading type='3'>Heading</Heading>
+          <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Paragraph>
+          <Link href='#'>Link</Link>
+        </>} />,
+  parameters: {
+    docs: {
+      description: {
+        story: 'A minimal two-column footer for use in applications. The left column can contain information about the app, and the right column is for relevant links.'
+      }
+    }
+  }
+}`,...f.parameters?.docs?.source}}},p=[`CFGov`,`App`]}))();export{f as App,d as CFGov,p as __namedExportsOrder,u as default};
