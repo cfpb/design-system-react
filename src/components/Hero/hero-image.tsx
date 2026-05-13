e467ea89cc12f486b2b24182e8c4e339691e1776
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-05-13 11:27:02 -0700

    remove hero work in favor of other PR

--- src/components/Hero/hero-image.tsx
+++ src/components/Hero/hero-image.tsx
@@ -5,11 +5,6 @@ interface HeroImageProperties {
   altText?: string;
 }
 
-/**
- * DS pattern: empty `.m-hero__image` with `background-image` (not `<img>`), so
- * `background-size: contain` and aspect ratio control the slot height.
- * https://cfpb.github.io/design-system/patterns/heroes
- */
 export const HeroImage = ({
   image,
   altText,
@@ -18,14 +13,7 @@ export const HeroImage = ({
 
   return (
     <div className='m-hero__image-wrapper'>
-      <div
-        className='m-hero__image'
-        role='img'
-        aria-label={altText}
-        style={{
-          backgroundImage: `url(${JSON.stringify(image)})`,
-        }}
-      />
+      <img src={image} alt={altText} className='m-hero__image' />
     </div>
   );
 };
