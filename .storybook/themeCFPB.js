46f0cfb5d390bbfa4b8bceee1db74ddbae4ace82
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-05-15 14:57:11 -0700

    update logo with SVG version

--- .storybook/themeCFPB.js
+++ .storybook/themeCFPB.js
@@ -1,5 +1,5 @@
 import { create } from 'storybook/theming/create';
-import CfpbLogo from '../src/assets/images/cfpb-logo.png';
+import CfpbLogo from '../src/assets/images/CFPB_logo.svg?url';
 
 const colors = {
   black: '#101820',
