beb5cd76781a45583c3772c1b35753312d4cd52b
flacoman91 <flacoman91@users.noreply.github.com>
2026-05-15 15:53:47 +0000

    Deploy preview for PR 582 🛫

---
+++ pr-previews/pr-582/assets/header.stories-xuWKn7Jo.js
@@ -0,0 +1,12 @@
+import{i as e}from"./preload-helper-Cs4UwXAW.js";import{t}from"./jsx-runtime-6SDZnDXc.js";import{_ as n,g as r,h as i,t as a}from"./src-C2syaQEg.js";var o,s,c,l,u;e((()=>{a(),n(),o=t(),s={title:`Components (Draft)/Header`,tags:[`autodocs`],component:i,argTypes:{},parameters:{sbNestedCanvasPadding:`flush`}},c={name:`Header`,render:e=>(0,o.jsx)(i,{...e}),args:{links:r}},l={render:e=>(0,o.jsx)(i,{...e}),args:{links:[]}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  name: 'Header',
+  render: properties => <Header {...properties} />,
+  args: {
+    links: ExampleLinks
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: properties => <Header {...properties} />,
+  args: {
+    links: []
+  }
+}`,...l.parameters?.docs?.source}}},u=[`Default`,`Basic`]}))();export{l as Basic,c as Default,u as __namedExportsOrder,s as default};
