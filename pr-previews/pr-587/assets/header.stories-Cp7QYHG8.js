5304fe0871df905b57af5c19a443b1964f17d438
virginiacc <virginiacc@users.noreply.github.com>
2026-05-15 00:29:16 +0000

    Deploy preview for PR 587 🛫

---
+++ pr-previews/pr-587/assets/header.stories-Cp7QYHG8.js
@@ -0,0 +1,7 @@
+import{i as e}from"./preload-helper-Cs4UwXAW.js";import{t}from"./jsx-runtime-6SDZnDXc.js";import{b as n,t as r,v as i,y as a}from"./src-29KWNnAJ.js";var o,s,c,l;e((()=>{r(),n(),o=t(),s={title:`Components (Draft)/Header`,tags:[`autodocs`],component:i,argTypes:{},parameters:{sbNestedCanvasPadding:`flush`}},c={name:`Header`,render:e=>(0,o.jsx)(i,{...e}),args:{links:a}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  name: 'Header',
+  render: properties => <Header {...properties} />,
+  args: {
+    links: ExampleLinks
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
--- pr-previews/pr-587/assets/mdx-react-shim-ACSMdJCi.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-Cs4UwXAW.js";import{D as t}from"./blocks-C6xjuclB.js";var n=e((()=>{t()}));export{n as t};
