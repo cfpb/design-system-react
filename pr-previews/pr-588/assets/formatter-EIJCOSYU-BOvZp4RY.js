55b647a45b1138c3a4e8ff0f161cba8b9cc0d674
flacoman91 <flacoman91@users.noreply.github.com>
2026-05-18 17:17:29 +0000

    Deploy preview for PR 588 🛫

---
+++ pr-previews/pr-588/assets/formatter-EIJCOSYU-BOvZp4RY.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-Cs4UwXAW.js";import{M as t,N as n,P as r,X as i,Z as a,j as o}from"./iframe-BoSleZhL.js";var s;e((()=>{n(),t(),a(),s=(0,i(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
