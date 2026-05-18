12a6e0f450c288d0f665691c32a551c90d76cd43
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-05-18 09:41:04 -0700

    remove unused logos, and pngs

--- .storybook/themeCFPB.js
+++ .storybook/themeCFPB.js
@@ -1,5 +1,5 @@
 import { create } from 'storybook/theming/create';
-import CfpbLogo from '../src/assets/images/CFPB-logo-illustrator.svg?url';
+import CfpbLogo from '../src/assets/images/CFPB-logo.svg?url';
 
 const colors = {
   black: '#101820',
--- src/components/Header/responsive-menu.tsx
+++ src/components/Header/responsive-menu.tsx
@@ -1,6 +1,6 @@
 import React, { JSX, useCallback, useState } from 'react';
 import type { KeyboardEvent, MouseEvent, ReactNode } from 'react';
-import CFPBLogo from '../../assets/images/CFPB-logo-illustrator.svg?url';
+import CFPBLogo from '../../assets/images/CFPB-logo.svg?url';
 import { Button } from '../Buttons/button';
 import { Icon } from '../Icon/icon';
 import Link from '../Link/link';
