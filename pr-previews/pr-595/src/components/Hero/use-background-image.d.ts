f4d9cac63e315e2dc482c73ac2d586b7bbc7db84
flacoman91 <flacoman91@users.noreply.github.com>
2026-05-26 19:36:14 +0000

    Remove preview for PR 595 🛬

--- pr-previews/pr-595/src/components/Hero/use-background-image.d.ts
+++
@@ -1,9 +0,0 @@
-import { Ref } from '../../../node_modules/react';
-/**
- * Add inline styles which apply a background-image to the referenced <div>
- *
- * @param image Image URL
- * @param shoudAddStyles Flag to indicate that inline styles should be included
- * @returns Ref that should be attached to the wrapper <div>
- */
-export declare const useBackgroundImage: (image?: string, shoudAddStyles?: boolean) => Ref<HTMLDivElement>;
--- pr-previews/pr-595/src/components/Well/well.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { WellContainer } from './well';
-declare const meta: Meta<typeof WellContainer>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const Container: Story;
-export declare const Content: Story;
