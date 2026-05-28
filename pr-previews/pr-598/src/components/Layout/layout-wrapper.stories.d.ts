bdf117b769ebbf5dee13183f9d164a89a9e6fceb
flacoman91 <flacoman91@users.noreply.github.com>
2026-05-28 19:45:13 +0000

    Remove preview for PR 598 🛬

--- pr-previews/pr-598/src/components/Layout/layout-wrapper.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { Layout } from '../../index';
-declare const meta: Meta<typeof Layout.Wrapper>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const Wrapper: Story;
