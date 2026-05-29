0bb6474c0d072d0b1141c2d4392e9f3788afb24b
flacoman91 <flacoman91@users.noreply.github.com>
2026-05-29 03:14:16 +0000

    Remove preview for PR 597 🛬

--- pr-previews/pr-597/src/components/Hero/hero.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { Hero } from '../../index';
-declare const meta: Meta<typeof Hero>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const WithIllustration: Story;
-export declare const WithPhotograph: Story;
-export declare const WithKnockoutText: Story;
