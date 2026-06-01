6c51f13bc4976dfcc684013b53c5b578494e6d4d
flacoman91 <flacoman91@users.noreply.github.com>
2026-06-01 15:42:11 +0000

    Remove preview for PR 600 🛬

--- pr-previews/pr-600/src/components/Breadcrumb/breadcrumb.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-declare const meta: Meta<typeof BreadCrumbComponent>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const Default: Story;
-export declare const Crumb: Story;
--- pr-previews/pr-600/src/components/Layout/layout-content.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { Layout } from '../../index';
-declare const meta: Meta<typeof Layout.Content>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const Content: Story;
