5475c2c6102089b35263f82090b154b59d432378
flacoman91 <flacoman91@users.noreply.github.com>
2026-06-02 16:23:22 +0000

    Remove preview for PR 601 🛬

--- pr-previews/pr-601/src/components/Grid/grid-row.d.ts
+++
@@ -1,7 +0,0 @@
-import { JSX } from '../../../node_modules/react';
-interface GridRowProperties {
-    children: JSX.Element | JSX.Element[] | string;
-    className?: string;
-}
-export declare function GridRow({ children, className, ...properties }: GridRowProperties): JSX.Element;
-export {};
