155e2f16e0c6480183f2493c59752bb4bf1b446e
flacoman91 <flacoman91@users.noreply.github.com>
2026-06-03 19:50:55 +0000

    Deploy preview for PR 603 🛫

--- pr-previews/pr-603/src/components/TextIntroduction/test-helpers.d.ts
+++ pr-previews/pr-603/src/components/TextIntroduction/test-helpers.d.ts
@@ -2,18 +2,18 @@ export declare const heading = "Heading 1";
 export declare const subheading = "Lead paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 export declare const description = "Descriptive paragraph lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.";
 export declare const callToActionText = "Call-to-action link";
-export declare const callToAction: import("react/jsx-runtime").JSX.Element;
+export declare const callToAction: import("react").JSX.Element;
 export declare const placeholders: {
     heading: string;
     subheading: string;
     description: string;
-    callToAction: import("react/jsx-runtime").JSX.Element;
+    callToAction: import("react").JSX.Element;
 };
 declare const _default: {
     heading: string;
     description: string;
     subheading: string;
-    callToAction: import("react/jsx-runtime").JSX.Element;
+    callToAction: import("react").JSX.Element;
     callToActionText: string;
 };
 export default _default;
