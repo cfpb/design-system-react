f33da5cd06f97f92d7643e82637c207691e9492d
flacoman91 <flacoman91@users.noreply.github.com>
2026-06-03 19:45:06 +0000

    Remove preview for PR 605 🛬

--- pr-previews/pr-605/src/components/Grid/grid.stories.d.ts
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { Grid } from '../../index';
-declare const meta: Meta<typeof Grid.Wrapper>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const StandardGrid: Story;
-export declare const NestedGrid: Story;
-export declare const CenteredGrid: Story;
-export declare const GridColWidthEnum: Story;
--- pr-previews/pr-605/src/components/TextInput/text-input-status.d.ts
+++
@@ -1,3 +0,0 @@
-export type TextInputStatusType = 'error' | 'info' | 'success' | 'warning';
-export declare const TextInputStatusClassMap: Record<TextInputStatusType, string>;
-export declare const getTextInputStatusClass: (status: TextInputStatusType) => string;
