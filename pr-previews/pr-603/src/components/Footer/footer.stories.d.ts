bc293421721569379e594b8ec93262a3be074e6f
flacoman91 <flacoman91@users.noreply.github.com>
2026-06-05 17:58:00 +0000

    Remove preview for PR 603 🛬

--- pr-previews/pr-603/src/components/Footer/footer.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { Footer } from '../../index';
-declare const meta: Meta<typeof Footer>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const CFGov: Story;
-export declare const App: Story;
--- pr-previews/pr-603/src/types/jsx-element.d.ts
+++
@@ -1,2 +0,0 @@
-import { JSX } from '../../node_modules/react';
-export type JSXElement = JSX.Element | null;
