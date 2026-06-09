bcd71b1289a7593bb98534a2d6e0ae6a0647f923
flacoman91 <flacoman91@users.noreply.github.com>
2026-06-09 22:14:51 +0000

    Remove preview for PR 609 🛬

--- pr-previews/pr-609/src/components/Alert/alert-link.d.ts
+++
@@ -1,7 +0,0 @@
-import { JSX } from '../../../node_modules/react';
-export interface AlertLinkProperties {
-    href: string;
-    label: string;
-    isExternal?: boolean;
-}
-export declare const AlertLink: ({ href, label, isExternal, }: AlertLinkProperties) => JSX.Element;
--- pr-previews/pr-609/src/components/Select/select-single.d.ts
+++
@@ -1,3 +0,0 @@
-import { JSX } from '../../../node_modules/react';
-import { SelectProperties } from './select';
-export declare const SelectSingle: ({ id, options, label, onChange, value, defaultOptionLabel, ...properties }: SelectProperties) => JSX.Element;
