13182602c946c677884cc632995175f36afe3e3c
flacoman91 <flacoman91@users.noreply.github.com>
2026-06-12 23:25:01 +0000

    Deploy preview for PR 611 🛫

---
+++ pr-previews/pr-611/src/components/Header/header.stories.d.ts
@@ -0,0 +1,8 @@
+import { Meta, StoryObj } from '@storybook/react-vite';
+import { Header } from '../../index';
+declare const meta: Meta<typeof Header>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+export declare const Default: Story;
+export declare const Basic: Story;
+export declare const LinksWithIcon: Story;
