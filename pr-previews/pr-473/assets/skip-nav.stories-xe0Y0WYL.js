83a3dcc7875e0bd6f4491994bfc097ab425c03e0
flacoman91 <flacoman91@users.noreply.github.com>
2026-06-12 18:53:12 +0000

    Deploy preview for PR 473 🛫

--- pr-previews/pr-473/assets/skip-nav.stories-xe0Y0WYL.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-CaZkqeYb.js";import{c as n,t as r}from"./src-BEXKnrCd.js";var i,a,o,s;e((()=>{r(),i=t(),a={title:`Components (Draft)/Skip Navigation`,tags:[`autodocs`],component:n,argTypes:{}},o={render:e=>(0,i.jsx)(n,{...e}),args:{href:`#main`}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: properties => <SkipNav {...properties} />,
-  args: {
-    href: '#main'
-  }
-}`,...o.parameters?.docs?.source}}},s=[`Default`]}))();export{o as Default,s as __namedExportsOrder,a as default};
