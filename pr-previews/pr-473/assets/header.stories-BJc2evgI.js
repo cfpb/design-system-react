31add52088074691478d0a7dfea353b114cc3b2f
flacoman91 <flacoman91@users.noreply.github.com>
2026-06-12 21:44:40 +0000

    Deploy preview for PR 473 🛫

---
+++ pr-previews/pr-473/assets/header.stories-BJc2evgI.js
@@ -0,0 +1,13 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t}from"./jsx-runtime-CaZkqeYb.js";import{E as n,O as r,k as i,t as a}from"./src-DsKWVAxW.js";var o,s,c,l,u;e((()=>{a(),i(),o=t(),s={title:`Components (Draft)/Headers`,tags:[`autodocs`],component:n,argTypes:{lang:{control:`select`,options:[`en`,`es`],description:`Logo language (English or Spanish)`}},parameters:{sbNestedCanvasPadding:`flush`}},c={name:`Header`,render:e=>(0,o.jsx)(n,{...e}),args:{links:r,lang:`en`}},l={render:e=>(0,o.jsx)(n,{...e}),args:{links:[]}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  name: 'Header',
+  render: properties => <Header {...properties} />,
+  args: {
+    links: ExampleLinks,
+    lang: 'en'
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: properties => <Header {...properties} />,
+  args: {
+    links: []
+  }
+}`,...l.parameters?.docs?.source}}},u=[`Default`,`Basic`]}))();export{l as Basic,c as Default,u as __namedExportsOrder,s as default};
---
+++ pr-previews/pr-473/assets/mdx-react-shim-YNR5OQwV.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{h as t}from"./blocks-Bde9CEE_.js";var n=e((()=>{t()}));export{n as t};
