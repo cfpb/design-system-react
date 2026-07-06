755c21f4cbc6d4c2a54563bbc59b245a39c665b5
flacoman91 <flacoman91@users.noreply.github.com>
2026-07-06 21:32:51 +0000

    Remove preview for PR 623 🛬

--- pr-previews/pr-623/src/components/Label/label.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { Label } from '../../index';
-declare const meta: Meta<typeof Label>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const LabelHeading: Story;
-export declare const InlineLabel: Story;
