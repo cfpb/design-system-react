11b33f82eaa52af8d563e9ac974d38e126d61ba3
flacoman91 <flacoman91@users.noreply.github.com>
2026-07-06 19:59:02 +0000

    Deploy preview for PR 623 🛫

---
+++ pr-previews/pr-623/src/components/Divider/divider.d.ts
@@ -0,0 +1,3 @@
+import { HTMLProps, ReactElement } from '../../../node_modules/react';
+export declare const Divider: ({ className, ...properties }: HTMLProps<HTMLDivElement>) => ReactElement;
+export default Divider;
---
+++ pr-previews/pr-623/src/components/Layout/layout-main.stories.d.ts
@@ -0,0 +1,7 @@
+import { Meta, StoryObj } from '@storybook/react-vite';
+import { Layout } from '../../index';
+declare const meta: Meta<typeof Layout.Main>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+export declare const Layout_2_1: Story;
+export declare const Layout_1_3: Story;
