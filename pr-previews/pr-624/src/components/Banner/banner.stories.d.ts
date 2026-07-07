053a28fb5f8c05e3d4ed7c25deac335d1b4110e3
flacoman91 <flacoman91@users.noreply.github.com>
2026-07-07 21:29:20 +0000

    Remove preview for PR 624 🛬

--- pr-previews/pr-624/src/components/Banner/banner.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { Banner } from '../../index';
-declare const meta: Meta<typeof Banner>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const USGovBanner: Story;
-export declare const USGovBannerWithCFGovLinks: Story;
-export declare const USGovBannerWithLinks: Story;
