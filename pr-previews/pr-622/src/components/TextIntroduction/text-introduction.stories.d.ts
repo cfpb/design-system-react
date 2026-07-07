207b288c078419dabbd6868f809aaac473dbba82
flacoman91 <flacoman91@users.noreply.github.com>
2026-07-07 20:40:52 +0000

    Remove preview for PR 622 🛬

--- pr-previews/pr-622/src/components/TextIntroduction/text-introduction.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { TextIntroduction } from '../../index';
-declare const meta: Meta<typeof TextIntroduction>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const Standard: Story;
-export declare const Flexible: Story;
