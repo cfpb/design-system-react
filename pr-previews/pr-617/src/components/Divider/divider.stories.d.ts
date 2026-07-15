36e7b0390959c7a4cafab51af83006b8f203dd24
flacoman91 <flacoman91@users.noreply.github.com>
2026-07-15 21:56:49 +0000

    Remove preview for PR 617 🛬

--- pr-previews/pr-617/src/components/Divider/divider.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { Divider } from '../../index';
-declare const meta: Meta<typeof Divider>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const Default: Story;
-export declare const ExampleUsage: Story;
--- pr-previews/pr-617/src/components/Select/select.stories.d.ts
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { SelectSingle } from '../../index';
-declare const meta: Meta<typeof SelectSingle>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const SingleSelect: Story;
-export declare const SingleSelectHover: Story;
-export declare const SingleSelectFocus: Story;
-export declare const SingleSelectDisabled: Story;
