0dc165a26bfd89c49e23d2952f9b3345bbce4271
flacoman91 <flacoman91@users.noreply.github.com>
2026-07-16 17:31:18 +0000

    Remove preview for PR 587 🛬

--- pr-previews/pr-587/src/components/select/select-utilities.d.ts
+++
@@ -1,4 +0,0 @@
-import { JSX } from '../../../node_modules/react';
-import { SelectOption } from './select';
-export declare const buildOptions: (options: SelectOption[], defaultOptionLabel: string) => JSX.Element[];
-export declare const findOptionByValue: (options: SelectOption[], value: string) => SelectOption | undefined;
