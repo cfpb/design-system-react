088a8fc4b1184e1866cd6395df5055d23f8590b6
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-07-16 11:21:51 -0700

    text casing

--- src/context/dsr-context.tsx
+++ src/context/dsr-context.tsx
@@ -1,24 +1,24 @@
-import { createContext, use } from 'react'
+import { createContext, use } from 'react';
 import type { ComponentType, ReactNode } from 'react';
-import { BaseLink } from '../components/Link/base-link';
-import type { BaseLinkProperties } from '../components/Link/base-link';
+import { BaseLink } from '../components/link/base-link';
+import type { BaseLinkProperties } from '../components/link/base-link';
 
 /**
- * 
- * DSR context allows you to integrate the DSR with a router library 
- * by specifying a custom component that will be output by the DSR's 
- * Link component instead of the default anchor element. 
- * 
- * Your custom link component will be substituted everywhere the DSR's 
- * Link component is used, including in the Header, Breadcrumbs, 
+ *
+ * DSR context allows you to integrate the DSR with a router library
+ * by specifying a custom component that will be output by the DSR's
+ * Link component instead of the default anchor element.
+ *
+ * Your custom link component will be substituted everywhere the DSR's
+ * Link component is used, including in the Header, Breadcrumbs,
  * and Secondary Navigation components.
- * 
+ *
  * Example usage with Tanstack Router:
- * 
+ *
  * rootRoute.tsx:
- * 
+ *
  * import { createRootRouteWithContext, Link, Outlet } from '@tanstack/react-router'
- * 
+ *
  * const rootRoute = createRootRouteWithContext<RouterContext>()({
  *  component: (): JSX.Element => (
  *    <DSRContext value={{LinkComponent: Link}} >
@@ -26,7 +26,7 @@ import type { BaseLinkProperties } from '../components/Link/base-link';
  *    </DSRContext>
  *  )
  * })
- * 
+ *
  */
 
 export interface DSRContextType {
@@ -42,15 +42,16 @@ export const useDSRContext = () => use(DSRContext);
 export interface DSRProviderProperties {
   LinkComponent: ComponentType<BaseLinkProperties>;
   children?: ReactNode;
-};
+}
 
-export const DSRProvider = ({ children, LinkComponent }: { 
-  children: ReactNode; 
+export const DSRProvider = ({
+  children,
+  LinkComponent,
+}: {
+  children: ReactNode;
   LinkComponent: ComponentType<BaseLinkProperties>;
 }) => {
   return (
-    <DSRContext value={{LinkComponent: LinkComponent}}>
-      {children}
-    </DSRContext>
+    <DSRContext value={{ LinkComponent: LinkComponent }}>{children}</DSRContext>
   );
 };
