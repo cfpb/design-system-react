fcf4264c115f74edd770e0afe62d4267fbb377cf
flacoman91 <flacoman91@users.noreply.github.com>
2026-07-16 19:03:14 +0000

    Deploy preview for PR 563 🛫

---
+++ pr-previews/pr-563/assets/breadcrumb.stories-CYCxPXQY.js
@@ -0,0 +1,21 @@
+import{i as e}from"./preload-helper-BdFrVu1K.js";import{t}from"./jsx-runtime-f3rHp9ZU.js";import{Z as n,t as r}from"./src-B7AEp-fS.js";var i,a,o,s,c;e((()=>{r(),i=t(),a={title:`Components (Draft)/Breadcrumbs`,tags:[`autodocs`],component:n,argTypes:{}},o={name:`Breadcrumb`,render:e=>(0,i.jsx)(n,{...e}),args:{crumbs:[{to:`/`,label:`Page link`},{to:`/`,label:`Page link`}]}},s={render:e=>(0,i.jsx)(n,{...e}),args:{crumbs:[{to:`/`,label:`Page link`}]}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  name: 'Breadcrumb',
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      to: '/',
+      label: 'Page link'
+    }, {
+      to: '/',
+      label: 'Page link'
+    }]
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  render: properties => <Breadcrumb {...properties} />,
+  args: {
+    crumbs: [{
+      to: '/',
+      label: 'Page link'
+    }]
+  }
+}`,...s.parameters?.docs?.source}}},c=[`Default`,`Crumb`]}))();export{s as Crumb,o as Default,c as __namedExportsOrder,a as default};
