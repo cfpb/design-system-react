c988e68e9fab65c5121996bf715322839a240d98
flacoman91 <flacoman91@users.noreply.github.com>
2026-08-05 22:55:10 +0000

    Remove preview for PR 633 🛬

--- pr-previews/pr-633/src/components/headings/heading.d.ts
+++
@@ -1,8 +0,0 @@
-import { JSX, HTMLProps } from '../../../node_modules/react';
-export type HeadingType = '1' | '2' | '3' | '4' | '5' | 'display' | 'eyebrow' | 'slug';
-interface HeadingProperties extends HTMLProps<HTMLHeadingElement> {
-    /** Heading type (1-5, display, eyebrow, slug) */
-    type?: HeadingType;
-}
-export declare const Heading: ({ type, children, className, ...properties }: HeadingProperties) => JSX.Element;
-export {};
--- pr-previews/pr-633/src/components/link/base-link.d.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from '../../../node_modules/react';
-import { JSXElement } from '../../types/jsx-element';
-export interface BaseLinkProperties {
-    to: string | undefined;
-    children: ReactNode;
-    [key: string]: unknown;
-}
-export declare const BaseLink: ({ to, children, ...others }: BaseLinkProperties) => JSXElement | null;
