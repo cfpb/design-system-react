0666fb997ee74e5a8477f57d201a347ebf498d53
flacoman91 <1038306+flacoman91@users.noreply.github.com>
2026-08-21 20:03:36 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@238661c36fb131ded66ee6fd119686c55e5949e9 🚀

---
+++ assets/header.stories-fDZZ5Il5.js
@@ -0,0 +1,13 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-DeHZSEgm.js";import{i as n,n as r,r as i,t as a}from"./header-CarEi8CQ.js";var o,s,c,l,u;function d(){return(d=e((()=>{r(),n(),o=t(),s={title:`Components (Draft)/Headers`,tags:[`autodocs`],component:a,argTypes:{lang:{control:`select`,options:[`en`,`es`],description:`Logo language (English or Spanish)`}},parameters:{sbNestedCanvasPadding:`flush`}},c={name:`Header`,render:e=>(0,o.jsx)(a,{...e}),args:{links:i,lang:`en`}},l={render:e=>(0,o.jsx)(a,{...e}),args:{links:[]}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  name: 'Header',
+  render: properties => <Header {...properties} />,
+  args: {
+    links: ExampleLinks,
+    lang: 'en'
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: properties => <Header {...properties} />,
+  args: {
+    links: []
+  }
+}`,...l.parameters?.docs?.source}}},u=[`Default`,`Basic`]})))()}d();export{l as Basic,c as Default,u as __namedExportsOrder,s as default};
