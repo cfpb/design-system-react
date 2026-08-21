75c100d77f0b47ef67fca6bd8960a780f38b2f40
flacoman91 <flacoman91@users.noreply.github.com>
2026-08-21 20:02:53 +0000

    Remove preview for PR 638 🛬

--- pr-previews/pr-638/src/components/table/table-utilities.d.ts
+++
@@ -1,4 +0,0 @@
-import { TableProperties } from './table';
-import { JSXElement } from '../../types/jsx-element';
-export declare const buildColumnHeaders: (columns: TableProperties["columns"]) => JSXElement;
-export declare const buildRows: (rows: TableProperties["rows"], columns: TableProperties["columns"]) => JSXElement;
