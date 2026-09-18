1f922c298a35fdab762992ad9b2c5930620b607b
flacoman91 <1038306+flacoman91@users.noreply.github.com>
2026-09-18 23:01:32 +0000

    Deploying to gh-pages from @ cfpb/design-system-react@b098664e56b559a31ed0fb849f4ec32e185f84a8 🚀

---
+++ assets/iframe-FsLVheax.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-DJS-rH8m.js";e();
---
+++ assets/tagline.stories-DWfA4_XM.js
@@ -0,0 +1,12 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-DeHZSEgm.js";import{n,t as r}from"./cfpb-tagline-D7d3qPyC.js";var i,a,o,s,c,l,u;function d(){return(d=e((()=>{n(),i=t(),a=e=>(r.init(),(0,i.jsx)(`cfpb-tagline`,{...e})),o=e=>e?`<cfpb-tagline isLarge></cfpb-tagline>`:`<cfpb-tagline></cfpb-tagline>`,s={title:`Components (Verified)/Taglines`,tags:[`autodocs`,`web-component`],component:a,parameters:{docs:{source:{type:`code`,language:`jsx`,transform:(e,{args:t})=>o(t.isLarge)},description:{component:`
+
+Taglines are short paragraphs of text with the USA flag to their left that are used in the header and footer across consumerfinance.gov.
+
+Use the \`<cfpb-tagline>\` web component after calling \`CfpbTagline.init()\`.
+
+Source: https://cfpb.github.io/design-system/components/taglines
+`}}}},c={},l={args:{isLarge:!0}},u=[`Standard`,`Large`],c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    isLarge: true
+  }
+}`,...l.parameters?.docs?.source}}}})))()}d();export{l as Large,c as Standard,u as __namedExportsOrder,s as default};
