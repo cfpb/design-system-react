73f8075d423698854ac2612e8632ed916bd04232
flacoman91 <flacoman91@users.noreply.github.com>
2026-09-18 23:00:27 +0000

    Remove preview for PR 640 🛬

--- pr-previews/pr-640/src/components/buttons/button-group.d.ts
+++
@@ -1,6 +0,0 @@
-import { HTMLProps } from '../../../node_modules/react';
-import { JSXElement } from '../../types/jsx-element';
-/**
- * Display a collection of related Buttons
- */
-export declare const ButtonGroup: ({ className, children, ...other }: HTMLProps<HTMLDivElement>) => JSXElement;
--- pr-previews/pr-640/src/components/well/well.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
-import { WellContainer, WellContent } from './well';
-declare const meta: Meta<typeof WellContainer>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-type ContentStory = StoryObj<typeof WellContent>;
-export declare const Container: Story;
-export declare const Content: ContentStory;
