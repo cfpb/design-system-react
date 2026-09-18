32313835d616b6b625c82a52dd39fd5405ca0973
flacoman91 <flacoman91@users.noreply.github.com>
2026-09-18 12:10:18 +0000

    Deploy preview for PR 640 🛫

---
+++ pr-previews/pr-640/assets/iframe-ObNBVm52.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-Emq09hNN.js";e();
--- pr-previews/pr-640/src/components/text-input/text-area.d.ts
+++ pr-previews/pr-640/src/components/text-input/text-area.d.ts
@@ -9,6 +9,6 @@ export interface TextAreaType {
     status?: TextInputStatusType;
 }
 /**
- * Source: <a href='https://cfpb.github.io/design-system/components/text-inputs#text-area-input-1' target='_blank'> https://cfpb.github.io/design-system/components/text-inputs#text-area-input-1</a>
+ * Source: <a href='https://cfpb.github.io/design-system/components/text-inputs#text-area-input' target='_blank'> https://cfpb.github.io/design-system/components/text-inputs#text-area-input</a>
  */
 export declare const TextArea: import('../../../node_modules/react').ForwardRefExoticComponent<Omit<import('../../../node_modules/react').ClassAttributes<HTMLTextAreaElement> & import('../../../node_modules/react').TextareaHTMLAttributes<HTMLTextAreaElement> & TextAreaType, "ref"> & import('../../../node_modules/react').RefAttributes<HTMLTextAreaElement>>;
